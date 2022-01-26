bbb09cc8c7f95571ffd3c217f4fb5ec5e098d9cd
lyb9812 <liyuanbo9812@pku.edu.cn>
2022-01-27 00:25:26 +0800

    add convertion from wfc_2d to WFC_K

--- source/src_lcao/LCAO_evolve.cpp
+++ source/src_lcao/LCAO_evolve.cpp
@@ -4,7 +4,9 @@
 #include "../src_io/write_HS.h"
 #include"../input.h"
 #include <complex>
+#include "../module_base/lapack_connector.h"
 #include "../module_base/scalapack_connector.h"
+#include "../module_base/blas_connector.h"
 //fuxiang add 2016-10-28
 
 Evolve_LCAO_Matrix::Evolve_LCAO_Matrix(){}
@@ -37,6 +39,80 @@ void Evolve_LCAO_Matrix::evolve_complex_matrix(const int &ik, std::complex<doubl
 	ModuleBase::GlobalFunc::OUT_TIME("evolve(std::complex)", time_start, time_end);
 	
 	return;
+}
+
+inline int globalIndex(int localIndex, int nblk, int nprocs, int myproc)
+{
+    int iblock, gIndex;
+    iblock=localIndex/nblk;
+    gIndex=(iblock*nprocs+myproc)*nblk+localIndex%nblk;
+    return gIndex;
+}
+
+inline int work2WFC(
+	int naroc[2],
+	int nb,
+	int dim0,
+	int dim1,
+	int iprow,
+	int ipcol,
+	std::complex<double>* work,
+	std::complex<double>** WFC,
+	std::complex<double>** WFCAUG)
+{
+    for(int j=0; j<naroc[1]; ++j)
+    {
+        int igcol=globalIndex(j, nb, dim1, ipcol);
+        if(igcol>=GlobalV::NBANDS) continue;
+        for(int i=0; i<naroc[0]; ++i)
+        {
+            int igrow=globalIndex(i, nb, dim0, iprow);
+	        int mu_local=GlobalC::GridT.trace_lo[igrow];
+            if(mu_local>=0)
+            {
+                WFC[igcol][mu_local]=work[j*naroc[0]+i];
+            }
+	        int mu_aug=GlobalC::LOWF.trace_aug[igrow];
+            if(mu_aug>=0)
+            {
+                WFCAUG[igcol][mu_aug]=work[j*naroc[0]+i];
+            }
+        }
+    }
+    return 0;
+}
+
+inline int WFC2work(
+	int naroc[2],
+	int nb,
+	int dim0,
+	int dim1,
+	int iprow,
+	int ipcol,
+	std::complex<double>* work,
+	std::complex<double>** WFC,
+	std::complex<double>** WFCAUG)
+{
+    for(int j=0; j<naroc[1]; ++j)
+    {
+        int igcol=globalIndex(j, nb, dim1, ipcol);
+        if(igcol>=GlobalV::NBANDS) continue;
+        for(int i=0; i<naroc[0]; ++i)
+        {
+            int igrow=globalIndex(i, nb, dim0, iprow);
+	        int mu_local=GlobalC::GridT.trace_lo[igrow];
+            if(mu_local>=0)
+            {
+                work[j*naroc[0]+i]=WFC[igcol][mu_local];
+            }
+	        int mu_aug=GlobalC::LOWF.trace_aug[igrow];
+            if(mu_aug>=0)
+            {
+                work[j*naroc[0]+i]=WFCAUG[igcol][mu_aug];
+            }
+        }
+    }
+    return 0;
 }
 
 void Evolve_LCAO_Matrix::using_LAPACK_complex(const int &ik, std::complex<double>** c, std::complex<double>** c_init)const
@@ -297,107 +373,81 @@ int Evolve_LCAO_Matrix::using_ScaLAPACK_complex(const int &ik, complex<double>**
 	//matrix-matrix product
 	//pzgemm
 
-	//cout << "begin1: " <<endl;
-
 	char uplo = 'U';
 	const int inc = 1;
 
 	const int  one_int = 1;
+	const int zero_int = 0;
 	
-	//int nprocs, myid;
-	//MPI_status status;
-	//MPI_Comm_size(comm_2D, &nprocs);
-	//MPI_Comm_rank(comm_2D, &myid);
+	int ncol,nrow;
+	ncol = GlobalC::ParaO.ncol;
+	nrow = GlobalC::ParaO.nrow;
 
 	int loc_pos;
-	//complex<double>* Stmp = GlobalC::LM.Sdiag2;
-	//complex<double>* Htmp1 = GlobalC::LM.Hdiag2;
-	//complex<double>* Htmp2 = GlobalC::LM.Sdiag2;
-	//complex<double>* Htmp3 = GlobalC::LM.Sdiag2;
 
-	complex<double>* Stmp = new complex<double> [GlobalC::ParaO.nloc];
-	complex<double>* Htmp1 = new complex<double> [GlobalC::ParaO.nloc];
-	complex<double>* Htmp2 = new complex<double> [GlobalC::ParaO.nloc];
-	complex<double>* Htmp3 = new complex<double> [GlobalC::ParaO.nloc];
-	ModuleBase::GlobalFunc::ZEROS(Stmp,GlobalC::ParaO.nloc);
-	ModuleBase::GlobalFunc::ZEROS(Htmp1,GlobalC::ParaO.nloc);
-	ModuleBase::GlobalFunc::ZEROS(Htmp2,GlobalC::ParaO.nloc);
-	ModuleBase::GlobalFunc::ZEROS(Htmp3,GlobalC::ParaO.nloc);
+	//complex<double>* Stmp = new complex<double> [GlobalC::ParaO.nloc];
+	//complex<double>* Htmp1 = new complex<double> [GlobalC::ParaO.nloc];
+	//complex<double>* Htmp2 = new complex<double> [GlobalC::ParaO.nloc];
+	//complex<double>* Htmp3 = new complex<double> [GlobalC::ParaO.nloc];
+	std::vector<std::complex<double>> Stmp;
+	std::vector<std::complex<double>> Htmp1;
+	std::vector<std::complex<double>> Htmp2;
+	std::vector<std::complex<double>> Htmp3;
+	ModuleBase::GlobalFunc::ZEROS(Stmp.data(),GlobalC::ParaO.nloc);
+	ModuleBase::GlobalFunc::ZEROS(Htmp1.data(),GlobalC::ParaO.nloc);
+	ModuleBase::GlobalFunc::ZEROS(Htmp2.data(),GlobalC::ParaO.nloc);
+	ModuleBase::GlobalFunc::ZEROS(Htmp3.data(),GlobalC::ParaO.nloc);
+
+	zcopy_(&GlobalC::ParaO.nloc, GlobalC::LM.Sloc2.data(), &inc, Stmp.data(), &inc);
+	zcopy_(&GlobalC::ParaO.nloc, GlobalC::LM.Hloc2.data(), &inc, Htmp1.data(), &inc);
+	zcopy_(&GlobalC::ParaO.nloc, GlobalC::LM.Hloc2.data(), &inc, Htmp2.data(), &inc);
+	zcopy_(&GlobalC::ParaO.nloc, GlobalC::LM.Hloc2.data(), &inc, Htmp3.data(), &inc);
 	
-	//cout << "GlobalC::LM.Hloc2" << *GlobalC::LM.Hloc2 << endl;
-	//cout << "*Htmp2: " << *Htmp2 << endl;
-
-        double *eigen = new double[GlobalV::NLOCAL];
-        ModuleBase::GlobalFunc::ZEROS(eigen, GlobalV::NLOCAL);
-
-	//cout << "GlobalV::NLOCAL : " << GlobalV::NLOCAL << endl;
-	//cout << "GlobalC::ParaO.nloc : " << GlobalC::ParaO.nloc << endl;
-
-	//cout << "begin02:" <<endl;
-
-        //assert(loc_size > 0);
-        //complex<double>* Z = new complex<double>[this->loc_size * NLOCAL];
-        //ModuleBase::GlobalFunc::ZEROS(Z, this->loc_size * NLOCAL);
-
-	zcopy_(&GlobalC::ParaO.nloc, GlobalC::LM.Sloc2.data(), &inc, Stmp, &inc);
-	zcopy_(&GlobalC::ParaO.nloc, GlobalC::LM.Hloc2.data(), &inc, Htmp1, &inc);
-	zcopy_(&GlobalC::ParaO.nloc, GlobalC::LM.Hloc2.data(), &inc, Htmp2, &inc);
-	zcopy_(&GlobalC::ParaO.nloc, GlobalC::LM.Hloc2.data(), &inc, Htmp3, &inc);
-
-	//cout << "*Htmp2: " << *Htmp2 << endl;
-
+	double tmp_dt;
+	//tmp_dt = INPUT.mdp.dt;
+	tmp_dt = 0.01;
 	complex<double> alpha = {1.0, 0.0};
 	char transa = 'N';
 	int desca = 0; 
-	complex<double> beta = {0.0, -0.5*0.02*41.34};  // this need modify
+	complex<double> beta = {0.0, -0.5*tmp_dt*41.34};  // this need modify
 	int descc = 0;
-
-	//cout << "begin03:" << endl;
 
         pzgeadd_(
 		&transa,
 		&GlobalV::NLOCAL, &GlobalV::NLOCAL,
 		&alpha,
-		Stmp, &one_int, &one_int, GlobalC::ParaO.desc,
+		Stmp.data(), &one_int, &one_int, GlobalC::ParaO.desc,
                 &beta,
-		Htmp1, &one_int, &one_int, GlobalC::ParaO.desc);
+		Htmp1.data(), &one_int, &one_int, GlobalC::ParaO.desc);	
 
 	//beta = (0.0, 0.5)*INPUT.md_dt;
-	beta = {0.0, 0.5*0.02*41.34}; // this need modify
-
-	//cout << "*Htmp1: " << *Htmp1 << endl;
-	//cout << "Htmp2: " << *Htmp2 << endl;
+	beta = {0.0, 0.5*tmp_dt*41.34}; // this need modify
 
 	pzgeadd_(
 		&transa,
 		&GlobalV::NLOCAL, &GlobalV::NLOCAL,
 		&alpha,
-		Stmp, &one_int, &one_int, GlobalC::ParaO.desc, 
+		Stmp.data(), &one_int, &one_int, GlobalC::ParaO.desc, 
 		&beta,
-		Htmp2, &one_int, &one_int, GlobalC::ParaO.desc);
+		Htmp2.data(), &one_int, &one_int, GlobalC::ParaO.desc);
 
 	//Next, invert the denominator
 	int *ipiv = new int[ GlobalC::ParaO.nloc ];
+	ModuleBase::GlobalFunc::ZEROS(ipiv,GlobalC::ParaO.nloc);
 	int info;
-
-	//cout << "*Htmp2: " << *Htmp2 << endl;
-	//cout << "begin04:" << endl;
 
 	pzgetrf_(
 		&GlobalV::NLOCAL, &GlobalV::NLOCAL, 
-		Htmp2, &one_int, &one_int, GlobalC::ParaO.desc,
+		Htmp2.data(), &one_int, &one_int, GlobalC::ParaO.desc,
 		ipiv,  &info);
 
 	int LWORK=-1, liWORK=-1;
 	std::vector<std::complex<double>> WORK(1,0);
 	std::vector<int> iWORK(1,0);
 
-
-	//cout << "begin05:" << endl;
-
 	pzgetri_(
 		&GlobalV::NLOCAL, 
-		Htmp2, &one_int, &one_int, GlobalC::ParaO.desc,
+		Htmp2.data(), &one_int, &one_int, GlobalC::ParaO.desc,
 		ipiv,  WORK.data(),  &LWORK, iWORK.data(), &liWORK, &info);
 
 	LWORK = WORK[0].real();
@@ -407,52 +457,119 @@ int Evolve_LCAO_Matrix::using_ScaLAPACK_complex(const int &ik, complex<double>**
 
 	pzgetri_(
 		&GlobalV::NLOCAL, 
-		Htmp2, &one_int, &one_int, GlobalC::ParaO.desc,
+		Htmp2.data(), &one_int, &one_int, GlobalC::ParaO.desc,
 		ipiv,  WORK.data(),  &LWORK, iWORK.data(), &liWORK, &info);
-
-	//alpha = (1.0, 0.0);
-	//beta = (0.0, 0.0);
-	char transb = 'T'; //This place requires subsequent testing of different transb.
+	
+	char transb = 'T';
 	int descb = 0; 
 
 	double alpha_1 = 1.0;
 	double beta_1 = 0.0;
+	std::complex<double> im(0,1);
 
-	//cout << "*Htmp2: " << *Htmp2 << endl;
-	//cout << "begin06:" << endl;
-	
-
+	transa='T';
+	transb='T';
 	pzgemm_(
 		&transa, &transb,
 		&GlobalV::NLOCAL, &GlobalV::NLOCAL, &GlobalV::NLOCAL,
 		&alpha_1,
-		Htmp2, &one_int, &one_int, GlobalC::ParaO.desc,
-		Htmp1, &one_int, &one_int, GlobalC::ParaO.desc, 
+		Htmp2.data(), &one_int, &one_int, GlobalC::ParaO.desc,
+		Htmp1.data(), &one_int, &one_int, GlobalC::ParaO.desc, 
 		&beta_1,
-		Htmp3, &one_int, &one_int, GlobalC::ParaO.desc);
-
+		Htmp3.data(), &one_int, &one_int, GlobalC::ParaO.desc);
 	
-
-	//cout << "U_operator Success!!!" <<endl;
-
-	pzgemv_(
-		&transa,
-		&GlobalV::NLOCAL, &GlobalV::NLOCAL, 
+	int nprocs, myid;
+	long maxnloc,nloc; // maximum number of elements in local matrix
+	nloc=GlobalC::ParaO.nloc;
+	MPI_Comm comm_2D_0;
+	MPI_Comm comm_2D;
+	comm_2D_0 = MPI_COMM_WORLD;
+	int dim0,dim1;
+	dim0 = (int)sqrt((double)GlobalV::DSIZE); 
+	while (GlobalV::DSIZE%dim0!=0)
+	{
+		dim0 = dim0 - 1;
+	}
+	assert(dim0 > 0);
+	dim1=GlobalV::DSIZE/dim0;
+	int dim[2];
+	dim[0]=dim0;
+	dim[1]=dim1;
+	int period[2]={1,1};
+	int reorder=0;
+	MPI_Cart_create(comm_2D_0,2,dim,period,reorder,&comm_2D);
+    MPI_Status status;
+    MPI_Comm_size(comm_2D, &nprocs);
+    MPI_Comm_rank(comm_2D, &myid);
+    MPI_Reduce(&nloc, &maxnloc, 1, MPI_LONG, MPI_MAX, 0, comm_2D);
+    MPI_Bcast(&maxnloc, 1, MPI_LONG, 0, comm_2D);
+	int naroc[2]; // maximum number of row or column
+	int nb;
+	nb = GlobalC::ParaO.nb;
+	std::complex<double> *work1=new std::complex<double>[maxnloc];
+	ModuleBase::GlobalFunc::ZEROS(work1,maxnloc);
+	std::complex<double> *work2=new std::complex<double>[maxnloc];
+	ModuleBase::GlobalFunc::ZEROS(work2,maxnloc);
+    for(int iprow=0; iprow<dim0; ++iprow)
+    {
+        for(int ipcol=0; ipcol<dim1; ++ipcol)
+        {
+            const int coord[2]={iprow, ipcol};
+            int src_rank;
+            MPI_Cart_rank(comm_2D, coord, &src_rank);
+            if(myid==src_rank)
+        	{
+                naroc[0]=nrow;
+                naroc[1]=ncol;
+				info=WFC2work(naroc, nb, dim0, dim1, iprow, ipcol, work1, GlobalC::LOWF.WFC_K_backup[ik], GlobalC::LOWF.WFC_K_aug_backup[ik]);
+			}
+		}
+	}
+	
+	alpha_1=1.0;
+	beta_1=0.0;
+	transa = 'T';
+	transb = 'T';
+	pzgemm_(
+		&transa, &transb,
+		&GlobalV::NLOCAL, &GlobalV::NLOCAL, &GlobalV::NLOCAL,
 		&alpha_1,
-		Htmp3, &one_int, &one_int, GlobalC::ParaO.desc,
-		wfc_2d.c, &one_int, &one_int, GlobalC::ParaO.desc, &one_int, 
+		work1, &one_int, &one_int, GlobalC::ParaO.desc,
+		Htmp3.data(), &one_int, &one_int, GlobalC::ParaO.desc, 
 		&beta_1,
-		wfc_2d.c, &one_int, &one_int, GlobalC::ParaO.desc, &one_int
-        );
-
-
-
-        // the eigenvalues.
-        //dcopy_(&NBANDS, eigen, &inc, ekb, &inc);
-        delete[] eigen;
-
-        // Z is delete in gath_eig
-        //ModuleBase::timer::tick("Evolve_LCAO_Matrix","gath_eig_complex",'G');
+		work2, &one_int, &one_int, GlobalC::ParaO.desc);
+	
+	alpha=1.0;
+	beta=0.0;
+	pzgeadd_(
+		&transa,
+		&GlobalV::NLOCAL, &GlobalV::NLOCAL,
+		&alpha,
+		work2, &one_int, &one_int, GlobalC::ParaO.desc,
+        &beta,
+		work1, &one_int, &one_int, GlobalC::ParaO.desc);
+	
+	for(int iprow=0; iprow<dim0; ++iprow)
+    {
+        for(int ipcol=0; ipcol<dim1; ++ipcol)
+        {
+            const int coord[2]={iprow, ipcol};
+            int src_rank;
+            MPI_Cart_rank(comm_2D, coord, &src_rank);
+            if(myid==src_rank)
+        	{
+				zcopy_(&nloc, work1, &inc, work2, &inc);
+                naroc[0]=nrow;
+                naroc[1]=ncol;
+				}
+			info=MPI_Bcast(naroc, 2, MPI_INT, src_rank, comm_2D);
+            info=MPI_Bcast(work2, maxnloc, MPI_DOUBLE_COMPLEX, src_rank, comm_2D);
+			info=work2WFC(naroc, nb,dim0, dim1, iprow, ipcol, work2, GlobalC::LOWF.WFC_K[ik], GlobalC::LOWF.WFC_K_aug[ik]);
+		}
+	}
+	delete work1;
+	delete work2;
+	delete ipiv;
 	return 0;
 
 }
