fbfef7a1cdba19428b32bebe4cd495c91e4fe980
root <hongriTianqi>
2022-02-28 09:34:35 +0800

    Revert "modify math_bspline.cpp and math_bspline.h"
    This reverts commit 558d98529f215c666eaa58b5ae3c6a8a340d9c55.

--- source/module_base/math_bspline.cpp
+++ source/module_base/math_bspline.cpp
@@ -34,6 +34,12 @@ void Bspline::init(int norderin, double Dxin, double xiin)
     }
 }
 
+void Bspline::cleanp()
+{
+    delete[] bezier;
+    bezier = NULL;
+}
+
 double Bspline::bezier_ele(int n)
 {
     return this->bezier[n];
