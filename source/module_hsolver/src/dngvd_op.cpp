35aed8b63b7c6e6a2165aee8f29cc45979f8b6be
haozhihan <haozhi.han@stu.pku.edu.cn>
2022-12-02 20:50:33 +0800

    Fix dngv_op function bug

--- source/module_hsolver/src/dngvd_op.cpp
+++ source/module_hsolver/src/dngvd_op.cpp
@@ -87,24 +87,47 @@ template <>
 void dngv_op<double, psi::DEVICE_CPU>::operator()(const psi::DEVICE_CPU* d,
                                                   const int nstart,
                                                   const int ldh,
-                                                  const std::complex<double>* A,
-                                                  const std::complex<double>* B,
-                                                  double* W,
-                                                  std::complex<double>* V)
+                                                  const std::complex<double>* hcc,
+                                                  const std::complex<double>* scc,
+                                                  double* eigenvalue,
+                                                  std::complex<double>* vcc)
+                                                  
 {
-    int info = 0;
+
     int lwork = 0;
+    psi::DEVICE_CPU * cpu_ctx = {};
 
-    for (int i = 0; i < nstart * ldh; i++)
-    {
-        V[i] = A[i];
-    }
+    ModuleBase::ComplexMatrix sdum(nstart, ldh);
+    ModuleBase::ComplexMatrix hdum;
 
+    ModuleBase::ComplexMatrix hc(nstart, nstart);
+    ModuleBase::ComplexMatrix hvec(nstart, nstart);
+    psi::memory::synchronize_memory_op<std::complex<double>, psi::DEVICE_CPU, psi::DEVICE_CPU>()(
+            cpu_ctx,
+            cpu_ctx,
+            hc.c,
+            hcc,
+            nstart * nstart
+    );
+
+    ModuleBase::ComplexMatrix sc(nstart, nstart);
+    psi::memory::synchronize_memory_op<std::complex<double>, psi::DEVICE_CPU, psi::DEVICE_CPU>()(
+            cpu_ctx,
+            cpu_ctx,
+            sc.c,
+            scc,
+            nstart * nstart
+    );
+    sdum = sc;
+
+    // workspace query
     int nb = LapackConnector::ilaenv(1, "ZHETRD", "U", nstart, -1, -1, -1);
+
     if (nb < 1)
     {
         nb = std::max(1, nstart);
     }
+
     if (nb == 1 || nb >= nstart)
     {
         lwork = 2 * nstart; // mohan modify 2009-08-02
@@ -113,25 +136,75 @@ void dngv_op<double, psi::DEVICE_CPU>::operator()(const psi::DEVICE_CPU* d,
     {
         lwork = (nb + 1) * nstart;
     }
-    std::complex<double>* work = new std::complex<double>[lwork];
-    int rwork_dim = 3 * nstart - 2;
-    double* rwork = new double[rwork_dim];
+
+    std::complex<double> *work = new std::complex<double>[lwork];
     ModuleBase::GlobalFunc::ZEROS(work, lwork);
+
+    //=====================================================================
+    // input s and (see below) h are copied so that they are not destroyed
+    //=====================================================================
+
+    int info = 0;
+    int rwork_dim = 3 * nstart - 2;
+
+    double *rwork = new double[rwork_dim];
     ModuleBase::GlobalFunc::ZEROS(rwork, rwork_dim);
 
+    //===========================
+    // calculate all eigenvalues
+    //===========================
+    hvec = hc;
+    LapackConnector::zhegv(1, 'V', 'U', nstart, hvec, ldh, sdum, ldh, eigenvalue, work, lwork, rwork, info);
 
-    // The A and B storage space is (nstart * ldh), and the data that really participates in the zhegvx
-    // operation is (nstart * nstart). In this function, the data that A and B participate in the operation will
-    // be extracted into the new local variables aux and bux (the internal of the function).
-    // V is the output of the function, the storage space is also (nstart * ldh), and the data size of valid V
-    // obtained by the zhegvx operation is (nstart * nstart) and stored in zux (internal to the function). When
-    // the function is output, the data of zux will be mapped to the corresponding position of V.
-    LapackConnector::zhegv(1, 'V', 'U', nstart, V, nstart, B, nstart, W, work, lwork, rwork, info, ldh);
-
-    assert(0 == info);
-
-    delete[] work;
+    psi::memory::synchronize_memory_op<std::complex<double>, psi::DEVICE_CPU, psi::DEVICE_CPU>()(
+            cpu_ctx,
+            cpu_ctx,
+            vcc,
+            hvec.c,
+            nstart * nstart
+    );
     delete[] rwork;
+    delete[] work;
+    // int info = 0;
+    // int lwork = 0;
+
+    // int nb = LapackConnector::ilaenv(1, "ZHETRD", "U", nstart, -1, -1, -1);
+    // if (nb < 1)
+    // {
+    //     nb = std::max(1, nstart);
+    // }
+    // if (nb == 1 || nb >= nstart)
+    // {
+    //     lwork = 2 * nstart; // mohan modify 2009-08-02
+    // }
+    // else
+    // {
+    //     lwork = (nb + 1) * nstart;
+    // }
+    // std::complex<double>* work = new std::complex<double>[lwork];
+    // int rwork_dim = 3 * nstart - 2;
+    // double* rwork = new double[rwork_dim];
+    // ModuleBase::GlobalFunc::ZEROS(work, lwork);
+    // ModuleBase::GlobalFunc::ZEROS(rwork, rwork_dim);
+
+    // for (int i = 0; i < nstart * ldh; i++)
+    // {
+    //     V[i] = A[i];
+    // }
+
+
+    // // The A and B storage space is (nstart * ldh), and the data that really participates in the zhegvx
+    // // operation is (nstart * nstart). In this function, the data that A and B participate in the operation will
+    // // be extracted into the new local variables aux and bux (the internal of the function).
+    // // V is the output of the function, the storage space is also (nstart * ldh), and the data size of valid V
+    // // obtained by the zhegvx operation is (nstart * nstart) and stored in zux (internal to the function). When
+    // // the function is output, the data of zux will be mapped to the corresponding position of V.
+    // LapackConnector::zhegv(1, 'V', 'U', nstart, V, nstart, B, nstart, W, work, lwork, rwork, info, ldh);
+
+    // assert(0 == info);
+
+    // delete[] work;
+    // delete[] rwork;
 }
 
 } // namespace hsolver
