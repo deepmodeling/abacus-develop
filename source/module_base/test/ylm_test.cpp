ba92ab3aa5b9f313f3e90f699ad047e261562481
SeptemberMy <119288223+SeptemberMy@users.noreply.github.com>
2023-01-12 23:45:17 +0800

    Add files via upload

---
+++ source/module_base/test/ylm_test.cpp
@@ -0,0 +1,33 @@
+#include "../ylm.h"
+#include "gtest/gtest.h"
+/************************************************
+ *  unit test of class ylm
+ ***********************************************/
+
+/**
+ * - Tested Functions:
+ *   - ZEROS
+ *     - set all elements of a double float array to zero
+ * */
+
+class ylmTest : public testing::Test
+{
+	
+};
+
+TEST_F(ylmTest,Zeros)
+{
+    double aaaa[100];
+    ModuleBase::Ylm::ZEROS(aaaa,100); 
+    EXPECT_EQ(aaaa[0],0.0);
+    EXPECT_EQ(aaaa[49],0.0);
+    EXPECT_EQ(aaaa[99],0.0);
+   for(int i = 0; i < 100; i++)
+	{
+        EXPECT_EQ(aaaa[i],0.0);
+	}
+
+}
+
+
+
