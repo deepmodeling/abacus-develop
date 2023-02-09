4eab6988887e9264c7a20815e0839b76807b1a42
wenfei-li <liwenfei@gmail.com>
2023-02-09 21:16:00 +0800

    working on calculate_dH_sparse

--- source/module_hamilt_lcao/hamilt_lcaodft/LCAO_hamilt.cpp
+++ source/module_hamilt_lcao/hamilt_lcaodft/LCAO_hamilt.cpp
@@ -352,15 +352,19 @@ void LCAO_Hamilt::calculate_dH_sparse(const int &current_spin, const double &spa
     this->LM->DHloc_fixedR_y = new double[nnr];
     this->LM->DHloc_fixedR_z = new double[nnr];
 
+    ModuleBase::GlobalFunc::ZEROS(this->LM->DHloc_fixedR_x, this->LM->ParaV->nloc);
+    ModuleBase::GlobalFunc::ZEROS(this->LM->DHloc_fixedR_y, this->LM->ParaV->nloc);
+    ModuleBase::GlobalFunc::ZEROS(this->LM->DHloc_fixedR_z, this->LM->ParaV->nloc);
     // calculate dT=<phi|kin|dphi> in LCAO
     // calculate T + VNL(P1) in LCAO basis
     this->genH.build_ST_new('T', true, GlobalC::ucell, this->LM->Hloc_fixedR.data());
     this->genH.build_Nonlocal_mu_new (this->LM->Hloc_fixed.data(), true);
+    
     calculate_dSTN_R_sparse(current_spin, sparse_threshold);
 
-    delete[] this->LM->DHloc_fixed_x;
-    delete[] this->LM->DHloc_fixed_y;
-    delete[] this->LM->DHloc_fixed_z;
+    delete[] this->LM->DHloc_fixedR_x;
+    delete[] this->LM->DHloc_fixedR_y;
+    delete[] this->LM->DHloc_fixedR_z;
 
     GK.cal_dvlocal_R_sparseMatrix(current_spin, sparse_threshold, this->LM);
 }
@@ -965,17 +969,17 @@ void LCAO_Hamilt::calculate_dSTN_R_sparse(const int &current_spin, const double 
 
                             if(GlobalV::NSPIN!=4)
                             {
-                                temp_value_double = this->LM->DHloc_fixed_x[index];
+                                temp_value_double = this->LM->DHloc_fixedR_x[index];
                                 if (std::abs(temp_value_double) > sparse_threshold)
                                 {
                                     this->LM->dHRx_sparse[current_spin][dR][iw1_all][iw2_all] = temp_value_double;
                                 }
-                                temp_value_double = this->LM->DHloc_fixed_y[index];
+                                temp_value_double = this->LM->DHloc_fixedR_y[index];
                                 if (std::abs(temp_value_double) > sparse_threshold)
                                 {
                                     this->LM->dHRy_sparse[current_spin][dR][iw1_all][iw2_all] = temp_value_double;
                                 }
-                                temp_value_double = this->LM->DHloc_fixed_z[index];
+                                temp_value_double = this->LM->DHloc_fixedR_z[index];
                                 if (std::abs(temp_value_double) > sparse_threshold)
                                 {
                                     this->LM->dHRz_sparse[current_spin][dR][iw1_all][iw2_all] = temp_value_double;
