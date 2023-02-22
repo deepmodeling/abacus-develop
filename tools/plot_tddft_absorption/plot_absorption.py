6ac328935444c276392a2ad1572a60969e8c8732
lyb9812 <liyuanbo9812@pku.edu.cn>
2023-02-22 22:30:36 +0800

    update scripts to plot absorption

--- tools/plot_tddft_absorption/plot_absorption.py
+++ tools/plot_tddft_absorption/plot_absorption.py
@@ -1,43 +1,40 @@
 from re import split
 import numpy as np
 import matplotlib.pyplot as plt
-from ase.units import Bohr
-from math import pi
-from scipy.constants import c
 from os import listdir
 from os import mkdir
-from shutil import move
-from shutil import copyfile
 from os import path
+#Function to plot spectrum
+#t_name: the path of dipolefile
+#out: The pics would be stored in "./out"
+#move,N_t:The range of steps to do the fft
 def Sp_plot(t_name,out,file,move=0,N_t=-1,dt=0.0034,direction=-1):
     Dipole=dipole_read(t_name,out,file,direction=direction)
     if(N_t==-1):
         N_t=Dipole.shape[1]
-        N_t-=move
-    shift_omega_step=1
-    lambda_x=(c/np.linspace(shift_omega_step,N_t-1,N_t-shift_omega_step)*dt*N_t*1e-6)
+    N_t-=move
+    lambda_x=4.13*np.linspace(0,N_t-1,N_t)/(dt*N_t)
     fig,ax=plt.subplots()
     if(direction==1 or direction==-1):
-        alpha_x=np.fft.fft(Dipole[0][move:N_t])
+        alpha_x=np.fft.fft(Dipole[0][move:N_t+move])
         S_x=np.abs(alpha_x.imag)
-        ax.plot(lambda_x,S_x[shift_omega_step:],label='X')
+        ax.plot(lambda_x,S_x[:],label='X')
     if(direction==2 or direction==-1):
-        alpha_y=np.fft.fft(Dipole[1][move:N_t])
+        alpha_y=np.fft.fft(Dipole[1][move:N_t+move])
         S_y=np.abs(alpha_y.imag)
-        ax.plot(lambda_x,S_y[shift_omega_step:],label='Y')
+        ax.plot(lambda_x,S_y[:],label='Y')
     if(direction==3 or direction==-1):
-        alpha_z=np.fft.fft(Dipole[2][move:N_t])
+        alpha_z=np.fft.fft(Dipole[2][move:N_t+move])
         S_z=np.abs(alpha_z.imag)
-        ax.plot(lambda_x,S_x[shift_omega_step:],label='Z') 
+        ax.plot(lambda_x,S_z[:],label='Z')
+    #write peaks
     ax.set_ylabel('Strength')
     ax.set_xlabel('frequency(eV)')
     ax.set_title("Spectra")
-    ax.set_xlim(80,200)
+    ax.set_xlim(0,25)
     plt.legend()
     plt.savefig('./'+out+'/'+file+'spec.png')
-
-def Sp_field_plot():
-    pass
+#function to read in dipole_file, and return 3*nstep 2D array
 def dipole_read(total_name,out,file,dt=0.0034,direction=-1):
     #Initial
     list_d=[[] for i in range(3)]
@@ -47,18 +44,12 @@ def dipole_read(total_name,out,file,dt=0.0034,direction=-1):
     line=1
     with open(total_name,mode='r')as f1:
         while(line!=''):
-            while(m<3):
-                line=f1.readline()
-                if(line==''):break
-                tmp=line.split()
-                if (m==0):
-                    list_d[0].append(float(tmp[1]))
-                elif (m==1):
-                    list_d[1].append(float(tmp[1]))
-                elif (m==2):
-                    list_d[2].append(float(tmp[1]))
-                m+=1
-            m=0
+            line=f1.readline()
+            if(line==''):break
+            tmp=line.split()
+            list_d[0].append(float(tmp[1]))
+            list_d[1].append(float(tmp[2]))
+            list_d[2].append(float(tmp[3]))
     #Plot part
     Dipole=np.array(list_d)
     N_t=Dipole.shape[1]
@@ -76,27 +67,13 @@ def dipole_read(total_name,out,file,dt=0.0034,direction=-1):
     plt.legend()
     plt.savefig('./'+out+'/'+file+'dipole.png')
     return Dipole
-#reading para
-def autoanlysis(filename,dir,out):
-    list_dir=listdir("./"+dir+"/")
-    count=0
-    print("total file: "+str(len(list_dir)))
-    #main cycle
-    for file in list_dir:
-        if(filename in file):
-            count+=1
-            t_name="./"+dir+"/"+file
-            Sp_plot(t_name,out,file)
 
 #main part
-filename='dipole.dat'
-dir='dipole_nomove'
-out='PIC'
-file='C2H2_nomove_X_dipole.dat'
+dir='OUT.ABACUS'                        #The directory of the dipole file
+out='PIC'                               #The path you want to ouput your file, equals to directory "./out"
 if(not path.exists('./'+out)):
     mkdir(out)
-#direction='X'
-#autoanlysis(filename,dir,out)
 #single plot
+file='SPIN1_DIPOLE'                     #filename of dipole file
 t_name="./"+dir+"/"+file
-Sp_plot(t_name,out,file,direction=1)
+Sp_plot(t_name,out,file,dt=0.0034)
