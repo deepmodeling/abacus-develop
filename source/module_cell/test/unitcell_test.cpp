036435ba358af84a6256463c4cf9c40f19a5e7a7
root <hongriTianqi>
2023-02-23 05:51:24 +0000

    add an empty file

---
+++ source/module_cell/test/unitcell_test.cpp
@@ -0,0 +1,15 @@
+#include "gtest/gtest.h"
+
+/************************************************
+ *  unit test of class UnitCell
+ ***********************************************/
+
+/**
+ *
+ *
+ */
+
+class UnitCellTest : public testing::Test
+{
+protected:
+};
