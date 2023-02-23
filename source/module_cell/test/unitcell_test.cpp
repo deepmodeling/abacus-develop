ad338107691570bd826c95db6ce33507b8d25597
root <hongriTianqi>
2023-02-23 07:54:38 +0000

    add unitcell_ut in cmake file

--- source/module_cell/test/unitcell_test.cpp
+++ source/module_cell/test/unitcell_test.cpp
@@ -1,4 +1,5 @@
 #include "gtest/gtest.h"
+#include "module_cell/unitcell.h"
 
 /************************************************
  *  unit test of class UnitCell
@@ -13,3 +14,8 @@ class UnitCellTest : public testing::Test
 {
 protected:
 };
+
+TEST_F(UnitCellTest,TT)
+{
+	EXPECT_EQ(1,1);
+}
