d4cb63f0f698e73d93657cdf79bb6c5d88fc618d
root <hongriTianqi>
2023-02-28 09:39:34 +0000

    fix another typo

--- source/module_cell/unitcell.cpp
+++ source/module_cell/unitcell.cpp
@@ -1757,7 +1757,7 @@ void UnitCell::remake_cell()
 		double celldm13 = (latvec.e11 * latvec.e31 + latvec.e12 * latvec.e32 + latvec.e13 * latvec.e33);
 		double cos13 = celldm13 / celldm1 / celldm3;
 		double celldm23 = (latvec.e21 * latvec.e31 + latvec.e22 * latvec.e32 + latvec.e23 * latvec.e33);
-		double cos23 = celldm23 / celldm1 / celldm3;
+		double cos23 = celldm23 / celldm2 / celldm3;
 
 		double sin12 = std::sqrt(1.0 - cos12 * cos12);
 		if(cos12 >= 1.0)
