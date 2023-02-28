88f70449fb2a92db62c1fe0b068c98283973b0ce
root <pxlppq007@163.com>
2023-02-28 18:17:33 +0800

    fix: return nullptr, if only one atom

--- source/module_hamilt_general/module_vdw/vdw.cpp
+++ source/module_hamilt_general/module_vdw/vdw.cpp
@@ -8,7 +8,12 @@ namespace vdw
 
 std::unique_ptr<Vdw> make_vdw(const UnitCell &ucell, const Input &input)
 {
-    if (input.vdw_method == "d2")
+    if (ucell.nat < 2 && input.vdw_method != "none")
+    {
+        ModuleBase::WARNING("VDW", "Only one atom in this system, and will not do the calculation of VDW");
+        return nullptr;
+    }
+    else if (input.vdw_method == "d2")
     {
         std::unique_ptr<Vdwd2> vdw_ptr = make_unique<Vdwd2>(ucell);
         vdw_ptr->parameter().initial_parameters(input);
