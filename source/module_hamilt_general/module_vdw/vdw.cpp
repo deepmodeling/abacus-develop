f28bd5b0d87d2af2c8cb0337536dbbbd30719b38
root <pxlppq007@163.com>
2023-02-28 13:48:05 +0800

    fix(vdw): use the incoming variable `input` but not global `INPUT` in make_vdw

--- source/module_hamilt_general/module_vdw/vdw.cpp
+++ source/module_hamilt_general/module_vdw/vdw.cpp
@@ -8,14 +8,14 @@ namespace vdw
 
 std::unique_ptr<Vdw> make_vdw(const UnitCell &ucell, const Input &input)
 {
-    if (INPUT.vdw_method == "d2")
+    if (input.vdw_method == "d2")
     {
         std::unique_ptr<Vdwd2> vdw_ptr = make_unique<Vdwd2>(ucell);
         vdw_ptr->parameter().initial_parameters(input);
         vdw_ptr->parameter().initset(ucell);
         return vdw_ptr;
     }
-    else if (INPUT.vdw_method == "d3_0" || INPUT.vdw_method == "d3_bj")
+    else if (input.vdw_method == "d3_0" || input.vdw_method == "d3_bj")
     {
         std::unique_ptr<Vdwd3> vdw_ptr = make_unique<Vdwd3>(ucell);
         vdw_ptr->parameter().initial_parameters(input);
