73509299f3a714d6679620cae810f50337fef07f
root <hongriTianqi>
2023-03-03 04:47:32 +0000

    Test: add ut for unitcell

--- source/module_cell/test/unitcell_test_p.cpp
+++ source/module_cell/test/unitcell_test_p.cpp
@@ -2,14 +2,23 @@
 #include "gmock/gmock.h"
 #include "memory"
 #include "module_base/mathzone.h"
+#include "module_base/global_variable.h"
 #include "module_cell/unitcell.h"
 #include<vector>
 #include<valarray>
 
 InfoNonlocal::InfoNonlocal(){}
 InfoNonlocal::~InfoNonlocal(){}
-Magnetism::Magnetism(){}
-Magnetism::~Magnetism(){}
+Magnetism::Magnetism()
+{
+	this->tot_magnetization = 0.0;
+	this->abs_magnetization = 0.0;
+	this->start_magnetization = nullptr;
+}
+Magnetism::~Magnetism()
+{
+	delete[] this->start_magnetization;
+}
 
 /************************************************
  *  unit test of class UnitCell
@@ -40,10 +49,23 @@ Magnetism::~Magnetism(){}
  *   - PeriodicBoundaryAdjustment
  *     - periodic_boundary_adjustment(): move atoms inside the unitcell after relaxation
  *   - PrintCell
- *     - print_cell(ofs): print basic info into ofs
+ *     - print_cell(ofs): print basic cell info into ofs
  *   - PrintCellCif
  *     - print_cell_cif(fn): print cif file (named as fn)
- *
+ *   - PrintSTRU
+ *     - print_stru_file(): print STRU file of ABACUS
+ *   - PrintTau
+ *     - print_tau(): print atomic coordinates, magmom and initial velocities
+ *   - PrintUnitcellPseudo
+ *     - Actually an integrated function to call UnitCell::print_cell and Atom::print_Atom
+ *   - UpdatePosTau1
+ *     - update_pos_tau(const double* pos)
+ *   - UpdatePosTau2
+ *     - update_pos_tau(const ModuleBase::Vector3<double>* posd_in)
+ *   - UpdatePosTaud
+ *     - update_pos_taud(double* posd_in)
+ *   - UpdateVel
+ *     - update_vel(const ModuleBase::Vector3<double>* vel_in)
  */
 
 //mock function
@@ -74,6 +96,7 @@ public:
 			std::vector<double> atomic_mass_in,
 			std::string coor_type_in,
 			std::valarray<double> coordinates_in,
+			std::valarray<double> mbl_in = {0},
 			std::valarray<double> velocity_in = {0}
 			):
 		system_name(system_name_in),
@@ -93,7 +116,8 @@ public:
 		atomic_mass(atomic_mass_in),
 		coor_type(coor_type_in),
 		coordinates(coordinates_in),
-		velocity(velocity_in)
+		mbl(mbl_in),
+		velocity(velocity_in) // velocity assume the existence of mbl in print_stru_file()
 	{}
 	std::string system_name;
 	std::string latname;
@@ -112,6 +136,7 @@ public:
 	std::vector<double> atomic_mass;
 	std::string coor_type;
 	std::valarray<double> coordinates;
+	std::valarray<double> mbl;
 	std::valarray<double> velocity;
 	// ntype
 	int ntype;
@@ -132,11 +157,13 @@ public:
 		delete[] ucell->pseudo_fn;
 		delete[] ucell->pseudo_type;
 		delete[] ucell->orbital_fn;
+		delete[] ucell->magnet.start_magnetization; //mag set here
 		ucell->atom_label = new std::string[ucell->ntype];
 		ucell->atom_mass = new double[ucell->ntype];
 		ucell->pseudo_fn = new std::string[ucell->ntype];
 		ucell->pseudo_type = new std::string[ucell->ntype];
 		ucell->orbital_fn = new std::string[ucell->ntype];
+		ucell->magnet.start_magnetization = new double[ucell->ntype]; //mag set here
 		for(int it=0;it<ucell->ntype;++it)
 		{
 			ucell->atom_label[it] = this->elements[it];
@@ -144,6 +171,7 @@ public:
 			ucell->pseudo_fn[it] = this->pp_files[it];
 			ucell->pseudo_type[it] = this->pp_types[it];
 			ucell->orbital_fn[it] = this->orb_files[it];
+			ucell->magnet.start_magnetization[it] = 0.0; //mag set here
 		}
 		//lattice info
 		ucell->lat0 = this->lat0;
@@ -210,6 +238,7 @@ public:
 			ucell->atoms[it].angle2 = new double[ucell->atoms[it].na];
 			ucell->atoms[it].m_loc_ = new ModuleBase::Vector3<double>[ucell->atoms[it].na];
 			ucell->atoms[it].mbl = new ModuleBase::Vector3<int>[ucell->atoms[it].na];
+			ucell->atoms[it].mass = ucell->atom_mass[it]; // mass set here
 			for(int ia=0; ia<ucell->atoms[it].na; ++ia)
 			{
 				if (ucell->Coordinate == "Direct")
@@ -232,15 +261,24 @@ public:
 						ucell->atoms[it].taud[ia].x, ucell->atoms[it].taud[ia].y, ucell->atoms[it].taud[ia].z);
 				}
 				ucell->atoms[it].tau_original[ia] = ucell->atoms[it].tau[ia];
-				ucell->atoms[it].vel[ia].set(0,0,0);
+				if(this->init_vel)
+				{
+					ucell->atoms[it].vel[ia].x = this->velocity[this->atomic_index*3+0];
+					ucell->atoms[it].vel[ia].y = this->velocity[this->atomic_index*3+1];
+					ucell->atoms[it].vel[ia].z = this->velocity[this->atomic_index*3+2];
+				}
+				else
+				{
+					ucell->atoms[it].vel[ia].set(0,0,0);
+				}
 				ucell->atoms[it].m_loc_[ia].set(0,0,0);
 				ucell->atoms[it].angle1[ia] = 0;
 				ucell->atoms[it].angle2[ia] = 0;
 				if(this->selective_dynamics)
 				{
-					ucell->atoms[it].mbl[ia].x = this->velocity[this->atomic_index*3+0];
-					ucell->atoms[it].mbl[ia].y = this->velocity[this->atomic_index*3+1];
-					ucell->atoms[it].mbl[ia].z = this->velocity[this->atomic_index*3+2];
+					ucell->atoms[it].mbl[ia].x = this->mbl[this->atomic_index*3+0];
+					ucell->atoms[it].mbl[ia].y = this->mbl[this->atomic_index*3+1];
+					ucell->atoms[it].mbl[ia].z = this->mbl[this->atomic_index*3+2];
 				}
 				else
 				{
@@ -492,6 +530,35 @@ TEST_P(UcellTest,PrintCell)
 	}
 }
 
+TEST_P(UcellTest,PrintUnitcellPseudo)
+{
+	UcellTestPrepare utp = GetParam();
+	std::unique_ptr<UnitCell> ucell;
+	GlobalV::relax_new = utp.relax_new;
+	ucell = utp.SetUcellInfo();
+	if(utp.system_name == "C1H2-Index")
+	{
+		GlobalV::test_pseudo_cell = 1;
+		std::string fn="printcell.log";
+		ucell->print_unitcell_pseudo(fn);
+		std::ifstream ifs;
+    		ifs.open("printcell.log");
+    		std::string str((std::istreambuf_iterator<char>(ifs)),std::istreambuf_iterator<char>());
+    		EXPECT_THAT(str, testing::HasSubstr("latName = bcc"));
+    		EXPECT_THAT(str, testing::HasSubstr("ntype = 2"));
+    		EXPECT_THAT(str, testing::HasSubstr("nat = 3"));
+    		EXPECT_THAT(str, testing::HasSubstr("GGT :"));
+    		EXPECT_THAT(str, testing::HasSubstr("omega = 6748.33"));
+    		EXPECT_THAT(str, testing::HasSubstr("label = C"));
+    		EXPECT_THAT(str, testing::HasSubstr("mass = 12"));
+    		EXPECT_THAT(str, testing::HasSubstr("atom_position(cartesian) Dimension = 1"));
+    		EXPECT_THAT(str, testing::HasSubstr("label = H"));
+    		EXPECT_THAT(str, testing::HasSubstr("mass = 1"));
+    		EXPECT_THAT(str, testing::HasSubstr("atom_position(cartesian) Dimension = 2"));
+		remove("printcell.log");
+	}
+}
+
 TEST_P(UcellTest,PrintCellCif)
 {
 	UcellTestPrepare utp = GetParam();
@@ -513,12 +580,173 @@ TEST_P(UcellTest,PrintCellCif)
 	}
 }
 
+TEST_P(UcellTest,PrintSTRU)
+{
+	UcellTestPrepare utp = GetParam();
+	std::unique_ptr<UnitCell> ucell;
+	GlobalV::relax_new = utp.relax_new;
+	ucell = utp.SetUcellInfo();
+	if(utp.system_name == "C1H2-Index")
+	{
+		//Cartesian type of coordinates
+		std::string fn = "C1H2_STRU";
+		int type = 1; // for Cartesian
+		int level = 1; //print velocity in STRU
+		ucell->print_stru_file(fn,type,level);
+		std::ifstream ifs;
+		ifs.open("C1H2_STRU");
+    		std::string str((std::istreambuf_iterator<char>(ifs)),std::istreambuf_iterator<char>());
+    		EXPECT_THAT(str, testing::HasSubstr("C 12 C.upf upf201"));
+    		EXPECT_THAT(str, testing::HasSubstr("H 1 H.upf upf201"));
+    		EXPECT_THAT(str, testing::HasSubstr("Cartesian"));
+    		EXPECT_THAT(str, testing::HasSubstr("H #label"));
+    		EXPECT_THAT(str, testing::HasSubstr("0 #magnetism"));
+    		EXPECT_THAT(str, testing::HasSubstr("2 #number of atoms"));
+    		EXPECT_THAT(str, testing::HasSubstr("1.5  1.5  1.5  m  0  0  0  v  0.1  0.1  0.1"));
+    		EXPECT_THAT(str, testing::HasSubstr("0.5  0.5  0.5  m  0  0  1  v  0.1  0.1  0.1"));
+		str.clear();
+		ifs.close();
+		remove("C1H2_STRU");
+		//direct type of coordinates
+		type = 2; //for direct
+		ucell->print_stru_file(fn,type,level);
+		ifs.open("C1H2_STRU");
+		str= {(std::istreambuf_iterator<char>(ifs)),std::istreambuf_iterator<char>()};
+    		EXPECT_THAT(str, testing::HasSubstr("C 12 C.upf upf201"));
+    		EXPECT_THAT(str, testing::HasSubstr("H 1 H.upf upf201"));
+    		EXPECT_THAT(str, testing::HasSubstr("Direct"));
+    		EXPECT_THAT(str, testing::HasSubstr("H #label"));
+    		EXPECT_THAT(str, testing::HasSubstr("0 #magnetism"));
+    		EXPECT_THAT(str, testing::HasSubstr("2 #number of atoms"));
+    		EXPECT_THAT(str, testing::HasSubstr("0.15  0.15  0.15  m  0  0  0  v  0.1  0.1  0.1"));
+    		EXPECT_THAT(str, testing::HasSubstr("0.05  0.05  0.05  m  0  0  1  v  0.1  0.1  0.1"));
+		ifs.close();
+		remove("C1H2_STRU");
+	}
+}
+
+TEST_P(UcellTest,PrintTau)
+{
+	UcellTestPrepare utp = GetParam();
+	std::unique_ptr<UnitCell> ucell;
+	GlobalV::relax_new = utp.relax_new;
+	ucell = utp.SetUcellInfo();
+	if(utp.system_name == "C1H2-Index")
+	{
+		GlobalV::ofs_running.open("print_tau_direct");
+		EXPECT_EQ(ucell->Coordinate,"Direct");
+		ucell->print_tau();
+		GlobalV::ofs_running.close();
+		std::ifstream ifs;
+		ifs.open("print_tau_direct");
+    		std::string str((std::istreambuf_iterator<char>(ifs)),std::istreambuf_iterator<char>());
+    		EXPECT_THAT(str, testing::HasSubstr("DIRECT COORDINATES"));
+    		EXPECT_THAT(str, testing::HasSubstr("taud_C1                 0.1                 0.1                 0.1"));
+		ifs.close();
+		remove("print_tau_direct");
+	}
+	else if(utp.system_name == "C1H2-Cartesian")
+	{
+		GlobalV::ofs_running.open("print_tau_Cartesian");
+		EXPECT_EQ(ucell->Coordinate,"Cartesian");
+		ucell->print_tau();
+		GlobalV::ofs_running.close();
+		std::ifstream ifs;
+		ifs.open("print_tau_Cartesian");
+    		std::string str((std::istreambuf_iterator<char>(ifs)),std::istreambuf_iterator<char>());
+    		EXPECT_THAT(str, testing::HasSubstr("CARTESIAN COORDINATES"));
+    		EXPECT_THAT(str, testing::HasSubstr("tauc_C1                   1                   1                   1"));
+		ifs.close();
+		remove("print_tau_Cartesian");
+	}
+}
+
+TEST_P(UcellTest,UpdateVel)
+{
+	UcellTestPrepare utp = GetParam();
+	std::unique_ptr<UnitCell> ucell;
+	GlobalV::relax_new = utp.relax_new;
+	ucell = utp.SetUcellInfo();
+	if(utp.system_name == "C1H2-Index")
+	{
+		ModuleBase::Vector3<double>* vel_in = new ModuleBase::Vector3<double>[ucell->nat];
+		for(int iat=0; iat<ucell->nat; ++iat)
+		{
+			vel_in[iat].set(iat*0.1,iat*0.1,iat*0.1);
+		}
+		ucell->update_vel(vel_in);
+		for(int iat=0; iat<ucell->nat; ++iat)
+		{
+			EXPECT_DOUBLE_EQ(vel_in[iat].x,0.1*iat);
+			EXPECT_DOUBLE_EQ(vel_in[iat].y,0.1*iat);
+			EXPECT_DOUBLE_EQ(vel_in[iat].z,0.1*iat);
+		}
+		delete[] vel_in;
+	}
+}
+
+TEST_P(UcellTest,UpdatePosTau1)
+{
+	UcellTestPrepare utp = GetParam();
+	std::unique_ptr<UnitCell> ucell;
+	GlobalV::relax_new = utp.relax_new;
+	ucell = utp.SetUcellInfo();
+	if(utp.system_name == "C1H2-Index")
+	{
+		double* pos_in = new double[ucell->nat*3];
+		ucell->set_iat2itia();
+		for(int iat=0; iat<ucell->nat; ++iat)
+		{
+			pos_in[iat*3] = 0.01*ucell->lat0;
+			pos_in[iat*3+1] = 0.01*ucell->lat0;
+			pos_in[iat*3+2] = 0.01*ucell->lat0;
+		}
+		ucell->update_pos_tau(pos_in);
+		for(int iat=0; iat<ucell->nat; ++iat)
+		{
+			int it, ia;
+			ucell->iat2iait(iat,&ia,&it);
+			if(ucell->atoms[it].mbl[ia].x != 0) EXPECT_DOUBLE_EQ(ucell->atoms[it].tau[ia].x,0.01);
+			if(ucell->atoms[it].mbl[ia].y != 0) EXPECT_DOUBLE_EQ(ucell->atoms[it].tau[ia].y,0.01);
+			if(ucell->atoms[it].mbl[ia].z != 0) EXPECT_DOUBLE_EQ(ucell->atoms[it].tau[ia].z,0.01);
+		}
+		delete[] pos_in;
+	}
+}
+
+TEST_P(UcellTest,UpdatePosTau2)
+{
+	UcellTestPrepare utp = GetParam();
+	std::unique_ptr<UnitCell> ucell;
+	GlobalV::relax_new = utp.relax_new;
+	ucell = utp.SetUcellInfo();
+	if(utp.system_name == "C1H2-Index")
+	{
+		ModuleBase::Vector3<double>* pos_in = new ModuleBase::Vector3<double>[ucell->nat];
+		ucell->set_iat2itia();
+		for(int iat=0; iat<ucell->nat; ++iat)
+		{
+			pos_in[iat].set(0.01*ucell->lat0,0.01*ucell->lat0,0.01*ucell->lat0);
+		}
+		ucell->update_pos_tau(pos_in);
+		for(int iat=0; iat<ucell->nat; ++iat)
+		{
+			int it, ia;
+			ucell->iat2iait(iat,&ia,&it);
+			if(ucell->atoms[it].mbl[ia].x != 0) EXPECT_DOUBLE_EQ(ucell->atoms[it].tau[ia].x,0.01);
+			if(ucell->atoms[it].mbl[ia].y != 0) EXPECT_DOUBLE_EQ(ucell->atoms[it].tau[ia].y,0.01);
+			if(ucell->atoms[it].mbl[ia].z != 0) EXPECT_DOUBLE_EQ(ucell->atoms[it].tau[ia].z,0.01);
+		}
+		delete[] pos_in;
+	}
+}
+
 INSTANTIATE_TEST_SUITE_P(UcellForTest,UcellTest,::testing::Values(
 			UcellTestPrepare("C1H2-Index",	//system-name
 				"bcc",		//latname
 				2,		//lmaxmax
-				false,		//init_vel
-				false,		//selective_dyanmics
+				true,		//init_vel
+				true,		//selective_dyanmics
 				true,		//relax_new
 				"volume",	//fixed_axes
 				1.8897261254578281, //lat0
@@ -534,7 +762,40 @@ INSTANTIATE_TEST_SUITE_P(UcellForTest,UcellTest,::testing::Values(
 				"Direct",	//coordination type
 				{0.1,0.1,0.1,	//atomic coordinates
 				 0.15,0.15,0.15,
-				 0.05,0.05,0.05}),
+				 0.05,0.05,0.05},
+				{1,1,1,	//if atom can move: mbl
+				 0,0,0,
+				 0,0,1},
+				{0.1,0.1,0.1,	//velocity: vel
+				 0.1,0.1,0.1,
+				 0.1,0.1,0.1}),
+			UcellTestPrepare("C1H2-Cartesian",	//system-name
+				"bcc",		//latname
+				2,		//lmaxmax
+				true,		//init_vel
+				true,		//selective_dyanmics
+				true,		//relax_new
+				"volume",	//fixed_axes
+				1.8897261254578281, //lat0
+				{10.0,0.0,0.0,	//latvec
+				 0.0,10.0,0.0,
+				 0.0,0.0,10.0},
+				{"C","H"},	//elements
+				{"C.upf","H.upf"},	//upf file
+				{"upf201","upf201"},	//upf types
+				{"C.orb","H.orb"},	//orb file
+				{1,2},		//number of each elements
+				{12.0,1.0},	//atomic mass
+				"Cartesian",	//coordination type
+				{1,1,1,	//atomic coordinates
+				 1.5,1.5,1.5,
+				 0.5,0.5,0.5},
+				{1,1,1,	//if atom can move: mbl
+				 0,0,0,
+				 0,0,1},
+				{0.1,0.1,0.1,	//velocity: vel
+				 0.1,0.1,0.1,
+				 0.1,0.1,0.1}),
 			UcellTestPrepare("C1H2-CheckDTau",	//system-name
 				"bcc",		//latname
 				2,		//lmaxmax
@@ -600,7 +861,10 @@ INSTANTIATE_TEST_SUITE_P(UcellForTest,UcellTest,::testing::Values(
 				 0.05,0.05,0.05},
 				{1,1,1,	//if atom can move
 				 0,0,0,
-				 0,0,1}),
+				 0,0,1},
+				{0.1,0.1,0.1,	//velocity
+				 0.1,0.1,0.1,
+				 0.1,0.1,0.1}),
 			UcellTestPrepare("C1H2-PBA",	//system-name
 				"bcc",		//latname
 				2,		//lmaxmax
