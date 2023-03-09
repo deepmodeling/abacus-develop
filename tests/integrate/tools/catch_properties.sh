add35ae73bef434f1f65ceae295b545324d9d819
sunliang98 <1700011430@pku.edu.cn>
2023-03-09 14:44:10 +0800

    <Test> Remove BAND_CHG_H2 from catch_properties.sh.

--- tests/integrate/tools/catch_properties.sh
+++ tests/integrate/tools/catch_properties.sh
@@ -339,18 +339,18 @@ fi
 
 if [ $calculation == "istate" ]; then
 	nfile=0
-	chgfiles=`ls OUT.autotest/ | grep -E '_CHG$'`
-	if test -z "$chgfiles"; then
-		echo "Can't find BAND_CHG files"
-		exit 1
-	else
-		for chg in $chgfiles;
-		do
-			total_chg=`../tools/sum_BAND_CHG_H2 OUT.autotest/$chg`
-			nfile=$(($nfile+1))
-			echo "nelec$nfile $total_chg" >>$1
-		done
-	fi
+	# chgfiles=`ls OUT.autotest/ | grep -E '_CHG$'`
+	# if test -z "$chgfiles"; then
+	# 	echo "Can't find BAND_CHG files"
+	# 	exit 1
+	# else
+	# 	for chg in $chgfiles;
+	# 	do
+	# 		total_chg=`../tools/sum_BAND_CHG_H2 OUT.autotest/$chg`
+	# 		nfile=$(($nfile+1))
+	# 		echo "nelec$nfile $total_chg" >>$1
+	# 	done
+	# fi
 	cubefiles=`ls OUT.autotest/ | grep -E '.cube$'`
 	if test -z "$cubefiles"; then
 		echo "Can't find BAND_CHG files"
