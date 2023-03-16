e78a492c689b28f8c3f1fbcf895d20f740b81331
wenfei-li <liwenfei@gmail.com>
2023-03-16 19:51:22 +0800

    test : prepare for relax

---
+++ source/module_relax/relax_new/test/relax_test.cpp
@@ -0,0 +1,19 @@
+#include "gtest/gtest.h"
+#include "../relax.h"
+#include "module_cell/unitcell.h"
+#include "module_io/input.h"
+#include "relax_test.h"
+
+class Test_RL : public testing::Test
+{
+    protected:
+
+    void SetUp()
+    {
+    }
+};
+
+TEST_F(Test_RL, relax_new)
+{
+
+}
