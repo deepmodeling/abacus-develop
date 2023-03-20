c999ce1d8a6ab4066b107bb9cde5a364e0628b02
root <hongriTianqi>
2023-03-20 09:59:19 +0000

    add ut for write_rho

--- source/module_io/test_serial/rho_io_test.cpp
+++ source/module_io/test_serial/rho_io_test.cpp
@@ -1,4 +1,5 @@
 #include "gtest/gtest.h"
+#include "gmock/gmock.h"
 #include "module_io/rho_io.h"
 #include "module_base/global_variable.h"
 #include "prepare_unitcell.h"
@@ -23,7 +24,7 @@ Magnetism::~Magnetism(){}
  *     - the serial version without MPI
  */
 
-class ReadRhoTest : public ::testing::Test
+class RhoIOTest : public ::testing::Test
 {
 protected:
 	int nspin = 1;
@@ -51,7 +52,7 @@ protected:
 	}
 };
 
-TEST_F(ReadRhoTest,Read)
+TEST_F(RhoIOTest,Read)
 {
 	int is = 0;
 	std::string fn = "./support/SPIN1_CHG.cube";
@@ -65,4 +66,32 @@ TEST_F(ReadRhoTest,Read)
 	EXPECT_DOUBLE_EQ(ef,0.461002);
 	EXPECT_DOUBLE_EQ(rho[0][0],1.27020863940e-03);
 	EXPECT_DOUBLE_EQ(rho[0][46655],1.33581335706e-02);
+}
+
+TEST_F(RhoIOTest,Write)
+{
+	int is = 0;
+	std::string fn = "./support/SPIN1_CHG.cube";
+	int nx = 36;
+	int ny = 36;
+	int nz = 36;
+	double ef;
+	UcellTestPrepare utp = UcellTestLib["Si"];
+	ucell = utp.SetUcellInfo();
+	//first read
+	ModuleIO::read_rho(is,nspin,fn,rho[is],nx,ny,nz,ef,ucell,prenspin);
+	EXPECT_DOUBLE_EQ(ef,0.461002);
+	EXPECT_DOUBLE_EQ(rho[0][0],1.27020863940e-03);
+	EXPECT_DOUBLE_EQ(rho[0][46655],1.33581335706e-02);
+	//then write
+	std::string ssc = "SPIN1_CHG.cube";
+	GlobalV::MY_RANK = 0;
+	GlobalV::out_chg = 1;
+	ModuleIO::write_rho(rho[is],is,nspin,0,ssc,nx,ny,nz,ef,ucell);
+	std::ifstream ifs;
+	ifs.open("SPIN1_CHG.cube");
+	std::string str((std::istreambuf_iterator<char>(ifs)),std::istreambuf_iterator<char>());
+	EXPECT_THAT(str, testing::HasSubstr("1 (nspin) 0.461002 (fermi energy, in Ry)"));
+	ifs.close();
+	remove("SPIN1_CHG.cube");
 }
