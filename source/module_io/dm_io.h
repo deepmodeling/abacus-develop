a1756f7f789380e3ddc2ca03e433adcd20e5c6d0
root <hongriTianqi>
2023-03-28 02:41:28 +0000

    remove GlobalC dependence in read_dm

--- source/module_io/dm_io.h
+++ source/module_io/dm_io.h
@@ -7,15 +7,25 @@
 namespace ModuleIO
 {
 	
-void read_dm(const int &is, const std::string &fn, double*** DM, double** DM_R);
+void read_dm(
+#ifdef __MPI
+	const int nnrg,
+	const int* trace_lo,
+#endif
+	const int &is,
+	const std::string &fn,
+	double*** DM,
+	double** DM_R,
+	double& ef,
+	const UnitCell* ucell);
 
 void write_dm(
 #ifdef __MPI
 	const int* trace_lo,
 #endif
-	const int &is, 
-	const int &iter, 
-	const std::string &fn, 
+	const int &is,
+	const int &iter,
+	const std::string &fn,
 	const int &precision,
 	const int &out_dm,
 	double*** DM,
