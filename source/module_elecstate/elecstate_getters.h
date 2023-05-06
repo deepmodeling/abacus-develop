2c3f1ac0c6cef1682a164aeb675d200a6738b074
root <hongriTianqi>
2023-05-06 03:08:51 +0000

    add annotations for elecstate getters

--- source/module_elecstate/elecstate_getters.h
+++ source/module_elecstate/elecstate_getters.h
@@ -1,8 +1,13 @@
+// Date: 2021/4/10
+// Description: Getters for elecstate module
 namespace elecstate
 {
 
+// get the value of GlobalC::rhopw->nrxx, need to be refactored in the future
 const int get_rhopw_nrxx();
+// get the value of GlobalC::rhopw->nxyz, need to be refactored in the future
 const int get_rhopw_nxyz();
+// get the value of GlobalC::ucell.omega, need to be refactored in the future
 const double get_ucell_omega();
 
 }
