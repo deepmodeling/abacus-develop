4d490303dbec73d7e4e533c064d5e34eaf285e7d
root <hongriTianqi>
2023-05-10 02:16:15 +0000

    remove GlobalC::kv in istate_envelop.cpp

--- source/module_esolver/esolver_ks_lcao_elec.cpp
+++ source/module_esolver/esolver_ks_lcao_elec.cpp
@@ -397,9 +397,14 @@ namespace ModuleESolver
         {
             IState_Envelope IEP(this->pelec);
             if (GlobalV::GAMMA_ONLY_LOCAL)
-                IEP.begin(this->psid, this->LOWF, this->UHM.GG, INPUT.out_wfc_pw, GlobalC::wf.out_wfc_r);
+                IEP.begin(this->psid,
+                          this->LOWF,
+                          this->UHM.GG,
+                          INPUT.out_wfc_pw,
+                          GlobalC::wf.out_wfc_r,
+                          &(GlobalC::kv));
             else
-                IEP.begin(this->psi, this->LOWF, this->UHM.GK, INPUT.out_wfc_pw, GlobalC::wf.out_wfc_r);
+                IEP.begin(this->psi, this->LOWF, this->UHM.GK, INPUT.out_wfc_pw, GlobalC::wf.out_wfc_r, &(GlobalC::kv));
         }
         else
         {
