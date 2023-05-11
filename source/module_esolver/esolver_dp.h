77ea5e68d60cbc00a38f4f8172c6a33580f58e7f
YuLiu98 <liuyu@stu.pku.edu.cn>
2023-05-11 19:23:35 +0800

    Feature: enable auto match type map

--- source/module_esolver/esolver_dp.h
+++ source/module_esolver/esolver_dp.h
@@ -13,43 +13,59 @@
 namespace ModuleESolver
 {
 
-    class ESolver_DP : public ESolver
-    {
-    public:
+class ESolver_DP : public ESolver
+{
+  public:
 #ifdef __DPMD
-        ESolver_DP(std::string pot_file) : dp(pot_file)
-        {
-            classname = "ESolver_DP";
-        }
+    ESolver_DP(const std::string& pot_file) : dp(pot_file)
+    {
+        classname = "ESolver_DP";
+        dp_file = pot_file;
+    }
 #else
-        ESolver_DP(std::string pot_file)
-        {
-            classname = "ESolver_DP";
-        }
+    ESolver_DP(const std::string& pot_file)
+    {
+        classname = "ESolver_DP";
+        dp_file = pot_file;
+    }
 #endif
 
-        void Init(Input& inp, UnitCell& cell) override;
-        void Run(const int istep, UnitCell& cell) override;
-        void cal_Energy(double& etot) override;
-        void cal_Force(ModuleBase::matrix& force) override;
-        void cal_Stress(ModuleBase::matrix& stress) override;
-        void postprocess() override;
+    void Init(Input& inp, UnitCell& cell) override;
+    void Run(const int istep, UnitCell& cell) override;
+    void cal_Energy(double& etot) override;
+    void cal_Force(ModuleBase::matrix& force) override;
+    void cal_Stress(ModuleBase::matrix& stress) override;
+    void postprocess() override;
 
-        //--------------temporary----------------------------
+    /**
+     * @brief determine the type map of DP model
+     *
+     * @param ucell unitcell information
+     * @return true if find keyword "type_map" in DP model
+     * @return false if not find keyword "type_map" in DP model
+     */
+    bool type_map(const UnitCell& ucell);
+
+    //--------------temporary----------------------------
 #ifdef __DPMD
 #ifdef __DPMDC
-        deepmd::hpp::DeepPot dp;
+    deepmd::hpp::DeepPot dp;
 #else
-        deepmd::DeepPot dp;
+    deepmd::DeepPot dp;
 #endif
 #endif
-        std::vector<double> cell;
-        std::vector<int> atype;
-        std::vector<double> coord;
-        double dp_potential;
-        ModuleBase::matrix dp_force;
-        ModuleBase::matrix dp_virial;
-        //---------------------------------------------------
-    };
-}
+
+    std::string dp_file;      ///< the directory of DP model file
+    std::vector<int> dp_type; ///< convert atom type to dp type if find type_map
+    std::vector<double> cell;
+    std::vector<int> atype;
+    std::vector<double> coord;
+    double dp_potential;
+    ModuleBase::matrix dp_force;
+    ModuleBase::matrix dp_virial;
+    //---------------------------------------------------
+};
+
+} // namespace ModuleESolver
+
 #endif
