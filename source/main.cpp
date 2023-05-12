fbdf8ae5bb5fc62c7b89455d5ac6f25f4e71c31c
root <pxlppq007@163.com>
2023-05-12 16:24:45 +0800

    add VERSION file --version args, and print version and commit in log

--- source/main.cpp
+++ source/main.cpp
@@ -5,9 +5,21 @@
 #include "driver.h"
 #include "module_base/parallel_global.h"
 #include <ctime>
+#include <iostream>
 
 int main(int argc, char **argv)
 {
+    if (argc > 1 && std::string(argv[1]) == "--version")
+    {
+#ifdef VERSION
+        const char* version = VERSION;
+#else
+        const char* version = "unknown";
+#endif
+        std::cout << "ABACUS version " << version << std::endl;
+        std::exit(0);
+    }
+
     Parallel_Global::read_mpi_parameters(argc,argv);
 	// main program for doing electronic structure calculations
 	//----------------------------------------------------------
