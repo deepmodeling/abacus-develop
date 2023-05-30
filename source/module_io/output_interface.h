2c504c2988358283a50fe66f3b2e8885f797e744
root <hongriTianqi>
2023-05-30 02:47:49 +0000

    simplify output_interface

--- source/module_io/output_interface.h
+++ source/module_io/output_interface.h
@@ -1,8 +1,6 @@
 #ifndef OUTPUT_INTERFACE_H
 #define OUTPUT_INTERFACE_H
 
-#include <string>
-
 namespace ModuleIO
 {
 class Output_Interface
@@ -11,13 +9,7 @@ class Output_Interface
     virtual ~Output_Interface()
     {
     }
-    virtual void setFilename(const std::string& fn);
-    virtual void setPrecision(const int precision);
     virtual void write() = 0;
-
-  public:
-    std::string _fn;
-    int _precision;
 };
 } // namespace ModuleIO
 
