b0bdabaefd62b04c17fd5d04b9f323e105663277
Satinelamp <simens1187@gmail.com>
2023-06-29 01:55:16 +0800

    fix vdw

--- source/module_hamilt_general/module_vdw/vdw.cpp
+++ source/module_hamilt_general/module_vdw/vdw.cpp
@@ -15,14 +15,14 @@ std::unique_ptr<Vdw> make_vdw(const UnitCell &ucell, const Input &input)
     }
     else if (input.vdw_method == "d2")
     {
-        std::unique_ptr<Vdwd2> vdw_ptr = std::make_unique<Vdwd2>(ucell);
+        std::unique_ptr<Vdwd2> vdw_ptr = make_unique<Vdwd2>(ucell);
         vdw_ptr->parameter().initial_parameters(input);
         vdw_ptr->parameter().initset(ucell);
         return vdw_ptr;
     }
     else if (input.vdw_method == "d3_0" || input.vdw_method == "d3_bj")
     {
-        std::unique_ptr<Vdwd3> vdw_ptr = std::make_unique<Vdwd3>(ucell);
+        std::unique_ptr<Vdwd3> vdw_ptr = make_unique<Vdwd3>(ucell);
         vdw_ptr->parameter().initial_parameters(input);
         return vdw_ptr;
     }
