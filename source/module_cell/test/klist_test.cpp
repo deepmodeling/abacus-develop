a0c6cc1f66cd24ba0ae2d868f3d9cbfa2bc37fc0
hongriTianqi <z.hao.1@163.com>
2023-08-09 09:40:47 +0800

    fix klist_test.cpp

--- source/module_cell/test/klist_test.cpp
+++ source/module_cell/test/klist_test.cpp
@@ -770,3 +770,22 @@ TEST_F(KlistTest, IbzKpointIsMP)
 }
 
 #undef private
+
+int main(int argc, char **argv)
+{
+	MPI_Init(&argc, &argv);
+	testing::InitGoogleTest(&argc, argv);
+
+	int nproc;
+	int my_rank;
+	
+	MPI_Comm_size(MPI_COMM_WORLD,&nproc);
+	MPI_Comm_rank(MPI_COMM_WORLD,&my_rank);
+	//========================================
+	
+	int result = RUN_ALL_TESTS();
+	
+	MPI_Finalize();
+	
+	return result;
+}
