82769c7606012e53d01897e8a6dd0cb94ac8bb94
weiqingzhou <weiqingzhou@whu.edu.cn>
2023-08-10 12:26:11 +0800

    use mock to replace some links

--- source/module_elecstate/module_dm/test/test_dmk_io.cpp
+++ source/module_elecstate/module_dm/test/test_dmk_io.cpp
@@ -1,34 +1,6 @@
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
 #include "module_elecstate/module_dm/dmk_io.h"
-#include "prepare_unitcell.h"
-#include "module_cell/parallel_kpoints.h"
-#include "module_io/berryphase.h"
-
-#ifdef __LCAO
-InfoNonlocal::InfoNonlocal(){}
-InfoNonlocal::~InfoNonlocal(){}
-LCAO_Orbitals::LCAO_Orbitals(){}
-LCAO_Orbitals::~LCAO_Orbitals(){}
-#endif
-Magnetism::Magnetism()
-{
-	this->tot_magnetization = 0.0;
-	this->abs_magnetization = 0.0;
-	this->start_magnetization = nullptr;
-}
-Magnetism::~Magnetism()
-{
-	delete[] this->start_magnetization;
-}
-
-namespace GlobalC
-{
-	Parallel_Kpoints Pkpoints;
-	UnitCell ucell;
-}
-bool berryphase::berry_phase_flag=0;
-
 
 /************************************************
  *  unit test of read_dmk and write_dmk
@@ -47,19 +19,15 @@ bool berryphase::berry_phase_flag=0;
 class DMKIOTest : public ::testing::Test
 {
 protected:
-	int nspin = 1;
-	int lgd = 26;
 	int nks = 2;
-	std::vector<int> nw = {13};
-	int nlocal = 0;
-	UnitCell* ucell;
+	int nlocal = 26;
 	std::vector<ModuleBase::ComplexMatrix> DMK;
 	K_Vectors* kv = nullptr;
 	void SetUp()
 	{
 		// initalize a DMK
 		DMK.resize(nks);
-		ModuleBase::ComplexMatrix zero_dmk(lgd,lgd);
+		ModuleBase::ComplexMatrix zero_dmk(nlocal,nlocal);
 		for (int ik = 0;ik < nks;ik++)
 		{
 			DMK[ik] = zero_dmk;
@@ -74,20 +42,15 @@ protected:
 	{
 		DMK.clear();
 		delete kv;
-		//delete ucell;
 	}
 };
 
 TEST_F(DMKIOTest,IO)
 {
-	UcellTestPrepare utp = UcellTestLib["Si"];
-	ucell = utp.SetUcellInfo(nw, nlocal);
-
 	// read
 	std::string ssdk;
 	for (int ik = 0;ik < kv->nks;++ik){
 		ssdk = "./support/" + std::to_string(ik) + ".dmk";
-		//std::cout << ssdk << std::endl;
 		elecstate::read_dmk(*kv,ik,nlocal,ssdk,DMK);
 	}
 
