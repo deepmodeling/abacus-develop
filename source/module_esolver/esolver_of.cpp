f5ad710c5168f8de9f4de8efc44d4b74071296de
sunliang98 <1700011430@pku.edu.cn>
2023-08-23 09:40:42 +0800

    <format> Format the esolver_of.cpp

--- source/module_esolver/esolver_of.cpp
+++ source/module_esolver/esolver_of.cpp
@@ -352,7 +352,16 @@ void ESolver_OF::init_after_vc(Input &inp, UnitCell &ucell)
     // ===================================
     this->tf.set_para(this->nrxx, this->dV, GlobalV::of_tf_weight);
     this->vw.set_para(this->nrxx, this->dV, GlobalV::of_vw_weight);
-    this->wt.set_para(this->nrxx, this->dV, GlobalV::of_wt_alpha, GlobalV::of_wt_beta, this->nelec[0], GlobalV::of_tf_weight, GlobalV::of_vw_weight, GlobalV::of_read_kernel, GlobalV::of_kernel_file, this->pw_rho);
+    this->wt.set_para(this->nrxx,
+                      this->dV,
+                      GlobalV::of_wt_alpha,
+                      GlobalV::of_wt_beta,
+                      this->nelec[0],
+                      GlobalV::of_tf_weight,
+                      GlobalV::of_vw_weight,
+                      GlobalV::of_read_kernel,
+                      GlobalV::of_kernel_file,
+                      this->pw_rho);
     this->lkt.set_para(this->nrxx, this->dV, GlobalV::of_lkt_a);
     ModuleBase::GlobalFunc::DONE(GlobalV::ofs_running, "INIT KEDF");
 
