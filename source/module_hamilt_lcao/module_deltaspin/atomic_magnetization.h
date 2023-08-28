694e1c6955b09551b7f86663bac1c3875bb87088
root <hongriTianqi>
2023-08-28 05:25:26 +0000

    add atomic_magnetization function

--- source/module_hamilt_lcao/module_deltaspin/atomic_magnetization.h
+++ source/module_hamilt_lcao/module_deltaspin/atomic_magnetization.h
@@ -1,6 +1,10 @@
 #ifndef ATOMIC_MAGNETIZATION_H
 #define ATOMIC_MAGNETIZATION_H
 
-void calculate_MW_from_lambda();
+#include "module_cell/klist.h"
+#include "module_hamilt_lcao/hamilt_lcaodft/LCAO_hamilt.h"
+#include "module_hamilt_lcao/hamilt_lcaodft/local_orbital_charge.h"
+
+void calculate_MW_from_lambda(const int& step, LCAO_Hamilt& uhm, Local_Orbital_Charge& loc, const K_Vectors& kv);
 
 #endif // ATOMIC_MAGNETIZATION_H
