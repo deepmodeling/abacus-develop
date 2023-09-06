6887970448f8e829cc99d6f67e8ef7b0cff94eb3
root <hongriTianqi>
2023-09-06 09:35:23 +0000

    remove global variables

--- source/module_hamilt_lcao/module_sc/cal_mw.cpp
+++ source/module_hamilt_lcao/module_sc/cal_mw.cpp
@@ -19,10 +19,12 @@ void SpinConstrain::cal_MW(const int& step,
     orbMulP = this->cal_MW_k(loc.dm_k, uhm, kv);
     
     std::vector<std::vector<std::vector<double>>> AorbMulP = this->convert(orbMulP);
+
+    size_t nw = this->get_nw();
     
     if (GlobalV::MY_RANK == 0)
     {
-        const int nlocal = (GlobalV::NSPIN == 4) ? GlobalV::NLOCAL / 2 : GlobalV::NLOCAL;
+        const int nlocal = nw / 2;
         std::stringstream as;
         as << GlobalV::global_out_dir << "atomic_magnetization.txt";
         std::ofstream os;
@@ -39,9 +41,9 @@ void SpinConstrain::cal_MW(const int& step,
 
         double sch = 0.0;
         os << std::setprecision(4);
-        for (size_t is = 0; is != GlobalV::NSPIN; ++is)
+        for (int is = 0; is < this->nspin_; ++is)
         {
-            if (GlobalV::NSPIN == 4 && is > 0)
+            if (this->nspin_ == 4 && is > 0)
                 continue;
             double sss = 0.0;
             for (size_t iw = 0; iw != nlocal; ++iw)
@@ -56,7 +58,7 @@ void SpinConstrain::cal_MW(const int& step,
         for (size_t i = 0; i != ucell.nat; ++i)
         {
             double total_charge = 0.0, atom_mag = 0.0;
-            std::vector<double> total_charge_soc(GlobalV::NSPIN);
+            std::vector<double> total_charge_soc(this->nspin_);
             const int t = ucell.iat2it[i];
             int num = 0;
             os << i << std::setw(25) << "Zeta of " << ucell.atoms[t].label << std::setw(30) << "Spin 1" << std::setw(30)
@@ -64,10 +66,10 @@ void SpinConstrain::cal_MW(const int& step,
 
             for (size_t L = 0; L != ucell.atoms[t].nwl + 1; ++L)
             {
-                std::vector<double> sum_l(GlobalV::NSPIN, 0.0);
+                std::vector<double> sum_l(this->nspin_, 0.0);
                 for (size_t Z = 0; Z != ucell.atoms[t].l_nchi[L]; ++Z)
                 {
-                    std::vector<double> sum_m(GlobalV::NSPIN, 0.0);
+                    std::vector<double> sum_m(this->nspin_, 0.0);
                     for (size_t M = 0; M != (2 * L + 1); ++M)
                     {
                         double spin[4];
@@ -123,14 +125,11 @@ ModuleBase::matrix SpinConstrain::cal_MW_k(const std::vector<ModuleBase::Complex
 )
 {
     ModuleBase::TITLE("module_sc", "cal_MW_k");
-
-    const int nspin = (GlobalV::NSPIN == 2) ? 2 : 1;
-    const int nlocal = (GlobalV::NSPIN == 4) ? GlobalV::NLOCAL/2 : GlobalV::NLOCAL;
-    // std::vector<std::vector<double>> MecMulP(GlobalV::NSPIN, std::vector<double>(nlocal, 0));
-    // std::vector<std::vector<double>> orbMulP(GlobalV::NSPIN, std::vector<double>(nlocal, 0));
+    int nw = this->get_nw();
+    const int nlocal = nw/2;
     ModuleBase::matrix MecMulP, orbMulP;
-    MecMulP.create(GlobalV::NSPIN, nlocal);
-    orbMulP.create(GlobalV::NSPIN, nlocal);
+    MecMulP.create(this->nspin_, nlocal);
+    orbMulP.create(this->nspin_, nlocal);
 
     for(size_t ik = 0; ik != kv.nks; ++ik)
     {
@@ -147,9 +146,9 @@ ModuleBase::matrix SpinConstrain::cal_MW_k(const std::vector<ModuleBase::Complex
         const std::complex<double> one_float = {1.0, 0.0}, zero_float = {0.0, 0.0};        
         pzgemm_(&T_char,
                 &T_char,
-                &GlobalV::NLOCAL,
-                &GlobalV::NLOCAL,
-                &GlobalV::NLOCAL,
+                &nw,
+                &nw,
+                &nw,
                 &one_float,
                 dm[ik].c,
                 &one_int,
@@ -164,62 +163,49 @@ ModuleBase::matrix SpinConstrain::cal_MW_k(const std::vector<ModuleBase::Complex
                 &one_int,
                 &one_int,
                 uhm.LM->ParaV->desc);
-        if(GlobalV::NSPIN == 1 || GlobalV::NSPIN == 2)
+
+        for(size_t i=0; i < nw; ++i)
         {
-            const int spin = kv.isk[ik];
-            for(size_t i=0; i!=GlobalV::NLOCAL; ++i)
-                if(uhm.LM->ParaV->in_this_processor(i, i))
-                {
-                    const int ir = uhm.LM->ParaV->global2local_row(i);
-                    const int ic = uhm.LM->ParaV->global2local_col(i);
-                    MecMulP(spin, i) += mud(ic, ir).real();
-                }
-        }
-        else if(GlobalV::NSPIN == 4)
-        {
-            for(size_t i=0; i!=GlobalV::NLOCAL; ++i)
+            const int index = i%2;
+            if(!index)
             {
-                const int index = i%2;
-                if(!index)
+                const int j = i/2;
+                const int k1 = 2*j;
+                const int k2 = 2*j+1;
+                if(uhm.LM->ParaV->in_this_processor(k1, k1))
                 {
-                    const int j = i/2;
-                    const int k1 = 2*j;
-                    const int k2 = 2*j+1;
-                    if(uhm.LM->ParaV->in_this_processor(k1, k1))
-                    {
-                        const int ir = uhm.LM->ParaV->global2local_row(k1);
-                        const int ic = uhm.LM->ParaV->global2local_col(k1);
-                        MecMulP(0, j) += mud(ic, ir).real();
-                        MecMulP(3, j) += mud(ic, ir).real();
-                    }
-                    if(uhm.LM->ParaV->in_this_processor(k1, k2))
-                    {
-                        const int ir = uhm.LM->ParaV->global2local_row(k1);
-                        const int ic = uhm.LM->ParaV->global2local_col(k2);
-                        MecMulP(1, j) += mud(ic, ir).real();
-                        MecMulP(2, j) += mud(ic, ir).imag();
-                    }
-                    if(uhm.LM->ParaV->in_this_processor(k2, k1))
-                    {
-                        const int ir = uhm.LM->ParaV->global2local_row(k2);
-                        const int ic = uhm.LM->ParaV->global2local_col(k1);
-                        MecMulP(1, j) += mud(ic, ir).real();
-                        MecMulP(2, j) -= mud(ic, ir).imag();
-                    }
-                    if(uhm.LM->ParaV->in_this_processor(k2, k2))
-                    {
-                        const int ir = uhm.LM->ParaV->global2local_row(k2);
-                        const int ic = uhm.LM->ParaV->global2local_col(k2);
-                        MecMulP(0, j) += mud(ic, ir).real();
-                        MecMulP(3, j) -= mud(ic, ir).real();
-                    }
+                    const int ir = uhm.LM->ParaV->global2local_row(k1);
+                    const int ic = uhm.LM->ParaV->global2local_col(k1);
+                    MecMulP(0, j) += mud(ic, ir).real();
+                    MecMulP(3, j) += mud(ic, ir).real();
+                }
+                if(uhm.LM->ParaV->in_this_processor(k1, k2))
+                {
+                    const int ir = uhm.LM->ParaV->global2local_row(k1);
+                    const int ic = uhm.LM->ParaV->global2local_col(k2);
+                    MecMulP(1, j) += mud(ic, ir).real();
+                    MecMulP(2, j) += mud(ic, ir).imag();
+                }
+                if(uhm.LM->ParaV->in_this_processor(k2, k1))
+                {
+                    const int ir = uhm.LM->ParaV->global2local_row(k2);
+                    const int ic = uhm.LM->ParaV->global2local_col(k1);
+                    MecMulP(1, j) += mud(ic, ir).real();
+                    MecMulP(2, j) -= mud(ic, ir).imag();
+                }
+                if(uhm.LM->ParaV->in_this_processor(k2, k2))
+                {
+                    const int ir = uhm.LM->ParaV->global2local_row(k2);
+                    const int ic = uhm.LM->ParaV->global2local_col(k2);
+                    MecMulP(0, j) += mud(ic, ir).real();
+                    MecMulP(3, j) -= mud(ic, ir).real();
                 }
             }
         }
 #endif
     }
 #ifdef __MPI
-    MPI_Reduce(MecMulP.c, orbMulP.c, GlobalV::NSPIN*nlocal, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+    MPI_Reduce(MecMulP.c, orbMulP.c, this->nspin_*nlocal, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
 #endif 
 
     return orbMulP;
