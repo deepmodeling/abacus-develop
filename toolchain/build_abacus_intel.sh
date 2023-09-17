5a32724bb81af623266cee217b998a77c0c2dd84
James Misaka <ff6757442@gmail.com>
2023-09-17 17:51:30 +0800

    Update build_abacus_intel.sh

--- toolchain/build_abacus_intel.sh
+++ toolchain/build_abacus_intel.sh
@@ -26,6 +26,8 @@ CEREAL=$INSTALL_DIR/cereal-1.3.2/include/cereal
 LIBXC=$INSTALL_DIR/libxc-6.2.2
 LIBTORCH=$INSTALL_DIR/libtorch-2.0.1/share/cmake/Torch
 LIBNPY=$INSTALL_DIR/libnpy-0.1.0/include
+# LIBRI=$INSTALL_DIR/libri-0.1.0
+# LIBCOMM=$INSTALL_DIR/libcomm-0.1.0
 # DEEPMD=$HOME/apps/anaconda3/envs/deepmd
 
 # if use deepks and deepmd
