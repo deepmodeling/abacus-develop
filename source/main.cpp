f0c56461ce255a34a7dc89e5e4f86fdb6d4345ea
Satinelamp <simens1187@gmail.com>
2023-09-22 17:09:23 +0800

    change the stlye

--- source/main.cpp
+++ source/main.cpp
@@ -2,23 +2,25 @@
 // AUTHOR : mohan
 // DATE : 2008-11-10
 //==========================================================
+
 #include <ctime>
+
 #include "driver.h"
 #include "module_base/parallel_global.h"
 #include "module_io/parse_args.h"
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
-    ModuleIO::parse_args(argc,argv);
-    
-    Parallel_Global::read_mpi_parameters(argc,argv);
-	// main program for doing electronic structure calculations
-	//----------------------------------------------------------
-	Driver DD;
-	DD.init();
+    ModuleIO::parse_args(argc, argv);
+
+    Parallel_Global::read_mpi_parameters(argc, argv);
+    // main program for doing electronic structure calculations
+    //----------------------------------------------------------
+    Driver DD;
+    DD.init();
 
 #ifdef __MPI
-	Parallel_Global::finalize_mpi();
+    Parallel_Global::finalize_mpi();
 #endif
 
     return 0;
