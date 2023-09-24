61756f7519940ad6ccbabe443c541b997c5b7ba3
Satinelamp <simens1187@gmail.com>
2023-09-24 18:42:36 +0800

    add comments in main and parse_args.h

--- source/main.cpp
+++ source/main.cpp
@@ -9,14 +9,27 @@
 
 int main(int argc, char** argv)
 {
+    /*
+    read the arguement in the command-line
+    with "abacus -v", the program exit and returns version info
+    with no arguments, the program continues.
+    */
     ModuleIO::parse_args(argc, argv);
 
+    /*
+    read the mpi parameters in the command-line
+    */
     Parallel_Global::read_mpi_parameters(argc, argv);
-    // main program for doing electronic structure calculations
-    //----------------------------------------------------------
+
+    /*
+    main program for doing electronic structure calculations
+    */
     Driver DD;
     DD.init();
 
+    /*
+    After running mpi version of abacus, release the mpi resources.
+    */
 #ifdef __MPI
     Parallel_Global::finalize_mpi();
 #endif
