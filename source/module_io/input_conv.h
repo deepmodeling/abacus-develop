e4b9d026581aa88077bd17543144eb89e0904f94
Satinelamp <simens1187@gmail.com>
2023-09-25 23:13:18 +0800

    add comment input_conv.h

--- source/module_io/input_conv.h
+++ source/module_io/input_conv.h
@@ -19,9 +19,15 @@ namespace Input_Conv
 {
 void Convert(void);
 
-// Function `parse_expression` is used to parse input parameters as expressions into vectors
-// fn (string): expressions such as "3*1 0 2*0.5 3*0"
-// arr (vector): stores parsing results, for example, "3*1 0 2*0.5 1*1.5" can be parsed as [1, 1, 1, 0, 0.5, 0.5, 1.5]
+/**
+ * @brief To parse input parameters as expressions into vectors
+ * 
+ * @tparam T 
+ * @param fn  (string): expressions such as "3*1 0 2*0.5 3*0"
+ * @param arr (vector): stores parsing results, 
+ *            for example, "3*1 0 2*0.5 1*1.5" can be parsed as 
+ *            [1, 1, 1, 0, 0.5, 0.5, 1.5]
+ */
 template <typename T> void parse_expression(const std::string &fn, std::vector<T> &arr);
 
 #ifdef __LCAO
