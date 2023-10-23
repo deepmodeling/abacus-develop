3536e466f0ae6d6edf556a68a1ee51255720f5bd
Zhuxuegang2022 <xuegangzhu@qq.com>
2023-10-23 10:39:27 +0800

    just add space before ' ]'

--- tests/integrate/Autotest.sh
+++ tests/integrate/Autotest.sh
@@ -110,7 +110,7 @@ check_out(){
 			break
 		else
 			if [ $(echo "sqrt($deviation*$deviation) < $threshold"|bc) = 0 ]; then
-				if [ $key == "totalforceref" -o $key == "totalstressref"]; then
+				if [ $key == "totalforceref" -o $key == "totalstressref" ]; then
 					if [ $key == "totalforceref" && $(echo "sqrt($deviation*$deviation) < $force_threshold"|bc) = 0 ];then
 						echo -e "[WARNING   ] "\
 							"$key cal=$cal ref=$ref deviation=$deviation"
