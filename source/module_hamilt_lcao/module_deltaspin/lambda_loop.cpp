bbc3cd233193af63d318c5f719b66aad3a500ece
root <hongriTianqi>
2023-10-27 11:10:52 +0000

    apply check_gradient_decay in lambda loop

--- source/module_hamilt_lcao/module_deltaspin/lambda_loop.cpp
+++ source/module_hamilt_lcao/module_deltaspin/lambda_loop.cpp
@@ -14,27 +14,12 @@ void SpinConstrain<std::complex<double>, psi::DEVICE_CPU>::run_lambda_loop(int o
     std::vector<ModuleBase::Vector3<double>> initial_lambda(nat,0.0);
     std::vector<ModuleBase::Vector3<double>> delta_lambda(nat,0.0);
     // set nu, dnu and dnu_last_step
-    std::vector<ModuleBase::Vector3<double>> dnu(nat,0.0), dnu_last_step(nat,0.0), nu_change(nat,1.0);
+    std::vector<ModuleBase::Vector3<double>> dnu(nat, 0.0), dnu_last_step(nat, 0.0);
     // two controlling temp variables
-    std::vector<ModuleBase::Vector3<double>> temp_1(nat,0.0), temp_2(nat,0.0);
-    // MW during loop
-    // new_spin replace MW in the original code
-    // spin gradient
-    std::vector<std::vector<std::vector<std::vector<double>>>> spin_nu_gradient(
-        nat, std::vector<std::vector<std::vector<double>>>(
-            3, std::vector<std::vector<double>>(
-                nat, std::vector<double>(
-                    3,0.0))));
-    std::vector<ModuleBase::Vector3<double>> spin_nu_gradient_diag(nat,0.0);
-    std::pair<int, int> maxloc(std::make_pair(0,0));
-    std::vector<std::pair<int,int>> max_gradient_index(ntype, std::make_pair(0,0));
-    std::vector<double> max_gradient(ntype,0.0);
-    // question: bound_gradient should be read from INPUT?
-    std::vector<double> bound_gradient(ntype,0.0);
-
-    std::vector<ModuleBase::Vector3<double>> spin(nat,0.0), delta_spin(nat,0.0), delta_spin_old(nat,0.0);
+    std::vector<ModuleBase::Vector3<double>> temp_1(nat, 0.0);
+    std::vector<ModuleBase::Vector3<double>> spin(nat, 0.0), delta_spin(nat, 0.0);
     std::vector<ModuleBase::Vector3<double>> search(nat, 0.0), search_old(nat, 0.0);
-    std::vector<ModuleBase::Vector3<double>> new_spin(nat, 0.0), spin_change(nat, 0.0), spin_plus(nat, 0.0);
+    std::vector<ModuleBase::Vector3<double>> new_spin(nat, 0.0), spin_plus(nat, 0.0);
 
     double alpha_opt, alpha_plus;
     double beta;
@@ -48,12 +33,6 @@ void SpinConstrain<std::complex<double>, psi::DEVICE_CPU>::run_lambda_loop(int o
 
     const double zero = 0.0;
     const double one = 1.0;
-
-    for (int it = 0; it < ntype; it++)
-    {
-        bound_gradient[it] = this->decay_grad_[it];
-        //std::cout << "bound_gradient " << bound_gradient[it] << std::endl;
-    }
 
     this->print_header();
     // lambda loop
@@ -72,65 +51,12 @@ void SpinConstrain<std::complex<double>, psi::DEVICE_CPU>::run_lambda_loop(int o
             add_scalar_multiply_2d(initial_lambda, delta_lambda, one, this->lambda_);
             this->cal_mw_from_lambda(i_step);
             new_spin = this->Mi_;
-            subtract_2d(new_spin, spin, spin_change);
-            subtract_2d(delta_lambda, dnu_last_step, nu_change);
-            where_fill_scalar_2d(this->constrain_, 0, zero, spin_change);
-            where_fill_scalar_2d(this->constrain_, 0, one, nu_change);
-            // calculate spin_nu_gradient
-            for (int ia = 0; ia < nat; ia++)
+            bool GradLessThanBound = this->check_gradient_decay(new_spin, spin, delta_lambda, dnu_last_step);
+            if (i_step >= this->nsc_min_ && GradLessThanBound)
             {
-                for (int ic = 0; ic < 3; ic++)
-                {
-                    for (int ja = 0; ja < nat; ja++)
-                    {
-                        for(int jc = 0; jc < 3; jc++)
-                        {
-                            spin_nu_gradient[ia][ic][ja][jc] = spin_change[ia][ic] / nu_change[ja][jc];
-                        }
-                    }
-                }
-            }
-            for (const auto& sc_elem : this->get_atomCounts())
-            {
-                int it = sc_elem.first;
-                int nat_it = sc_elem.second;
-                max_gradient[it] = 0.0;
-                for (int ia = 0; ia < nat_it; ia++)
-                {
-                    for (int ic = 0; ic < 3; ic++)
-                    {
-                        spin_nu_gradient_diag[ia][ic] = spin_nu_gradient[ia][ic][ia][ic];
-                        if (std::abs(spin_nu_gradient_diag[ia][ic]) > std::abs(max_gradient[it]))
-                        {
-                            max_gradient[it] = spin_nu_gradient_diag[ia][ic];
-                            max_gradient_index[it].first = ia;
-                            max_gradient_index[it].second = ic;
-                        }
-                    }
-                }
-            }
-            //print_2d("diagonal gradient: ", spin_nu_gradient_diag);
-            //std::cout << "maximum gradient appears at: " << std::endl;
-            //for (int it = 0; it < ntype; it++)
-            //{
-            //    std::cout << "( " << max_gradient_index[it].first << ", " << max_gradient_index[it].second << " )" << std::endl;
-            //}
-            //std::cout << "maximum gradient: " << std::endl;
-            //for (int it = 0; it < ntype; it++)
-            //{
-            //    std::cout << max_gradient[it] << std::endl;
-            //}
-            for (int it = 0; it < ntype; it++)
-            {
-                if (i_step >= this->nsc_min_ && bound_gradient[it] > 0 && std::abs(max_gradient[it]) < bound_gradient[it])
-                {
-                    std::cout << "Reach limitation of current step ( maximum gradient < " 
-                        << bound_gradient[it] << " in atom type " << it << " ), exit." << std::endl;
-                    // roll back to the last step
-                    add_scalar_multiply_2d(initial_lambda, dnu_last_step, one, this->lambda_);
-                    this->print_termination();
-                    break;
-                }
+                add_scalar_multiply_2d(initial_lambda, dnu_last_step, one, this->lambda_);
+                this->print_termination();
+                break;
             }
             spin = new_spin;
         }
@@ -152,13 +78,11 @@ void SpinConstrain<std::complex<double>, psi::DEVICE_CPU>::run_lambda_loop(int o
             add_scalar_multiply_2d(initial_lambda, dnu_last_step, 1.0, this->lambda_);
             break;
         }
-
         if (i_step >= 2)
         {
             beta = mean_error / mean_error_old;
             add_scalar_multiply_2d(search, search_old, beta, search);
         }
-
         /// check if restriction is needed
         this->check_restriction(search, alpha_trial);
 
@@ -181,7 +105,6 @@ void SpinConstrain<std::complex<double>, psi::DEVICE_CPU>::run_lambda_loop(int o
         delta_lambda = dnu;
 
         search_old = search;
-        delta_spin_old = delta_spin;
         mean_error_old = mean_error;
 
         g = 1.5 * std::abs(alpha_opt) / alpha_trial;
