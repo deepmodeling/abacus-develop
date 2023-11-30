19486bae515dae8fb886769806e1f59447ec06ad
Qianruipku <Terry_Liu@pku.edu.cn>
2023-11-30 14:04:06 +0800

    Feature: add cond_smear for conductvities Fix: warning of catch_properties.sh

--- source/module_esolver/esolver_sdft_pw.h
+++ source/module_esolver/esolver_sdft_pw.h
@@ -39,6 +39,7 @@ class ESolver_SDFT_PW : public ESolver_KS_PW<std::complex<double>>
      *
      * @param nche_KG Number Chebyshev orders
      * @param fwhmin FWHM
+     * @param smear_type 1: Gaussian, 2: Lorentzian
      * @param wcut cutoff omega
      * @param dw_in omega step
      * @param dt_in t step
@@ -46,6 +47,7 @@ class ESolver_SDFT_PW : public ESolver_KS_PW<std::complex<double>>
      * @param npart_sto number stochastic wavefunctions parts to evalution simultaneously
      */
     void sKG(const int nche_KG,
+             const int& smear_type,
              const double fwhmin,
              const double wcut,
              const double dw_in,
