942ff53b45838264823b8e17adc002dd4ab01d0c
zhang haochong <solomon.zhc@gmail.com>
2024-01-13 08:26:05 +0000

    remove some func declare

--- source/module_hamilt_lcao/module_gint/gint_gpu/vbatch_matrix_multiple/vbatch_matrix_mul.cuh
+++ source/module_hamilt_lcao/module_gint/gint_gpu/vbatch_matrix_multiple/vbatch_matrix_mul.cuh
@@ -11,27 +11,6 @@
 cudaError_t checkCuda(cudaError_t result);
 cudaError_t checkCudaLastError();
 
-template<typename T, int DIM_X, int DIM_Y, int BLK_M, int BLK_N, int BLK_K,
-         int DIM_XA, int DIM_YA, int DIM_XB, int DIM_YB,
-         int THR_M, int THR_N>
-static __device__
-void vbatched_gemm_device(
-    int M, int N, int K,
-    T* __restrict__ A, int LDA,
-    T* __restrict__ B, int LDB,
-    T*       __restrict__ C, int LDC,
-    T* sA, int slda,
-    T* sB, int sldb);
-
-template <typename T, int DIM_X, int DIM_Y, int BLK_M, int BLK_N, int BLK_K,
-         int DIM_XA, int DIM_YA, int DIM_XB, int DIM_YB>
-static __global__
-void vbatched_gemm_kernel(
-    int* M, int* N, int K,
-    T * * Aarray, int* LDA,
-    T * * Barray, int* LDB,
-    T              ** Carray, int* LDC);
-
 template <typename T, int DIM_X, int DIM_Y, int BLK_M, int BLK_N, int BLK_K,
          int DIM_XA, int DIM_YA, int DIM_XB, int DIM_YB>
 void vbatched_gemm_impl(int max_m, int max_n,
@@ -40,11 +19,13 @@ void vbatched_gemm_impl(int max_m, int max_n,
                  T ** global_B_array, int* global_ldb,
                  T ** global_C_array, int* global_ldc,
                  int batchCount, cudaStream_t stream);
+
 typedef std::function<void(int, int,
                     int *, int *, int*,
                     double **, int *,
                     double **, int *,
                     double **, int *,
                     int, cudaStream_t)> matrix_multiple_func_type;
+
 void gemm_algo_selector(int k, matrix_multiple_func_type &func);
 #endif // VBATCH_MATRIX_MUL_H
