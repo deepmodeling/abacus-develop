af5cc0c6547ecb406c9a7d0199da8da99ca9039b
Jie Li <li_j20@fudan.edu.cn>
2024-01-24 12:23:50 +0000

    rewrite benchmark tests in fixtures

--- source/module_base/test/perf_sphbes_test.cpp
+++ source/module_base/test/perf_sphbes_test.cpp
@@ -15,63 +15,58 @@
  *      - Spherical_Bessel
  */
 
-const double q = 1;
-const int n = 1000;
-double* rc, * rinf, * jc, * jinf;
-double stop = 1000.0;
+class PerfSphbes : public benchmark::Fixture {
+public:
+    const double q = 1;
+    const int n = 1000;
+    double stop = 1000.0;
+    double dr = 0.0;
+    double* rc, *rinf, *jc, *jinf;
+    void SetUp(const benchmark::State& state){
+        const double rcut = state.range(0) + 0.5;
+        rc = new double[n + 10]; 
+        rinf = new double[n + 10];
+        jc = new double[n + 10];
+        jinf = new double[n + 10];
 
-void setupcut(const int n, const double rcut, double* r){
-    // generate a list of r from 0 to rcut in log grid
-    double rmin = 0.0001;
-    double log_rmin = std::log(rmin);
-    double log_rcut = std::log(rcut);
-    double dr = (log_rcut - log_rmin) / (n-1);
-    memset(r, 0, n * sizeof(double));
-    for (int i = 0; i < n; i++)
-        r[i] = std::exp(log_rmin + i * dr);
+        // generate data points in (0, rcut] in log scale
+        double rmin = 0.0001;
+        double log_rmin = std::log(rmin);
+        double log_rcut = std::log(rcut);
+        dr = (log_rcut - log_rmin) / (n-1);
+        memset(rc, 0, (n+10) * sizeof(double));
+        for (int i = 0; i < n; i++)
+            rc[i] = std::exp(log_rmin + i * dr);
+        
+        // generate data points in [rcut, stop] in linear scale
+        memset(rinf, 0, (n+10) * sizeof(double));
+        rinf[0] = rcut;
+        dr = (stop - rcut) / (n-1);
+        for (int i = 1; i < n; i++)
+            rinf[i] += rinf[i-1] + dr;
+    }
+    void TearDown(const benchmark::State& state){
+        delete[] rc;
+        delete[] rinf;
+        delete[] jc;
+        delete[]  jinf;
+    }
+};    
 
+BENCHMARK_DEFINE_F(PerfSphbes, BM_Spherical_Bessel)(benchmark::State& state) {
+    for (auto _ : state) {
+        ModuleBase::Sphbes::Spherical_Bessel(n, rc, q, state.range(0), jc);
+        ModuleBase::Sphbes::Spherical_Bessel(n, rinf, q, state.range(0), jinf);
+    }
 }
 
-void setupinf(const int n, const double rcut, double* r){
-    memset(r, 0, n * sizeof(double));
-    r[0] = rcut;
-    double dr = (stop - rcut) / (n-1);
-    for (int i = 1; i < n; i++)
-        r[i] += r[i-1] + dr;
-}
-
-static void DoSetup(const benchmark::State& state) {
-    const double rcut = state.range(0) + 0.5;
-    rc = new double[n + 10];
-    rinf = new double[n + 10];
-    jc = new double[n + 10];
-    jinf = new double[n + 10];
-    setupcut(n, rcut, rc);
-    setupinf(n, rcut, rinf);
-}
-
-static void DoTearDown(const benchmark::State& state) {
-    delete[] rc;
-    delete[] rinf;
-    delete[] jc;
-    delete[] jinf;
-}
-
-static void BM_sphbesj(benchmark::State& state) {
-    while (state.KeepRunning()) {
+BENCHMARK_DEFINE_F(PerfSphbes, BM_sphbesj)(benchmark::State& state) {
+    for (auto _ : state) {
         ModuleBase::Sphbes::sphbesj(n, rc, q, state.range(0), jc);
         ModuleBase::Sphbes::sphbesj(n, rinf, q, state.range(0), jinf);
     }
 }
 
-static void BM_Spherical_Bessel(benchmark::State& state) {
-    while (state.KeepRunning()) {
-        ModuleBase::Sphbes::Spherical_Bessel(n, rc, q, state.range(0), jc);
-        ModuleBase::Sphbes::Spherical_Bessel(n, rinf, q, state.range(0), jinf);
-    }
-}
-//Add the test time functions into google benchmark
-// display result in microsecond
-BENCHMARK(BM_sphbesj)->DenseRange(0, 11, 1)->Setup(DoSetup)->Teardown(DoTearDown)->Unit(benchmark::kMicrosecond);
-BENCHMARK(BM_Spherical_Bessel)->DenseRange(0, 11, 1)->Setup(DoSetup)->Teardown(DoTearDown)->Unit(benchmark::kMicrosecond);
+BENCHMARK_REGISTER_F(PerfSphbes, BM_sphbesj)->DenseRange(0, 11, 1)->Unit(benchmark::kMicrosecond);
+BENCHMARK_REGISTER_F(PerfSphbes, BM_Spherical_Bessel)->DenseRange(0, 11, 1)->Unit(benchmark::kMicrosecond);
 BENCHMARK_MAIN(); 
