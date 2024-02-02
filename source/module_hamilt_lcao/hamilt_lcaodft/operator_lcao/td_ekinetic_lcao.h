d38af281821db385836b6d10808e4ff2f78759dc
HTZhao <104255052+ESROAMER@users.noreply.github.com>
2024-02-02 13:26:25 +0800

    Update td_ekinetic_lcao.h

--- source/module_hamilt_lcao/hamilt_lcaodft/operator_lcao/td_ekinetic_lcao.h
+++ source/module_hamilt_lcao/hamilt_lcaodft/operator_lcao/td_ekinetic_lcao.h
@@ -46,12 +46,32 @@ class TDEkinetic<OperatorLCAO<TK,TR>> : public OperatorLCAO<TK, TR>
 
     virtual void contributeHR() override;
     virtual void contributeHk(int ik) override;
-    //init two center integrals and vector potential for td_ekintic term
+    /// @brief init two center integrals and vector potential for td_ekintic term
     void init_td(void);
+
+    /**
+     * @brief initialize HR, search the nearest neighbor atoms
+     * HContainer is used to store the non-local pseudopotential matrix with specific <I,J,R> atom-pairs
+     * the size of HR will be fixed after initialization
+     */
     void initialize_HR(Grid_Driver* GridD, const Parallel_Orbitals* paraV);
+
+    /**
+     * @brief initialize HR_tmp
+     * Allocate the memory for HR_tmp with the same size as HR
+    */
     void initialize_HR_tmp(const Parallel_Orbitals* paraV);
 
+    /**
+     * @brief calculate the HR local matrix of <I,J,R> atom pair
+     */
     void cal_HR_IJR(const int& iat1,const int& iat2,const Parallel_Orbitals* paraV,const ModuleBase::Vector3<double>& dtau,std::complex<double>* data_pointer,TR* s_pointer);
+
+    /**
+     * @brief calculate the ekinetic matrix correction term in tddft with specific <I,J,R> atom-pairs
+     * nearest neighbor atoms don't need to be calculated again
+     * loop the atom-pairs in HR and calculate the ekinetic matrix
+     */
     void calculate_HR(void);
     virtual void set_HR_fixed(void*)override;
 
@@ -59,20 +79,20 @@ class TDEkinetic<OperatorLCAO<TK,TR>> : public OperatorLCAO<TK, TR>
     const UnitCell* ucell = nullptr;
     
     HContainer<TR>* SR = nullptr;
-    //Store real space hamiltonian. TD term should include imaginary part, thus it has to be complex type. Only shared between TD operators.
+    /// @brief Store real space hamiltonian. TD term should include imaginary part, thus it has to be complex type. Only shared between TD operators.
     HContainer<std::complex<double>>* hR_tmp = nullptr;
     Grid_Driver* Grid = nullptr;
 
     const K_Vectors* kv;
-    //correction term iA⋅∇
+    /// @brief correction term iA⋅∇
     void td_ekinetic_scalar(std::complex<double>* Hloc, TR* Sloc, int nnr);
-    //correction term A^2*S
+    /// @brief correction term A^2*S
     void td_ekinetic_grad(std::complex<double>* Hloc, int nnr, ModuleBase::Vector3<double> grad_overlap);
 
     ORB_table_phi MOT;
 	  ORB_gaunt_table MGT;
     
-    //Store the two center integrals outcome <𝝓_𝝁𝟎 |𝛁| 𝝓_𝝂𝑹> for td_ekinetic term
+    /// @brief Store the two center integrals outcome <𝝓_𝝁𝟎 |𝛁| 𝝓_𝝂𝑹> for td_ekinetic term
     std::map<size_t,                                // TA
       std::map<size_t,                            // TB
         std::map<int,                           // LA
@@ -80,9 +100,12 @@ class TDEkinetic<OperatorLCAO<TK,TR>> : public OperatorLCAO<TK, TR>
             std::map<int,                   // LB
               std::map<size_t,            // NB
                 Center2_Orb::Orb11>>>>>> center2_orb11_s;
-    std::vector<AdjacentAtomInfo> adjs_all;
-    //Store the vector potential for td_ekinetic term
+
+    /// @brief Store the vector potential for td_ekinetic term
     ModuleBase::Vector3<double> cart_At;
+
+    /// @brief exact the nearest neighbor atoms from all adjacent atoms
+    std::vector<AdjacentAtomInfo> adjs_all;
 
     bool hR_tmp_done = false;
     bool allocated = false;
