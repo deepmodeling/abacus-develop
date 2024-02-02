6bfe2c9b202a76e9406fce0520f11a7fa24d67a0
HTZhao <104255052+ESROAMER@users.noreply.github.com>
2024-02-02 13:47:04 +0800

    Update td_current_io.h

--- source/module_io/td_current_io.h
+++ source/module_io/td_current_io.h
@@ -8,7 +8,8 @@
 
 namespace ModuleIO
 {
-//func to output current, only used in tddft
+#ifdef __LCAO
+/// @brief func to output current, only used in tddft
 void write_current(const int istep,
                     const psi::Psi<std::complex<double>>* psi,
                     const elecstate::ElecState* pelec,
@@ -17,14 +18,15 @@ void write_current(const int istep,
                     Record_adj& ra,
                     LCAO_Hamilt& UHM);
 
-//calculate sum_n[𝜌_(𝑛𝑘,𝜇𝜈)] for current calculation
+/// @brief calculate sum_n[𝜌_(𝑛𝑘,𝜇𝜈)] for current calculation
 void cal_tmp_DM(elecstate::DensityMatrix<std::complex<double>, double>& DM, const int ik, const int nspin);
 
-//Init DS_locR for currrent calculation
+/// @brief Init DS_locR for currrent calculation
 void Init_DS_tmp(const Parallel_Orbitals& pv,LCAO_Hamilt& UHM);
 
-//DS_locR will be initialized again in force calculation, so it must be destoryed here.
+/// @brief DS_locR will be initialized again in force calculation, so it must be destoryed here.
 void destory_DS_tmp(LCAO_Hamilt& UHM);
 }
+#endif // __LCAO
 
 #endif // TD_CURRENT_H
