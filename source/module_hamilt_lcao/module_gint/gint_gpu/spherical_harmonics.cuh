9a80cfccf87f64972bd351e2f26fb4b0d7996247
denghuilu <denghuilu@pku.edu.cn>
2024-03-03 15:06:32 +0800

    change vlocal and rho with dynamic parallelism

--- source/module_hamilt_lcao/module_gint/gint_gpu/spherical_harmonics.cuh
+++ source/module_hamilt_lcao/module_gint/gint_gpu/spherical_harmonics.cuh
@@ -1,7 +1,132 @@
 #include "cuda_runtime.h"  
 #include "device_launch_parameters.h"
 
+static __device__  void spherical_harmonics(double *dr,double distance,int nwl,double (&ylma)[49],double *ylmcoef)
+{
+    /***************************
+        L = 0
+        ***************************/
+        ylma[0] = ylmcoef[0]; // l=0, m=0
+        double tmp0;
+        if (nwl == 0)
+            return;
 
+        /***************************
+        L = 1
+        ***************************/
+        ylma[1] = ylmcoef[1] * dr[2];  // l=1, m=0
+        ylma[2] = -ylmcoef[1] * dr[0]; // l=1, m=1
+        ylma[3] = -ylmcoef[1] * dr[1]; // l=1, m=-1
+        if (nwl == 1)
+            return;
+
+        /***************************
+        L = 2
+        ***************************/
+        ylma[4] = ylmcoef[2] * dr[2] * ylma[1] - ylmcoef[3] * ylma[0]; // l=2, m=0
+        tmp0 = ylmcoef[4] * dr[2];
+        ylma[5] = tmp0 * ylma[2]; // l=2,m=1
+        ylma[6] = tmp0 * ylma[3]; // l=2,m=-1
+
+        tmp0 = ylmcoef[4] * dr[0];
+        ylma[7] = ylmcoef[5] * ylma[4] - ylmcoef[6] * ylma[0] - tmp0 * ylma[2]; // l=2,m=2
+        ylma[8] = -tmp0 * ylma[3];
+        if (nwl == 2)
+            return;
+
+        /***************************
+        L = 3
+        ***************************/
+        ylma[9] = ylmcoef[7] * dr[2] * ylma[4] - ylmcoef[8] * ylma[1]; // l=3, m=0
+
+        tmp0 = ylmcoef[9] * dr[2];
+        ylma[10] = tmp0 * ylma[5] - ylmcoef[10] * ylma[2]; // l=3,m=1
+        ylma[11] = tmp0 * ylma[6] - ylmcoef[10] * ylma[3]; // l=3,m=-1
+
+        tmp0 = ylmcoef[11] * dr[2];
+        ylma[12] = tmp0 * ylma[7]; // l=3,m=2
+        ylma[13] = tmp0 * ylma[8]; // l=3,m=-2
+
+        tmp0 = ylmcoef[14] * dr[0];
+        ylma[14] = ylmcoef[12] * ylma[10] - ylmcoef[13] * ylma[2] - tmp0 * ylma[7]; // l=3,m=3
+        ylma[15] = ylmcoef[12] * ylma[11] - ylmcoef[13] * ylma[3] - tmp0 * ylma[8]; // l=3,m=-3
+        if (nwl == 3)
+            return;
+
+        /***************************
+        L = 4
+        ***************************/
+        ylma[16] = ylmcoef[15] * dr[2] * ylma[9] - ylmcoef[16] * ylma[4]; // l=4,m=0
+
+        tmp0 = ylmcoef[17] * dr[2];
+        ylma[17] = tmp0 * ylma[10] - ylmcoef[18] * ylma[5]; // l=4,m=1
+        ylma[18] = tmp0 * ylma[11] - ylmcoef[18] * ylma[6]; // l=4,m=-1
+
+        tmp0 = ylmcoef[19] * dr[2];
+        ylma[19] = tmp0 * ylma[12] - ylmcoef[20] * ylma[7]; // l=4,m=2
+        ylma[20] = tmp0 * ylma[13] - ylmcoef[20] * ylma[8]; // l=4,m=-2
+
+        tmp0 = 3.0 * dr[2];
+        ylma[21] = tmp0 * ylma[14]; // l=4,m=3
+        ylma[22] = tmp0 * ylma[15]; // l=4,m=-3
+
+        tmp0 = ylmcoef[23] * dr[0];
+        ylma[23] = ylmcoef[21] * ylma[19] - ylmcoef[22] * ylma[7] - tmp0 * ylma[14]; // l=4,m=4
+        ylma[24] = ylmcoef[21] * ylma[20] - ylmcoef[22] * ylma[8] - tmp0 * ylma[15]; // l=4,m=-4
+        if (nwl == 4)
+            return;
+
+        /***************************
+        L = 5
+        ***************************/
+        ylma[25] = ylmcoef[24] * dr[2] * ylma[16] - ylmcoef[25] * ylma[9]; // l=5,m=0
+
+        tmp0 = ylmcoef[26] * dr[2];
+        ylma[26] = tmp0 * ylma[17] - ylmcoef[27] * ylma[10]; // l=5,m=1
+        ylma[27] = tmp0 * ylma[18] - ylmcoef[27] * ylma[11]; // l=5,m=-1
+
+        tmp0 = ylmcoef[28] * dr[2];
+        ylma[28] = tmp0 * ylma[19] - ylmcoef[29] * ylma[12]; // l=5,m=2
+        ylma[29] = tmp0 * ylma[20] - ylmcoef[29] * ylma[13]; // l=5,m=-2
+
+        tmp0 = ylmcoef[30] * dr[2];
+        ylma[30] = tmp0 * ylma[21] - ylmcoef[31] * ylma[14]; // l=5,m=3
+        ylma[31] = tmp0 * ylma[22] - ylmcoef[31] * ylma[15]; // l=5,m=-3
+
+        tmp0 = ylmcoef[32] * dr[2];
+        ylma[32] = tmp0 * ylma[23]; // l=5,m=4
+        ylma[33] = tmp0 * ylma[24]; // l=5,m=-4
+
+        tmp0 = ylmcoef[35] * dr[0];
+        ylma[34] = ylmcoef[33] * ylma[30] - ylmcoef[34] * ylma[14] - tmp0 * ylma[23]; // l=5,m=5
+        ylma[35] = ylmcoef[33] * ylma[31] - ylmcoef[34] * ylma[15] - tmp0 * ylma[24]; // l=5,m=-5
+        if (nwl == 5)
+            return;
+        /*
+        // if nwl > 5
+        for (int il = 6; il <= nwl; il++)
+        {
+            int istart = il * il;
+            int istart1 = (il - 1) * (il - 1);
+            int istart2 = (il - 2) * (il - 2);
+
+            double fac2 = sqrt(4.0 * istart - 1.0);
+            double fac4 = sqrt(4.0 * istart1 - 1.0);
+
+            for (int im = 0; im < 2 * il - 1; im++)
+            {
+                int imm = (im + 1) / 2;
+                ylma[istart + im] = fac2 / sqrt((double)istart - imm * imm) * (dr[2] * ylma[istart1 + im] - sqrt((double)istart1 - imm * imm) / fac4 * ylma[istart2 + im]);
+            }
+
+            double bl1 = sqrt(2.0 * il / (2.0 * il + 1.0));
+            double bl2 = sqrt((2.0 * il - 2.0) / (2.0 * il - 1.0));
+            double bl3 = sqrt(2.0) / fac2;
+
+            ylma[istart + 2 * il - 1] = (bl3 * ylma[istart + 2 * il - 5] - bl2 * ylma[istart2 + 2 * il - 5] - 2.0 * dr[0] * ylma[istart1 + 2 * il - 3]) / bl1;
+            ylma[istart + 2 * il] = (bl3 * ylma[istart + 2 * il - 4] - bl2 * ylma[istart2 + 2 * il - 4] - 2.0 * dr[0] * ylma[istart1 + 2 * il - 2]) / bl1;
+        }*/
+}
 static __device__  void spherical_harmonics_d(double *dr,double distance,double (&grly)[49][3],int nwl,double (&ylma)[49],double *ylmcoef)
 {
     double tmp0;
