c6024be3ce3a3d31887dd7ecf6afb366ed9f1ac0
dzzz2001 <dzcpku2001@163.com>
2024-03-17 21:41:50 +0800

    enable cusolver in multi-k calculation

--- source/module_hsolver/diago_cusolver.cpp
+++ source/module_hsolver/diago_cusolver.cpp
@@ -2,8 +2,8 @@
 #include "module_base/global_variable.h"
 #include "module_base/lapack_connector.h"
 #include "module_base/timer.h"
-#include "module_base/tool_quit.h"
-#include "mpi.h"
+
+#include <type_traits>
 
 extern "C"
 {
@@ -11,81 +11,10 @@ extern "C"
 #include "module_hsolver/genelpa/scalapack.h"
 }
 
-
 // Define matrix types for real and complex numbers
-typedef hamilt::MatrixBlock<double> matd;
-typedef hamilt::MatrixBlock<std::complex<double>> matcd;
-
-inline void Cpdgemr2d(int M,
-                        int N,
-                        double* a,
-                        int ia,
-                        int ja,
-                        int* desca,
-                        double* b,
-                        int ib,
-                        int jb,
-                        int* descb,
-                        int blacs_ctxt)
-{
-    pdgemr2d_(&M, &N, a, &ia, &ja, desca, b, &ib, &jb, descb, &blacs_ctxt);
-}
-
-// use pdgemr2d to collect matrix from all processes to root process
-void gatherMatrix(matd &mat_l, matd &mat_g)
-{
-    double *a;a = mat_l.p;
-    int *desca;desca = mat_l.desc;
-    double *b;
-    // int *descb = mat_g.desc;
-    int ctxt = desca[1];
-    int nrows = desca[2];
-    int ncols = desca[3];
-    int nlrows = desca[4];
-    int nlcols = desca[5];
-    int nprows, npcols, myprow, mypcol;
-
-    Cblacs_gridinfo(ctxt, &nprows, &npcols, &myprow, &mypcol);
-    int myid = Cblacs_pnum(ctxt, myprow, mypcol);
-
-    const int ROOT_PROC = Cblacs_pnum(ctxt, desca[6], desca[7]);
-
-    mat_g.row = nrows;
-    mat_g.col = ncols;
-
-    if (myid == ROOT_PROC)
-        b = new double[nrows * ncols];
-    else
-        b = new double[1];
-
-    // set descb, which has all elements in the only block in the root process
-    int descb[9] = {1, ctxt, nrows, ncols, nrows, ncols, 0, 0, nrows};
-    mat_g.desc = descb;
-    mat_g.p = b;
-
-    Cpdgemr2d(nrows, ncols, a, 1, 1, desca, b, 1, 1, descb, ctxt);
-}
-
-// convert the Psi to a 2D block storage format
-void distributePsi(const int *desc_psi, double *psi, double *psi_g)
-{
-    ModuleBase::timer::tick("DiagoCusolver", "distribute");
-    int ctxt = desc_psi[1];
-    int nrows = desc_psi[2];
-    int ncols = desc_psi[3];
-    int rsrc = desc_psi[6];
-    int csrc = desc_psi[7];
-
-    int descg[9] = {1, ctxt, nrows, ncols, nrows, ncols, rsrc, csrc, nrows};
-    int descl[9];
-    
-    for (int i = 0; i < 9; i++)
-    {
-        descl[i] = desc_psi[i];
-    }
-    Cpdgemr2d(nrows, ncols, psi_g, 1, 1, descg, psi, 1, 1, descl, ctxt);
-    ModuleBase::timer::tick("DiagoCusolver", "distribute");
-}
+using complex = std::complex<double>;
+using matd = hamilt::MatrixBlock<double>;
+using matcd = hamilt::MatrixBlock<complex>;
 
 // Namespace for the diagonalization solver
 namespace hsolver
@@ -94,14 +23,16 @@ namespace hsolver
     template <>
     int DiagoCusolver<double>::DecomposedState = 0;
     template <>
-    int DiagoCusolver<std::complex<double>>::DecomposedState = 0;
+    int DiagoCusolver<complex>::DecomposedState = 0;
 
-    template <> DiagoCusolver<double>::DiagoCusolver(const Parallel_Orbitals * ParaV)
+    template <>
+    DiagoCusolver<double>::DiagoCusolver(const Parallel_Orbitals* ParaV)
     {
         this->ParaV = ParaV;
     }
 
-    template <> DiagoCusolver<std::complex<double>>::DiagoCusolver(const Parallel_Orbitals * Para)
+    template <>
+    DiagoCusolver<complex>::DiagoCusolver(const Parallel_Orbitals* ParaV)
     {
         this->ParaV = ParaV;
     }
@@ -112,15 +43,83 @@ namespace hsolver
     }
 
     template <>
-    DiagoCusolver<std::complex<double>>::~DiagoCusolver()
+    DiagoCusolver<complex>::~DiagoCusolver()
     {
+    }
+
+    static inline void Cpxgemr2d(int M, int N, double *a, int ia, int ja,
+                                 int *desca, double *b, int ib, int jb,
+                                 int *descb, int blacs_ctxt) {
+      pdgemr2d_(&M, &N, a, &ia, &ja, desca, b, &ib, &jb, descb, &blacs_ctxt);
+    }
+
+    static inline void Cpxgemr2d(int M, int N, complex *a, int ia, int ja,
+                                 int *desca, complex *b, int ib, int jb,
+                                 int *descb, int blacs_ctxt) {
+      pzgemr2d_(&M, &N, reinterpret_cast<double __complex__ *>(a), &ia, &ja,
+                desca, reinterpret_cast<double __complex__ *>(b), &ib, &jb,
+                descb, &blacs_ctxt);
+    }
+
+    // Use pdgemr2d to collect matrices from all processes to root process
+    template <typename mat>
+    static void gatherMatrix(const mat& mat_l, mat& mat_g)
+    {
+        auto a = mat_l.p;
+        decltype(a) b;
+        int* desca = mat_l.desc;
+        int ctxt = desca[1];
+        int nrows = desca[2];
+        int ncols = desca[3];
+        int nlrows = desca[4];
+        int nlcols = desca[5];
+        int nprows, npcols, myprow, mypcol;
+
+        Cblacs_gridinfo(ctxt, &nprows, &npcols, &myprow, &mypcol);
+        int myid = Cblacs_pnum(ctxt, myprow, mypcol);
+
+        const int ROOT_PROC = Cblacs_pnum(ctxt, desca[6], desca[7]);
+
+        mat_g.row = nrows;
+        mat_g.col = ncols;
+
+        if (myid == ROOT_PROC)
+            b = new typename std::remove_reference<decltype(*a)>::type[nrows * ncols];
+        else
+            b = new typename std::remove_reference<decltype(*a)>::type[1];
+
+        // Set descb, which has all elements in the only block in the root process
+        int descb[9] = {1, ctxt, nrows, ncols, nrows, ncols, 0, 0, nrows};
+        mat_g.desc = descb;
+        mat_g.p = b;
+
+        Cpxgemr2d(nrows, ncols, a, 1, 1, desca, b, 1, 1, descb, ctxt);
+    }
+
+    // Convert the Psi to a 2D block storage format
+    template <typename T>
+    static void distributePsi(const int* desc_psi, T* psi, T* psi_g)
+    {
+        ModuleBase::timer::tick("DiagoCusolver", "distribute");
+        int ctxt = desc_psi[1];
+        int nrows = desc_psi[2];
+        int ncols = desc_psi[3];
+        int rsrc = desc_psi[6];
+        int csrc = desc_psi[7];
+
+        int descg[9] = {1, ctxt, nrows, ncols, nrows, ncols, rsrc, csrc, nrows};
+        int descl[9];
+
+        std::copy(desc_psi, desc_psi + 9, descl);
+
+        Cpxgemr2d(nrows, ncols, psi_g, 1, 1, descg, psi, 1, 1, descl, ctxt);
+
+        ModuleBase::timer::tick("DiagoCusolver", "distribute");
     }
 
     // Diagonalization function for complex numbers
     template <>
-    void DiagoCusolver<std::complex<double>>::diag(hamilt::Hamilt<std::complex<double>>* phm_in,
-                                                   psi::Psi<std::complex<double>>& psi,
-                                                   Real* eigenvalue_in)
+    void DiagoCusolver<complex>::diag(hamilt::Hamilt<complex>* phm_in, psi::Psi<complex>& psi, Real* eigenvalue_in)
     {
         // Output the title for the current operation
         ModuleBase::TITLE("DiagoCusolver", "diag");
@@ -129,29 +128,69 @@ namespace hsolver
         matcd h_mat, s_mat;
         phm_in->matrix(h_mat, s_mat);
 
-        // Calculate the size based on the number of bands and basis functions
-        int size = psi.get_nbands() * psi.get_nbasis();
+#ifdef __MPI
+        // global matrix
+        matcd h_mat_g, s_mat_g;
 
-        // Allocate memory for eigenvalues and eigenvectors
+        // global psi for distribute
+        complex* psi_g;
+
+        // get the context and process information
+        int ctxt = ParaV->blacs_ctxt;
+        int nprows, npcols, myprow, mypcol;
+        Cblacs_gridinfo(ctxt, &nprows, &npcols, &myprow, &mypcol);
+        int myid = Cblacs_pnum(ctxt, myprow, mypcol);
+        const int ROOT_PROC = Cblacs_pnum(ctxt, ParaV->desc[6], ParaV->desc[7]);
+
+#endif
+
+        // Allocate memory for eigenvalues
         std::vector<double> eigen(GlobalV::NLOCAL, 0.0);
-        std::complex<double>* eigenvectors = new std::complex<double>[h_mat.row * h_mat.col];
 
         // Start the timer for the cusolver operation
         ModuleBase::timer::tick("DiagoCusolver", "cusolver");
 
-        // Call the dense complex diagonalization routine
-        this->dc.Dngvd_complex(h_mat.row, h_mat.col, h_mat.p, s_mat.p, eigen.data(), eigenvectors);
+#ifdef __MPI
+        // gather matrices from processes to root process
+        gatherMatrix(h_mat, h_mat_g);
+        gatherMatrix(s_mat, s_mat_g);
+#endif
 
+        // Call the dense complex diagonalization routine
+#ifdef __MPI
+        MPI_Barrier(MPI_COMM_WORLD);
+        if (myid == ROOT_PROC)
+        {
+            psi_g = new complex[h_mat_g.row * h_mat_g.col];
+            this->dc.Dngvd_complex(h_mat_g.col, h_mat_g.row, h_mat_g.p, s_mat_g.p, eigen.data(), psi_g);
+        }
+        else
+        {
+            psi_g = new complex[1];
+        }
+        MPI_Barrier(MPI_COMM_WORLD);
+        // broadcast eigenvalues to all processes
+        MPI_Bcast(eigen.data(), GlobalV::NBANDS, MPI_DOUBLE, ROOT_PROC, MPI_COMM_WORLD);
+
+        // distribute psi to all processes
+        distributePsi(this->ParaV->desc_wfc, psi.get_pointer(), psi_g);
+#else
+        // Call the dense complex diagonalization routine
+        this->dc.Dngvd_complex(h_mat.row, h_mat.col, h_mat.p, s_mat.p, eigen.data(), psi.get_pointer());
+#endif
         // Stop the timer for the cusolver operation
         ModuleBase::timer::tick("DiagoCusolver", "cusolver");
 
         // Copy the eigenvalues and eigenvectors to the output arrays
         const int inc = 1;
         BlasConnector::copy(GlobalV::NBANDS, eigen.data(), inc, eigenvalue_in, inc);
-        BlasConnector::copy(size, eigenvectors, inc, psi.get_pointer(), inc);
 
         // Free allocated memory
-        delete[] eigenvectors;
+#ifdef __MPI
+        delete[] h_mat_g.p;
+        delete[] s_mat_g.p;
+        delete[] psi_g;
+#endif
     }
 
     // Diagonalization function for real numbers
@@ -165,16 +204,21 @@ namespace hsolver
         matd h_mat, s_mat;
         phm_in->matrix(h_mat, s_mat);
 
-        #ifdef __MPI
+#ifdef __MPI
         // global matrix
         matd h_mat_g, s_mat_g;
+
+        // global psi for distribute
         double *psi_g;
-        int ctxt = h_mat.desc[1];
+
+        // get the context and process information
+        int ctxt = ParaV->blacs_ctxt;
         int nprows, npcols, myprow, mypcol;
         Cblacs_gridinfo(ctxt, &nprows, &npcols, &myprow, &mypcol);
         int myid = Cblacs_pnum(ctxt, myprow, mypcol);
-        const int ROOT_PROC = Cblacs_pnum(ctxt, h_mat.desc[6], h_mat.desc[7]);
-        #endif
+        const int ROOT_PROC = Cblacs_pnum(ctxt, ParaV->desc[6], ParaV->desc[7]);
+
+#endif
 
         // Allocate memory for eigenvalues
         std::vector<double> eigen(GlobalV::NLOCAL, 0.0);
@@ -182,17 +226,17 @@ namespace hsolver
         // Start the timer for the cusolver operation
         ModuleBase::timer::tick("DiagoCusolver", "cusolver");
 
-        #ifdef __MPI
-        // gather matrixs from processes
+#ifdef __MPI
+        // gather matrices from processes to root process
         gatherMatrix(h_mat, h_mat_g);
         gatherMatrix(s_mat, s_mat_g);
-        #endif
+#endif
 
         // Call the dense double diagonalization routine
-        #ifdef  __MPI
+#ifdef __MPI
         MPI_Barrier(MPI_COMM_WORLD);
         if (myid == ROOT_PROC)
-        {   
+        {
             psi_g = new double[h_mat_g.row * h_mat_g.col];
             this->dc.Dngvd_double(h_mat_g.col, h_mat_g.row, h_mat_g.p, s_mat_g.p, eigen.data(), psi_g);
         }
@@ -201,24 +245,28 @@ namespace hsolver
             psi_g = new double[1];
         }
         MPI_Barrier(MPI_COMM_WORLD);
+        // broadcast eigenvalues to all processes
         MPI_Bcast(eigen.data(), GlobalV::NBANDS, MPI_DOUBLE, ROOT_PROC, MPI_COMM_WORLD);
-        distributePsi(this->ParaV->desc_wfc, psi.get_pointer(), psi_g);
-        #else
-        this->dc.Dngvd_double(h_mat.col, h_mat.row, h_mat.p, s_mat.p, eigen.data(), psi.get_pointer());
-        #endif
 
-        
+        // distribute psi to all processes
+        distributePsi(this->ParaV->desc_wfc, psi.get_pointer(), psi_g);
+#else
+        this->dc.Dngvd_double(h_mat.col, h_mat.row, h_mat.p, s_mat.p, eigen.data(), psi.get_pointer());
+#endif
+
         // Stop the timer for the cusolver operation
         ModuleBase::timer::tick("DiagoCusolver", "cusolver");
 
         // Copy the eigenvalues to the output array
         const int inc = 1;
         BlasConnector::copy(GlobalV::NBANDS, eigen.data(), inc, eigenvalue_in, inc);
-        #ifdef __MPI
+
+        // Free allocated memory
+#ifdef __MPI
         delete[] h_mat_g.p;
         delete[] s_mat_g.p;
         delete[] psi_g;
-        #endif 
+#endif
     }
 
 } // namespace hsolver
