bf87dc149cc257e220fa29b47ee192112601c0e0
maki49 <1579492865@qq.com>
2024-03-19 11:44:37 +0800

    comments

--- source/module_ri/Exx_LRI_interface.h
+++ source/module_ri/Exx_LRI_interface.h
@@ -27,15 +27,13 @@ public:
     Exx_LRI_Interface(std::shared_ptr<Exx_LRI<Tdata>> exx_ptr) : exx_ptr(exx_ptr) {}
     Exx_LRI_Interface() = delete;
 
-    void write_Hexxs(const std::string &file_name) const;
+    /// read and write Hexxs using cereal
+    void write_Hexxs(const std::string& file_name) const;
     void read_Hexxs(const std::string& file_name);
 
+    /// read and write Hexxs in CSR format
     void write_Hexxs(const std::string& file_name, const UnitCell& ucell) const;
     void read_Hexxs(const std::string& file_name, const UnitCell& ucell);
-
-    std::map<Abfs::Vector3_Order<int>, std::map<size_t, std::map<size_t, Tdata>>>
-        calculate_RI_Tensor_sparse(const double& sparse_threshold,
-            const std::map<int, std::map<TAC, RI::Tensor<Tdata>>>& hR, const UnitCell& ucell)const;
 
     std::vector< std::map<int, std::map<TAC, RI::Tensor<Tdata>>>>& get_Hexxs() const { return this->exx_ptr->Hexxs; }
     
@@ -61,6 +59,13 @@ public:
         int& iter);
     int two_level_step = 0;
 private:
+
+    /// calculate CSR sparse matrix from the global matrix stored with RI::Tensor
+    /// the return type is same as LCAO_Matrix::SR_sparse,  HR_sparse, etc.
+    std::map<Abfs::Vector3_Order<int>, std::map<size_t, std::map<size_t, Tdata>>>
+        calculate_RI_Tensor_sparse(const double& sparse_threshold,
+            const std::map<int, std::map<TAC, RI::Tensor<Tdata>>>& hR, const UnitCell& ucell)const;
+
     std::shared_ptr<Exx_LRI<Tdata>> exx_ptr;
     Mix_DMk_2D mix_DMk_2D;
 };
