e913827db677bde2f5f6c98ad4d6f372077d2c99
AmengFromCDH <91943642+AmengFromCDH@users.noreply.github.com>
2024-04-13 09:34:01 +0800

    fixbug

--- source/module_cell/test/prepare_unitcell.h
+++ source/module_cell/test/prepare_unitcell.h
@@ -75,15 +75,15 @@ public:
 				this->lmaxmax,
 				this->init_vel,
 				this->fixed_axes);
-		delete[] ucell->atom_label;
+		ucell->atom_labe.clear();
 		delete[] ucell->atom_mass;
-		delete[] ucell->pseudo_fn;
+		ucell->pseudo_fn.clear();
 		delete[] ucell->pseudo_type;
 		delete[] ucell->orbital_fn;
 		delete[] ucell->magnet.start_magnetization; //mag set here
-		ucell->atom_label = new std::string[ucell->ntype];
+		ucell->atom_label.resize(ucell->ntype);
 		ucell->atom_mass = new double[ucell->ntype];
-		ucell->pseudo_fn = new std::string[ucell->ntype];
+		ucell->pseudo_fn.resize(ucell->ntype);
 		ucell->pseudo_type = new std::string[ucell->ntype];
 		ucell->orbital_fn = new std::string[ucell->ntype];
 		ucell->magnet.start_magnetization = new double[ucell->ntype]; //mag set here
