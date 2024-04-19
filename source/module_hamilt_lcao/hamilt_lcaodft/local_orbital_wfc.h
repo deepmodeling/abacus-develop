91258e6538247b1e78f642974f0f988de779bd77
kirk0830 <yike.huang@aliyun.com>
2024-04-19 19:02:45 +0800

    begin

--- source/module_hamilt_lcao/hamilt_lcaodft/local_orbital_wfc.h
+++ source/module_hamilt_lcao/hamilt_lcaodft/local_orbital_wfc.h
@@ -12,9 +12,20 @@ class Local_Orbital_wfc
 {
 public:
 
-	Local_Orbital_wfc();
+	Local_Orbital_wfc();    
 	~Local_Orbital_wfc();
-
+    // refactor new implementation: RAII
+    // a more look-looking name would be LocalOrbitalWfc, I suppose...
+    Local_Orbital_wfc(const int& nspin,
+                      const int& nks,
+                      const int& nbands,
+                      const int& nlocal,
+                      const int& gamma_only,
+                      const int& nb2d,
+                      const std::string& ks_solver,
+                      const std::string& readin_dir);
+    //
+    void initialize();
     ///=========================================
     /// grid wfc
     /// used to generate density matrix: LOC.DM_R,
@@ -25,7 +36,10 @@ public:
     std::complex<double>*** wfc_k_grid; // [NK, GlobalV::NBANDS, GlobalV::NLOCAL]
     std::complex<double>* wfc_k_grid2;  // [NK*GlobalV::NBANDS*GlobalV::NLOCAL]
 
+    // pointer to const Parallel_Orbitals object
     const Parallel_Orbitals* ParaV;
+    // pointer to const Grid_Technique object, although have no idea about what it is...
+    // the name of Grid_Technique should be changed to be more informative
     const Grid_Technique* gridt;
 
     /// read wavefunction coefficients: LOWF_*.txt
@@ -102,7 +116,7 @@ public:
     int nks;
 };
 
-#ifdef __MPI
+
 // the function should not be defined here!! mohan 2024-03-28
 template <typename T>
 int Local_Orbital_wfc::set_wfc_grid(int naroc[2],
@@ -116,6 +130,7 @@ int Local_Orbital_wfc::set_wfc_grid(int naroc[2],
                                     int myid,
                                     T** ctot)
 {
+#ifdef __MPI
     ModuleBase::TITLE(" Local_Orbital_wfc", "set_wfc_grid");
     if (!wfc && !ctot)
     {
@@ -142,8 +157,300 @@ int Local_Orbital_wfc::set_wfc_grid(int naroc[2],
 			}
         }
     }
+#endif
     return 0;
 }
 #endif
 
-#endif
+namespace LOWF{
+// standalone python style string split function
+void split(const std::string& src, const char& delimiter, std::vector<std::string>& dest)
+{
+    std::string str = src;
+    std::string substring;
+    std::string::size_type start = 0, index;
+    do
+    {
+        index = str.find_first_of(delimiter, start);
+        if (index != std::string::npos)
+        {
+            substring = str.substr(start, index - start);
+            dest.push_back(substring);
+            start = str.find_first_not_of(delimiter, index);
+            if (start == std::string::npos) return;
+        }
+    } while (index != std::string::npos);
+    // the last token
+    substring = str.substr(start);
+    dest.push_back(substring);
+}
+// standalone python style string startswith function
+bool startswith(const std::string& src, const std::string& prefix)
+{
+    return src.find(prefix) == 0;
+}
+// standalone python style string endswith function
+bool endswith(const std::string& src, const std::string& suffix)
+{
+    return src.rfind(suffix) == src.size() - suffix.size();
+}
+
+template<typename T>
+void cast_to_stdcomplex(const std::string& src, std::vector<T>& dest, const char& delimiter = ' ')
+{
+    // there may be parentheses in the string
+    // so we need to remove them first
+    std::string str = src;
+    str.erase(std::remove(str.begin(), str.end(), '('), str.end());
+    str.erase(std::remove(str.begin(), str.end(), ')'), str.end());
+    std::vector<std::string> tokens;
+    split(str, delimiter, tokens);
+    for (const auto& token : tokens)
+    {
+        dest.push_back(std::stod(token));
+    }
+}
+
+#include <regex>
+#include <cassert>
+// read file named as LOWF_K_*.txt, the name should not be hard-coded
+// structure of LOWF_K_*.txt:
+// [ik] (index of k points)
+// [xk] [yk] [zk]
+// [nb] (number of bands)
+// [nlocal] (number of orbitals)
+// [ib] (band)
+// [energy] (energy of the band)
+// [occ] (Occupations)
+// [real] [imag] [real] [imag] ... (wavefunction coefficients)
+// ...
+// [ib] (band)
+// [energy] (energy of the band)
+// [occ] (Occupations)
+// [real] [imag] [real] [imag] ... (wavefunction coefficients)
+// ...
+void read_abacus_lowf(const std::string& flowf,                 //<[in]  LOWF_K_*.txt
+                      int& ik,                                  //<[out] index of k points
+                      std::vector<int>& kvec_c,                 //<[out] k vector
+                      int& nbands,                              //<[out] number of bands
+                      int& nlocal,                              //<[out] number of orbitals
+                      std::vector<double>& energies,            //<[out] energies of bands
+                      std::vector<double>& occs,                //<[out] occupations
+                      std::vector<std::complex<double>>& lowf)  //<[out] wavefunction coefficients
+{
+    std::ifstream ifs(flowf.c_str());
+    if(!ifs) ModuleBase::WARNING_QUIT("read_abacus_lowf", "open file failed: " + flowf);
+    // will use line-by-line parse
+    std::string line;
+    bool read_kvec = false;
+    int iband = 0;
+    int ilocal = 0;
+    while (std::getline(ifs, line))
+    {
+        // remove leading and trailing whitespaces
+        line = std::regex_replace(line, std::regex("^ +| +$|( ) +"), "$1");
+        if(endswith(line, "(index of k points)"))
+        {
+            std::vector<std::string> result;
+            split(line, ' ', result);
+            ik = std::stoi(result[0]);
+            read_kvec = true;
+            continue;
+        }
+        if(read_kvec)
+        {
+            std::vector<std::string> result;
+            split(line, ' ', result);
+            for (const auto& token : result)
+            {
+                kvec_c.push_back(std::stod(token));
+            }
+            read_kvec = false;
+            continue;
+        }
+        if(endswith(line, "(number of bands)"))
+        {
+            std::vector<std::string> result;
+            split(line, ' ', result);
+            nbands = std::stoi(result[0]);
+        }
+        else if(endswith(line, "(number of orbitals)"))
+        {
+            std::vector<std::string> result;
+            split(line, ' ', result);
+            nlocal = std::stoi(result[0]);
+        }
+        else if(endswith(line, "(band)"))
+        {
+            std::vector<std::string> result;
+            split(line, ' ', result);
+            #ifdef __DEBUG
+            assert (ilocal == 0)||(ilocal == nlocal);
+            #endif
+            iband = std::stoi(result[0]);
+            ilocal = 0; // reset ilocal
+        }
+        else if(endswith(line, "(Ry)"))
+        {
+            std::vector<std::string> result;
+            split(line, ' ', result);
+            energies.push_back(std::stod(result[0]));
+        }
+        else if(endswith(line, "(Occupations)"))
+        {
+            std::vector<std::string> result;
+            split(line, ' ', result);
+            occs.push_back(std::stod(result[0]));
+            #ifdef __DEBUG
+            assert (ilocal == 0);
+            #endif
+        }
+        else// read wavefunction coefficients
+        {
+            std::vector<std::string> result;
+            split(line, ' ', result);
+            for (const auto& token : result)
+            {
+                std::vector<double> temp;
+                cast_to_stdcomplex(token, temp, ' ');
+                for (int i = 0; i < temp.size(); i += 2)
+                {
+                    lowf.push_back(std::complex<double>(temp[i], temp[i + 1]));
+                }
+                ilocal += temp.size() / 2;
+            }
+        }
+    }
+    #ifdef __DEBUG
+    assert (lowf.size() == nbands * nlocal);
+    assert (iband == nbands);
+    assert (ilocal == nlocal);
+    #endif
+}
+// overload for the case of double wavefunction coefficients
+void read_abacus_lowf(const std::string& flowf,                 //<[in]  LOWF_K_*.txt
+                      int& ik,                                  //<[out] index of k points
+                      std::vector<int>& kvec_c,                 //<[out] k vector
+                      int& nbands,                              //<[out] number of bands
+                      int& nlocal,                              //<[out] number of orbitals
+                      std::vector<double>& energies,            //<[out] energies of bands
+                      std::vector<double>& occs,                //<[out] occupations
+                      std::vector<double>& lowf)                //<[out] wavefunction coefficients
+{
+    std::ifstream ifs(flowf.c_str());
+    if(!ifs) ModuleBase::WARNING_QUIT("read_abacus_lowf", "open file failed: " + flowf);
+    // will use line-by-line parse
+    std::string line;
+    bool read_kvec = false;
+    int iband = 0;
+    int ilocal = 0;
+    while (std::getline(ifs, line))
+    {
+        // remove leading and trailing whitespaces
+        line = std::regex_replace(line, std::regex("^ +| +$|( ) +"), "$1");
+        if(endswith(line, "(index of k points)"))
+        {
+            std::vector<std::string> result;
+            split(line, ' ', result);
+            ik = std::stoi(result[0]);
+            read_kvec = true;
+            continue;
+        }
+        if(read_kvec)
+        {
+            std::vector<std::string> result;
+            split(line, ' ', result);
+            for (const auto& token : result)
+            {
+                kvec_c.push_back(std::stod(token));
+            }
+            read_kvec = false;
+            continue;
+        }
+        if(endswith(line, "(number of bands)"))
+        {
+            std::vector<std::string> result;
+            split(line, ' ', result);
+            nbands = std::stoi(result[0]);
+        }
+        else if(endswith(line, "(number of orbitals)"))
+        {
+            std::vector<std::string> result;
+            split(line, ' ', result);
+            nlocal = std::stoi(result[0]);
+        }
+        else if(endswith(line, "(band)"))
+        {
+            std::vector<std::string> result;
+            split(line, ' ', result);
+            #ifdef __DEBUG
+            assert (ilocal == 0)||(ilocal == nlocal);
+            #endif
+            iband = std::stoi(result[0]);
+            ilocal = 0; // reset ilocal
+        }
+        else if(endswith(line, "(Ry)"))
+        {
+            std::vector<std::string> result;
+            split(line, ' ', result);
+            energies.push_back(std::stod(result[0]));
+        }
+        else if(endswith(line, "(Occupations)"))
+        {
+            std::vector<std::string> result;
+            split(line, ' ', result);
+            occs.push_back(std::stod(result[0]));
+            #ifdef __DEBUG
+            assert (ilocal == 0);
+            #endif
+        }
+        else// read wavefunction coefficients
+        {
+            std::vector<std::string> result;
+            split(line, ' ', result);
+            for (const auto& token : result)
+            {
+                lowf.push_back(std::stod(token));
+                ilocal += 1;
+            }
+        }
+    }
+    #ifdef __DEBUG
+    assert (lowf.size() == nbands * nlocal);
+    assert (iband == nbands);
+    assert (ilocal == nlocal);
+    #endif
+}
+
+// function convert local index to global index
+int to_ixx_global(const int& ixx_local, //<[in] local index of something
+                  const int& nxx_block, //<[in] number of something per block
+                  const int& iproc,     //<[in] processor id
+                  const int& nprocs)    //<[in] number of processors
+{
+    int iblock = ixx_local / nxx_block;
+    // locali seems to be index of something but more than the number of blocks,
+    // then the index of block it belongs to can be calculated.
+
+    // then there seems to be a (iblock, iproc) 2-dimensional coordinate
+    // and flattened to a one-dimensional index
+    int temp = iblock * nprocs + iproc;
+    // means the 2d mapping would be:
+    //         proc1, proc2, proc3, proc4, ...
+    // block1
+    // block2
+    // block3
+    // block4
+    // ...
+
+    // then return the index of a two-dimensional mapping like:
+    // irow = (iblock*nprocs + iproc)
+    // icol = locali % nblock
+    // (irow, icol) in a two-dimensional mapping
+    //         block1, block2, block3, block4, ...
+    // ?1
+    // ?2                    (irow, icol)
+    // ...
+
+}
+} // end of namespace LOWF
