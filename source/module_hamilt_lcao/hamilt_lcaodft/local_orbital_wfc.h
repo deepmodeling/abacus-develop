dcd67308c203a5667ec39f0559a35cffe6c83d9f
kirk0830 <yike.huang@aliyun.com>
2024-04-23 10:17:35 +0800

    add some new functions

--- source/module_hamilt_lcao/hamilt_lcaodft/local_orbital_wfc.h
+++ source/module_hamilt_lcao/hamilt_lcaodft/local_orbital_wfc.h
@@ -422,35 +422,272 @@ void read_abacus_lowf(const std::string& flowf,                 //<[in]  LOWF_K_
     #endif
 }
 
-// function convert local index to global index
-int to_ixx_global(const int& ixx_local, //<[in] local index of something
-                  const int& nxx_block, //<[in] number of something per block
-                  const int& iproc,     //<[in] processor id
-                  const int& nprocs)    //<[in] number of processors
+/**
+ * @brief Given there is a two-dimensional strategy (iblock, jproc), which means one block can
+ * distribute its data to all processors. This function convert local index (ixx_local, iproc) pair 
+ * to global 1d flattened index.
+ * in which: 
+ * ixx_local is local index of something on one processor but across all blocks, 
+ * iproc is the index of processor. The need of nxx_block and nprocs will determine the 
+ * stepsize of "pointer" to access the memory.
+ * THIS FUNCTION PARSES THE LAYOUT OF THE MEMORY ACCORDING TO NPROC AND NXX_BLOCK
+ * THIS FUNCTION INDICATES A HARD-CORDED MEMORY LAYOUT/DISTRIBUTION STRATEGY
+ * 
+ * @param ixx_local [in] local index of something on one processor but across all blocks
+ * @param iproc [in] processor id
+ * @param ixx_global [out] global 1d flattened index
+ * @param nxx_block_local [in] number of something per block on one processor
+ * @param nprocs [in] number of processors
+ */
+void to_ixx_global(const int& ixx_local,
+                   const int& iproc,
+                   int& ixx_global,
+                   const int& nxx_block_local,
+                   const int& nprocs)
 {
-    int iblock = ixx_local / nxx_block;
-    // locali seems to be index of something but more than the number of blocks,
+    // ixx_local seems to be index of something but more than the number of blocks,
     // then the index of block it belongs to can be calculated.
+    // for example the ixx_local is 10 and nxx_block is 3, then iblock = 10 / 3 = 3
+    // which means the ixx_local indiced element on the 4th block
+    int iblock = ixx_local / nxx_block_local;          // block index
+    int ixx_block_local = ixx_local % nxx_block_local; // index within a block on one processor
 
     // then there seems to be a (iblock, iproc) 2-dimensional coordinate
     // and flattened to a one-dimensional index
-    int temp = iblock * nprocs + iproc;
     // means the 2d mapping would be:
-    //         proc1, proc2, proc3, proc4, ...
-    // block1
-    // block2
-    // block3
+    //          proc1,   proc2,   proc3,  proc4, ...
+    // block1    0         1        2       3    ...
+    // block2  nprocs+0 nprocs+1 nprocs+2 nprocs+3 ...
+    // block3 2*nprocs+0 2*nprocs+1 2*nprocs+2 2*nprocs+3 ...
     // block4
     // ...
+    // it means each block distributes its data to all processors.
+    // (iblock, jproc) -> ij_blockproc
+    int temp = iblock * nprocs + iproc; // block is the leading dimension
 
-    // then return the index of a two-dimensional mapping like:
-    // irow = (iblock*nprocs + iproc)
-    // icol = locali % nblock
-    // (irow, icol) in a two-dimensional mapping
-    //         block1, block2, block3, block4, ...
-    // ?1
-    // ?2                    (irow, icol)
+    // ixx_local % nxx_block is the local index with in a block
+    // temp * nxx_block is then the shift, then there seems to be a two-dimensional
+    // coordinate like (temp, ixx_block_local).
+    //              xx1_inblock, xx2_inblock, xx3_inblock, ...
+    // block1-proc1
+    // block1-proc2
+    // block1-proc3                RETURN                       <- temp
     // ...
-
+    // block2-proc1
+    // block2-proc2
+    // ...
+    // block3-proc1
+    // ...
+    
+    // In summary it is a mapping from (ixx_local, iproc) to a one-dimensional index
+    // named ixx_global
+    ixx_global = temp * nxx_block_local + ixx_block_local;
 }
+
+/**
+ * @brief Reverse function of to_ixx_global, convert global 1d flattened index to local index (ixx_local, iproc) pair
+ * 
+ * @param ixx_local [out] local index of something on one processor but across all blocks
+ * @param iproc [out] processor id
+ * @param ixx_global [in] global 1d flattened index
+ * @param nxx_block_local [in] number of something per block, or say capacity of something per block
+ * @param nprocs [in] number of processors
+ */
+void to_ixx_local(int& ixx_local,
+                  int& iproc,
+                  const int& ixx_global,
+                  const int& nxx_block_local,
+                  const int& nprocs)
+{
+    int nxx_block = nxx_block_local * nprocs; // number of something collected over all processors
+    int iblock = ixx_global / nxx_block;      // block index
+    int ixx_block = ixx_global % nxx_block;   // index within a block across all processors
+    iproc = ixx_block / nxx_block_local;      // processor index
+
+    int shift = iblock * nxx_block_local;     // shift: how many "something" in front of the "thing" of interest
+    int ixx_block_local = ixx_global % nxx_block_local; // index within a block on one processor
+    ixx_local = shift + ixx_block_local;      // local index
+}
+
+/**
+ * @brief refactored version of function ModuleIO::CTOT2q. Slicing a part of data 
+ * specified by (iproc_dim1, iproc_dim2) tuple from `src` and each element is indiced 
+ * by `global indices` (`ndim1_src` and `ndim2_src` are used to control the range)
+ * to `dst` on 2-dimensional memory space (whose size is ndim1_dst x ndim2_dst).
+ * With this function, any thing can be distributed to all processors, if want the
+ * present distribution strategy.
+ * 
+ * (iproc_dim1, iproc_dim2) identifies one processor, then copy corresponding block
+ * of data from src to dst.
+ * 
+ * @param iproc index of present processor
+ * @param nxx_block_local number of "something" per block per processor
+ * @param dst destination where data copied to
+ * @param ndim1_dst first dimension of destination, always to be nrow
+ * @param ndim2_dst second dimension of destination, always to be ncol
+ * @param src source storing data
+ * @param iproc_dim1 (on MPI 2D Parallel Grid) coordinate i, index of processor for dim1
+ * @param iproc_dim2 (on MPI 2D Parallel Grid) coordinate j, index of processor for dim2
+ * @param nprocs_dim1 (on MPI 2D Parallel Grid) bound of iproc_dim1
+ * @param nprocs_dim2 (on MPI 2D Parallel Grid) bound of iproc_dim2
+ * @param ndim1_src first dimension of source, similar with ndim1_dst but will be larger
+ * @param ndim2_src second dimension of source, similar with ndim2_dst but will be larger
+ */
+void distribute_2d(const int& iproc,             //< [in] (myid)
+                   const int& nxx_block_local,   //< [in] (nb)
+                   double* dst,                  //< [out] (work)
+                   const int& ndim1_dst,         //< [in] (naroc[0])
+                   const int& ndim2_dst,         //< [in] (naroc[1])
+                   double** src,                 //< [in] (CTOT)
+                   const int& iproc_dim1,        //< [in] (iprow)
+                   const int& iproc_dim2,        //< [in] (ipcol)
+                   const int& nprocs_dim1,       //< [in] (dim0)
+                   const int& nprocs_dim2,       //< [in] (dim1)
+                   const int& ndim1_src = -1,    //< [in] (nbands)
+                   const int& ndim2_src = -1)    //< [in] new parameter
+{
+    // FIRST DEFINE THE PROCESSOR INFORMATION
+    // workflow consistency judgement: only processor 0 can output the data
+    // but all processors have the same workflow
+    if(iproc != 0) return;
+    for(int i = 0; i < ndim1_dst; i++)
+    {
+        int ixx_global;
+        to_ixx_global(i, iproc_dim1, ixx_global, nxx_block_local, nprocs_dim1);
+        // check if the global index is out of range
+        if((ixx_global >= ndim1_src)||(ndim1_src < 0)) continue;
+        // if not
+        for(int j = 0; j < ndim2_dst; j++)
+        {
+            int jxx_global;
+            to_ixx_global(j, iproc_dim2, jxx_global, nxx_block_local, nprocs_dim2);
+            // check if the global index is out of range
+            if((jxx_global >= ndim2_src)||(ndim2_src < 0)) continue;
+            // if not, access the memory
+            int flatten_index = i * ndim2_dst + j;
+            dst[flatten_index] = src[ixx_global][jxx_global];
+        }
+    }
+}
+/**
+ * @brief overloaded version of `distribute_2d` function, which is used to distribute complex data
+ * for more information and annotation, see the above function.
+ * 
+ * @param iproc index of present processor
+ * @param nxx_block_local number of "something" per block per processor
+ * @param dst destination where data copied to
+ * @param ndim1_dst first dimension of destination, always to be nrow
+ * @param ndim2_dst second dimension of destination, always to be ncol
+ * @param src source storing data
+ * @param iproc_dim1 index of processor for dim1
+ * @param iproc_dim2 index of processor for dim2
+ * @param nprocs_dim1 number of processors for dim1
+ * @param nprocs_dim2 number of processors for dim2
+ * @param ndim1_src maximal number of the first dimension of source
+ * @param ndim2_src maximal number of the second dimension of source
+ */
+void distribute_2d(const int& iproc,             //< [in] (myid)
+                   const int& nxx_block_local,   //< [in] (nb)
+                   std::complex<double>* dst,    //< [out] (work)
+                   const int& ndim1_dst,         //< [in] (naroc[0])
+                   const int& ndim2_dst,         //< [in] (naroc[1])
+                   std::complex<double>** src,   //< [in] (CTOT)
+                   const int& iproc_dim1,        //< [in] (iprow)
+                   const int& iproc_dim2,        //< [in] (ipcol)
+                   const int& nprocs_dim1,       //< [in] (dim0)
+                   const int& nprocs_dim2,       //< [in] (dim1)
+                   const int& ndim1_src = -1,    //< [in] (nbands)
+                   const int& ndim2_src = -1)    //< [in] new parameter
+{
+    if(iproc != 0) return;
+    for(int i = 0; i < ndim1_dst; i++)
+    {
+        int ixx_global;
+        to_ixx_global(i, iproc_dim1, ixx_global, nxx_block_local, nprocs_dim1);
+        if((ixx_global >= ndim1_src)||(ndim1_src < 0)) continue;
+        for(int j = 0; j < ndim2_dst; j++)
+        {
+            int jxx_global;
+            to_ixx_global(j, iproc_dim2, jxx_global, nxx_block_local, nprocs_dim2);
+            if((jxx_global >= ndim2_src)||(ndim2_src < 0)) continue;
+            int flatten_index = i * ndim2_dst + j;
+            dst[flatten_index] = src[ixx_global][jxx_global];
+        }
+    }
+}
+
+/**
+ * @brief map data distributed from CTOT onto 2D parallel grid to exact wavefunction container
+ * 
+ * @param nxx_block_local number of basis function coefficients per block per processor
+ * @param orb_indices mapping from global index in CTOT to index of numerical atomic orbital
+ * @param src source storing data, which is distributed from CTOT
+ * @param ndim1_src maximal number of the first dimension of source
+ * @param ndim2_src maximal number of the second dimension of source
+ * @param wfc destination storing wavefunction coefficients, is nbands x nlocal
+ * @param iproc_dim1 index of processor for dim1
+ * @param iproc_dim2 index of processor for dim2
+ * @param nprocs_dim1 number of processors for dim1
+ * @param nprocs_dim2 number of processors for dim2
+ */
+void map_onto_wfc(const int& nxx_block_local,
+                  const std::vector<int>& orb_indices,
+                  double* src,
+                  const int& ndim1_src,
+                  const int& ndim2_src,
+                  double** wfc,
+                  const int& iproc_dim1,
+                  const int& iproc_dim2,
+                  const int& nprocs_dim1 = -1,
+                  const int& nprocs_dim2 = -1)
+{
+    if(wfc == nullptr) return;
+    for(int i = 0; i < ndim1_src; i++)
+    {
+        int ixx_global;
+        to_ixx_global(i, iproc_dim1, ixx_global, nxx_block_local, nprocs_dim1);
+        for(int j = 0; j < ndim2_src; j++)
+        {
+            int jxx_global;
+            to_ixx_global(j, iproc_dim2, jxx_global, nxx_block_local, nprocs_dim2);
+            int iorb = orb_indices[jxx_global];
+            #ifdef __DEBUG
+            assert (iorb >= 0);
+            assert (jxx_global >= 0);
+            #endif
+            wfc[ixx_global][iorb] = src[i * ndim2_src + j];
+        }
+    }
+}
+// overloaded version for complex data, for more information and annotation, see the above function
+void map_onto_wfc(const int& nxx_block_local,
+                  const std::vector<int>& orb_indices,
+                  std::complex<double>* src,
+                  const int& ndim1_src,
+                  const int& ndim2_src,
+                  std::complex<double>** wfc,
+                  const int& iproc_dim1,
+                  const int& iproc_dim2,
+                  const int& nprocs_dim1 = -1,
+                  const int& nprocs_dim2 = -1)
+{
+    if(wfc == nullptr) return;
+    for(int i = 0; i < ndim1_src; i++)
+    {
+        int ixx_global;
+        to_ixx_global(i, iproc_dim1, ixx_global, nxx_block_local, nprocs_dim1);
+        for(int j = 0; j < ndim2_src; j++)
+        {
+            int jxx_global;
+            to_ixx_global(j, iproc_dim2, jxx_global, nxx_block_local, nprocs_dim2);
+            int iorb = orb_indices[jxx_global];
+            #ifdef __DEBUG
+            assert (iorb >= 0);
+            assert (jxx_global >= 0);
+            #endif
+            wfc[ixx_global][iorb] = src[i * ndim2_src + j];
+        }
+    }
+}
+
 } // end of namespace LOWF
