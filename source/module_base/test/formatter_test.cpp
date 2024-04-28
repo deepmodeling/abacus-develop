f1ac6f9f227f59c76b04fb3763bf4d1f1612d591
kirk0830 <yike.huang@aliyun.com>
2024-04-28 13:49:57 +0800

    remove the use of formatter in write_HS, add some tests on FmtCore

--- source/module_base/test/formatter_test.cpp
+++ source/module_base/test/formatter_test.cpp
@@ -1,4 +1,81 @@
 #include "module_base/formatter.h"
 #include <iostream>
+#include <gtest/gtest.h>
+
+TEST(FormatterTest, FmtCoreStaticFormatConstCharPtr) {
+  std::string result = FmtCore::format("Hello, %s!", "world");
+  EXPECT_EQ(result, "Hello, world!");
+}
+
+TEST(FormatterTest, FmtCoreStaticFormatStdString) {
+  std::string result = FmtCore::format("Hello, %s!", std::string("world"));
+  EXPECT_EQ(result, "Hello, world!");
+}
+
+TEST(FormatterTest, FmtCoreStaticFormatInt) {
+  std::string result = FmtCore::format("Hello, %d!", 123);
+  EXPECT_EQ(result, "Hello, 123!");
+}
+
+TEST(FormatterTest, FmtCoreStaticFormatDouble) {
+  std::string result = FmtCore::format("Hello, %f!", 123.456);
+  EXPECT_EQ(result, "Hello, 123.456000!");
+}
+
+TEST(FormatterTest, FmtCoreStaticFormatChar) {
+  std::string result = FmtCore::format("Hello, %c!", 'a');
+  EXPECT_EQ(result, "Hello, a!");
+}
+
+TEST(FormatterTest, FmtCoreStaticFormatBool) {
+  std::string result = FmtCore::format("Hello, %s!", true);
+  EXPECT_EQ(result, "Hello, true!");
+}
+
+TEST(FormatterTest, FmtCoreStaticFormatPointer) {
+  std::string result = FmtCore::format("Hello, %p!", (void*)0x12345678);
+  EXPECT_EQ(result, "Hello, 0x12345678!");
+}
+
+TEST(FormatterTest, FmtCoreStaticFormatMultiple) {
+  std::string result = FmtCore::format("Hello, %s! %d %f %c %s %p", "world", 123, 123.456, 'a', true, (void*)0x12345678);
+  EXPECT_EQ(result, "Hello, world! 123 123.456000 a true 0x12345678");
+}
+
+TEST(FormatterTest, FmtCoreStaticFormatEmpty) {
+  std::string result = FmtCore::format("");
+  EXPECT_EQ(result, "");
+}
+
+TEST(FormatterTest, FmtCoreStaticFormatNoPlaceholder) {
+  std::string result = FmtCore::format("Hello, world!");
+  EXPECT_EQ(result, "Hello, world!");
+}
+
+TEST(FormatterTest, FmtCoreStaticFormatNoPlaceholderWithArgs) {
+  std::string result = FmtCore::format("Hello, world!", 123, 123.456, 'a', true, (void*)0x12345678);
+  EXPECT_EQ(result, "Hello, world!");
+}
+
+TEST(FormatterTest, FmtCoreStaticFormatNoArgs) {
+  std::string result = FmtCore::format("Hello, %s!");
+  EXPECT_EQ(result, "Hello, %s!");
+}
+
+TEST(FormatterTest, FmtCoreStaticFormatInvalidPlaceholder) {
+  std::string result = FmtCore::format("Hello, %z!", "world");
+  EXPECT_EQ(result, "Hello, %z!");
+}
+
+TEST(FormatterTest, FmtCoreStaticFormatInvalidPlaceholderWithArgs) {
+  std::string result = FmtCore::format("Hello, %z!", 123, 123.456, 'a', true, (void*)0x12345678);
+  EXPECT_EQ(result, "Hello, %z!");
+}
 
 
+
+
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
--- source/module_io/write_HS.hpp
+++ source/module_io/write_HS.hpp
@@ -4,7 +4,6 @@
 #include "module_base/timer.h"
 #include "module_cell/module_neighbor/sltk_grid_driver.h"
 #include "module_hamilt_pw/hamilt_pwdft/global.h"
-
 /*
 void ModuleIO::save_HS_ccf(const int &iter, const int &Hnnz, const int *colptr_H, const int *rowind_H,
         const double *nzval_H, const double *nzval_S, bool bit)
@@ -101,11 +100,6 @@ void ModuleIO::save_mat(const int istep,
     ModuleBase::GlobalFunc::OUT(GlobalV::ofs_running, "Dimension of " + label + " : ", dim);
 
     std::stringstream ss;
-
-    formatter::PhysicalFmt physfmt;
-    const int width = precision + 4;
-    const std::string fmtstr = "%" + std::to_string(width) + "." + std::to_string(precision) + "f";
-    physfmt.adjust_formatter_flexible(precision, -1, true);
     if (bit)ss << GlobalV::global_out_dir << file_name + "-" + label + "-bit";
     else
     {
@@ -188,6 +182,7 @@ void ModuleIO::save_mat(const int istep,
     else
     {
         std::ofstream g;
+        g << std::setprecision(precision);
 #ifdef __MPI
         if (drank == 0)
         {
@@ -231,7 +226,7 @@ void ModuleIO::save_mat(const int istep,
 
             if (drank == 0)
             {
-                for (int j = (tri ? i : 0); j < dim; j++) g << " " << physfmt.get_p_formatter()->format(line[tri ? j - i : j]);
+                for (int j = (tri ? i : 0); j < dim; j++) g << " " << line[tri ? j - i : j];
                 g << std::endl;
             }
             delete[] line;
@@ -246,12 +241,12 @@ void ModuleIO::save_mat(const int istep,
             std::ofstream g(ss.str().c_str());
 
         g << dim;
-
+        g << std::setprecision(precision);
         for (int i = 0; i < dim; i++)
         {
             for (int j = (tri ? i : 0); j < dim; j++)
             {
-                g << " " << physfmt.get_p_formatter()->format(mat[i * dim + j]);
+                g << " " << mat[i * dim + j];
             }
             g << std::endl;
         }
