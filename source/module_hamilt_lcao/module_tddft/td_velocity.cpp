e6c3664f73ca60c83abceeadfca9e7a9e9c5786e
ESROAMER <1290951107@qq.com>
2024-05-22 17:36:59 +0800

    clang-format new file

--- source/module_hamilt_lcao/module_tddft/td_velocity.cpp
+++ source/module_hamilt_lcao/module_tddft/td_velocity.cpp
@@ -1,7 +1,7 @@
-#include "module_base/timer.h"
 #include "td_velocity.h"
-#include "module_elecstate/potentials/H_TDDFT_pw.h"
 
+#include "module_base/timer.h"
+#include "module_elecstate/potentials/H_TDDFT_pw.h"
 
 bool TD_Velocity::tddft_velocity = false;
 bool TD_Velocity::out_mat_R = false;
@@ -16,126 +16,129 @@ std::vector<ModuleBase::Vector3<double>> TD_Velocity::At_from_file;
 
 TD_Velocity::TD_Velocity()
 {
-  if(init_vecpot_file && istep==-1)
-  {
-    this->read_cart_At();
-  }
-  return;
+    if (init_vecpot_file && istep == -1)
+    {
+        this->read_cart_At();
+    }
+    return;
 }
 TD_Velocity::~TD_Velocity()
 {
-  this->destroy_HS_R_td_sparse();
+    this->destroy_HS_R_td_sparse();
 }
 
-void TD_Velocity::output_cart_At(const std::string &out_dir)
+void TD_Velocity::output_cart_At(const std::string& out_dir)
 {
-  if(GlobalV::MY_RANK==0)
-  {
-    std::string out_file;
-    //generate the output file name
-    out_file= out_dir + "At.dat";
-    std::ofstream ofs;
-    //output title
-    if(istep==0)
+    if (GlobalV::MY_RANK == 0)
     {
-      ofs.open(out_file.c_str(), std::ofstream::out);
-      ofs << std::left 
-          << std::setw(8) << "#istep"
-          << std::setw(15) << "A_x" 
-          << std::setw(15) << "A_y" 
-          << std::setw(15) << "A_z" << std::endl;
+        std::string out_file;
+        // generate the output file name
+        out_file = out_dir + "At.dat";
+        std::ofstream ofs;
+        // output title
+        if (istep == 0)
+        {
+            ofs.open(out_file.c_str(), std::ofstream::out);
+            ofs << std::left << std::setw(8) << "#istep" << std::setw(15) << "A_x" << std::setw(15) << "A_y"
+                << std::setw(15) << "A_z" << std::endl;
+        }
+        else
+        {
+            ofs.open(out_file.c_str(), std::ofstream::app);
+        }
+        // output the vector potential
+        ofs << std::left << std::setw(8) << istep;
+        // divide by 2.0 to get the atomic unit
+        for (int i = 0; i < 3; i++)
+        {
+            ofs << std::scientific << std::setprecision(4) << std::setw(15) << cart_At[i] / 2.0;
+        }
+        ofs << std::endl;
+        ofs.close();
     }
-    else
-    {
-      ofs.open(out_file.c_str(), std::ofstream::app);
-    }
-    //output the vector potential
-    ofs << std::left <<std::setw(8) << istep;
-    // divide by 2.0 to get the atomic unit
-    for(int i=0; i<3; i++){
-        ofs << std::scientific << std::setprecision(4)<< std::setw(15) << cart_At[i]/2.0;
-    }
-    ofs << std::endl;
-    ofs.close();
-  }
-  return;
+    return;
 }
 
-void TD_Velocity::cal_cart_At(const ModuleBase::Vector3<double> a0, 
-                              const ModuleBase::Vector3<double> a1, 
+void TD_Velocity::cal_cart_At(const ModuleBase::Vector3<double> a0,
+                              const ModuleBase::Vector3<double> a1,
                               const ModuleBase::Vector3<double> a2,
                               const ModuleBase::Vector3<double> At)
 {
-  istep++;
-  if(init_vecpot_file)
-  {
-    this->cart_At=At_from_file[istep>max_istep?max_istep:istep];
-  }
-  else
-  {
-    const double l_norm[3]={a0.norm() ,a1.norm() ,a2.norm()};
-	  this->cart_At = a0*At[0]/l_norm[0] + a1*At[1]/l_norm[1] + a2*At[2]/l_norm[2];
-  }
-  //output the vector potential if needed
-  if(out_vecpot==true)
-  {
-    this->output_cart_At(GlobalV::global_out_dir);
-  }
+    istep++;
+    if (init_vecpot_file)
+    {
+        this->cart_At = At_from_file[istep > max_istep ? max_istep : istep];
+    }
+    else
+    {
+        const double l_norm[3] = {a0.norm(), a1.norm(), a2.norm()};
+        this->cart_At = a0 * At[0] / l_norm[0] + a1 * At[1] / l_norm[1] + a2 * At[2] / l_norm[2];
+    }
+    // output the vector potential if needed
+    if (out_vecpot == true)
+    {
+        this->output_cart_At(GlobalV::global_out_dir);
+    }
 }
 
 void TD_Velocity::read_cart_At(void)
 {
 
-  std::string in_file;
-  //generate the input file name
-  in_file= "At.dat";
-  std::ifstream ifs(in_file.c_str());
-  //check if the file is exist
-  if(!ifs)
-  {
-    ModuleBase::WARNING_QUIT("TD_Velocity::read_cart_At", "Cannot open Vector potential file!");
-  }
-  std::string line;
-  std::vector<std::string> str_vec;
-  //use tmp to skip the istep number
-  int tmp=0;
-  while(std::getline(ifs, line))
-  {
-    //A tmporary vector3 to store the data of this line
-    ModuleBase::Vector3<double> At;
-    if(line[0]=='#')
-      continue;
-    std::istringstream iss(line);
-    //skip the istep number
-    if (!(iss >> tmp)) {
-      ModuleBase::WARNING_QUIT("TD_Velocity::read_cart_At", "Error reading istep!");
-    }
-    //read the vector potential
-    double component;
-    // Read three components
-    for(int i = 0; i < 3; i++)
+    std::string in_file;
+    // generate the input file name
+    in_file = "At.dat";
+    std::ifstream ifs(in_file.c_str());
+    // check if the file is exist
+    if (!ifs)
     {
-      if (!(iss >> component)) 
-      {
-        ModuleBase::WARNING_QUIT("TD_Velocity::read_cart_At", "Error reading component " + std::to_string(i+1) + " for istep " + std::to_string(tmp) + "!");
-      }
-      At[i] = component;
+        ModuleBase::WARNING_QUIT("TD_Velocity::read_cart_At", "Cannot open Vector potential file!");
     }
-    //unit transform ,change unit into Ry/bohr/e*t_a.u.
-    At*=2.0;
-    //add the tmporary vector3 to the vector potential vector
-    At_from_file.push_back(At);
-  }
-  //set the max_istep
-  max_istep = At_from_file.size()-1;
-  ifs.close();
+    std::string line;
+    std::vector<std::string> str_vec;
+    // use tmp to skip the istep number
+    int tmp = 0;
+    while (std::getline(ifs, line))
+    {
+        // A tmporary vector3 to store the data of this line
+        ModuleBase::Vector3<double> At;
+        if (line[0] == '#')
+            continue;
+        std::istringstream iss(line);
+        // skip the istep number
+        if (!(iss >> tmp))
+        {
+            ModuleBase::WARNING_QUIT("TD_Velocity::read_cart_At", "Error reading istep!");
+        }
+        // read the vector potential
+        double component;
+        // Read three components
+        for (int i = 0; i < 3; i++)
+        {
+            if (!(iss >> component))
+            {
+                ModuleBase::WARNING_QUIT("TD_Velocity::read_cart_At",
+                                         "Error reading component " + std::to_string(i + 1) + " for istep "
+                                             + std::to_string(tmp) + "!");
+            }
+            At[i] = component;
+        }
+        // unit transform ,change unit into Ry/bohr/e*t_a.u.
+        At *= 2.0;
+        // add the tmporary vector3 to the vector potential vector
+        At_from_file.push_back(At);
+    }
+    // set the max_istep
+    max_istep = At_from_file.size() - 1;
+    ifs.close();
 
-  return;
+    return;
 }
 void TD_Velocity::destroy_HS_R_td_sparse(void)
 {
-  std::map<Abfs::Vector3_Order<int>, std::map<size_t, std::map<size_t, std::complex<double>>>> empty_HR_sparse_td_vel_up;
-  std::map<Abfs::Vector3_Order<int>, std::map<size_t, std::map<size_t, std::complex<double>>>> empty_HR_sparse_td_vel_down;
-  HR_sparse_td_vel[0].swap(empty_HR_sparse_td_vel_up);
-  HR_sparse_td_vel[1].swap(empty_HR_sparse_td_vel_down);
+    std::map<Abfs::Vector3_Order<int>, std::map<size_t, std::map<size_t, std::complex<double>>>>
+        empty_HR_sparse_td_vel_up;
+    std::map<Abfs::Vector3_Order<int>, std::map<size_t, std::map<size_t, std::complex<double>>>>
+        empty_HR_sparse_td_vel_down;
+    HR_sparse_td_vel[0].swap(empty_HR_sparse_td_vel_up);
+    HR_sparse_td_vel[1].swap(empty_HR_sparse_td_vel_down);
 }
--- source/module_hamilt_lcao/module_tddft/td_velocity.h
+++ source/module_hamilt_lcao/module_tddft/td_velocity.h
@@ -1,10 +1,10 @@
 #ifndef TD_VELOCITY_H
 #define TD_VELOCITY_H
-#include <map>
-
-#include "module_base/timer.h"
 #include "module_base/abfs-vector3_order.h"
-//Class to store TDDFT velocity gague infos.
+#include "module_base/timer.h"
+
+#include <map>
+// Class to store TDDFT velocity gague infos.
 class TD_Velocity
 {
   public:
@@ -13,9 +13,9 @@ class TD_Velocity
 
     void init();
 
-    /// @brief Judge if in tddft calculation or not 
+    /// @brief Judge if in tddft calculation or not
     static bool tddft_velocity;
-  
+
     /// @brief switch to control the output of HR
     static bool out_mat_R;
 
@@ -32,21 +32,20 @@ class TD_Velocity
     ModuleBase::Vector3<double> cart_At;
 
     /// @brief calculate the At in cartesian coordinate
-    void cal_cart_At(ModuleBase::Vector3<double> a0, 
-                     ModuleBase::Vector3<double> a1, 
+    void cal_cart_At(ModuleBase::Vector3<double> a0,
+                     ModuleBase::Vector3<double> a1,
                      ModuleBase::Vector3<double> a2,
                      ModuleBase::Vector3<double> At);
-  
-    //For TDDFT velocity gague, to fix the output of HR
-    std::map<Abfs::Vector3_Order<int>, std::map<size_t, std::map<size_t, std::complex<double>>>> HR_sparse_td_vel[2];
 
+    // For TDDFT velocity gague, to fix the output of HR
+    std::map<Abfs::Vector3_Order<int>, std::map<size_t, std::map<size_t, std::complex<double>>>> HR_sparse_td_vel[2];
 
   private:
     /// @brief read At from output file
     void read_cart_At(void);
 
     /// @brief output cart_At to output file
-    void output_cart_At(const std::string &out_dir);
+    void output_cart_At(const std::string& out_dir);
 
     /// @brief store isteps now
     static int istep;
@@ -56,11 +55,9 @@ class TD_Velocity
 
     /// @brief store the read in At_data
     static std::vector<ModuleBase::Vector3<double>> At_from_file;
-  
+
     /// @brief destory HSR data stored
     void destroy_HS_R_td_sparse(void);
-
 };
 
 #endif
-
