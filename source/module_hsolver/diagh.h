4401ab85e96bcf1cba657f73def27374b67a4216
haozhihan <haozhi.han@stu.pku.edu.cn>
2024-05-27 10:11:23 +0800

    update diag_comm_info struct

--- source/module_hsolver/diagh.h
+++ source/module_hsolver/diagh.h
@@ -26,18 +26,15 @@ namespace hsolver
 
 struct diag_comm_info
 {
-#ifdef __MPI
 
-    const MPI_Comm comm;
     const int rank;
     const int nproc;
 
-    diag_comm_info(const MPI_Comm &comm_in, const int rank_in, const int nproc_in) : comm(comm_in), rank(rank_in), nproc(nproc_in) {}
-#else
-    const int rank;
-    const int nproc;
-
+#ifndef __MPI
     diag_comm_info(const int rank_in, const int nproc_in) : rank(rank_in), nproc(nproc_in) {}
+#else
+    const MPI_Comm comm;
+    diag_comm_info(const MPI_Comm &comm_in, const int rank_in, const int nproc_in) : comm(comm_in), rank(rank_in), nproc(nproc_in) {}
 #endif
 };
 
