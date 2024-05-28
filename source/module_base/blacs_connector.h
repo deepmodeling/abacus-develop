abc15a4dcba6b4c44e584d38ff9cf34403fc360e
jinzx10 <jzx016@hotmail.com>
2024-05-28 19:13:27 +0800

    compromise to pyabacus compilation

--- source/module_base/blacs_connector.h
+++ source/module_base/blacs_connector.h
@@ -29,9 +29,6 @@
 #ifndef BLACS_CONNECTOR_H
 #define BLACS_CONNECTOR_H
 
-#ifdef __MPI
-#include <mpi.h>
-
 extern "C"
 {
 	void Cblacs_pinfo(int *myid, int *nprocs);
@@ -44,9 +41,14 @@ extern "C"
     int Cblacs_pnum(int icontxt, int prow, int pcol);
     void Cblacs_pcoord(int icontxt, int pnum, int *prow, int *pcol);
 	void Cblacs_exit(int icontxt);
-    int Csys2blacs_handle(MPI_Comm SysCtxt);
 }
 
+#ifdef __MPI
+#include <mpi.h>
+extern "C"
+{
+    int Csys2blacs_handle(MPI_Comm SysCtxt);
+}
 #endif // __MPI
 
 #endif
