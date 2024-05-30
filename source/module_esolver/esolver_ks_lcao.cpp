cc1dac7f65e7808991eea49526d19ea4e4f65f8a
kirk0830 <yike.huang@aliyun.com>
2024-05-30 12:59:54 +0800

    correct the caplitalization of orb_con.ParaV

--- source/module_esolver/esolver_ks_lcao.cpp
+++ source/module_esolver/esolver_ks_lcao.cpp
@@ -1272,7 +1272,7 @@ void ESolver_KS_LCAO<TK, TR>::after_scf(const int istep)
         RPA_LRI<TK, double> rpa_lri_double(GlobalC::exx_info.info_ri);
         rpa_lri_double.cal_postSCF_exx(*dynamic_cast<const elecstate::ElecStateLCAO<TK>*>(this->pelec)->get_DM(), MPI_COMM_WORLD, this->kv);
         rpa_lri_double.init(MPI_COMM_WORLD, this->kv);
-        rpa_lri_double.out_for_RPA(this->orb_con.paraV, *(this->psi), this->pelec);
+        rpa_lri_double.out_for_RPA(this->orb_con.ParaV, *(this->psi), this->pelec);
     }
 #endif
 
