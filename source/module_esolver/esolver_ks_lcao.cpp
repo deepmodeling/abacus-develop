fe71e34eb21c23469e487dfc28a11d734c5c2ce3
kirk0830 <yike.huang@aliyun.com>
2024-06-02 10:26:29 +0800

    correct for exx

--- source/module_esolver/esolver_ks_lcao.cpp
+++ source/module_esolver/esolver_ks_lcao.cpp
@@ -1069,7 +1069,7 @@ void ESolver_KS_LCAO<TK, TR>::iter_finish(int iter)
     if (GlobalC::restart.info_save.save_H && two_level_step > 0 &&
         (!GlobalC::exx_info.info_global.separate_loop || iter == 1)) // to avoid saving the same value repeatedly
     {
-        std::vector<TK> Hexxk_save(this->orb_con.ParaV->get_local_size());
+        std::vector<TK> Hexxk_save(this->orb_con.ParaV.get_local_size());
         for (int ik = 0; ik < this->kv.get_nks(); ++ik)
         {
             ModuleBase::GlobalFunc::ZEROS(Hexxk_save.data(), Hexxk_save.size());
