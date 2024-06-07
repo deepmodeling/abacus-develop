4889efc3257744528624bf019d6b598acff0d358
kirk0830 <yike.huang@aliyun.com>
2024-06-07 18:19:55 +0800

    move annotations from source file to header

--- source/module_io/read_wfc_lcao.cpp
+++ source/module_io/read_wfc_lcao.cpp
@@ -15,24 +15,6 @@
 #include <regex>
 #include <cassert>
 
-// only when you know why you need the T, you can write function with template parameter, 
-// otherwise, you should overload the function for different types
-// For example in this case, ONLY lowf support to be std::complex<double> and std::complex<float>,
-// not ekb, occ, wk and kvec_c.
-/**
- * @brief Read the wavefunction coefficients from the file (for complex wavefunction coefficients)
- * 
- * @tparam T 
- * @param flowf [in] file name like "LOWF_K_*.txt", dumped from ABACUS INPUT out_wfc_lcao 1 
- * @param ik [out] the index of k points
- * @param kvec_c [out] the k vector in Cartesian coordinates
- * @param nbands [out] the number of bands
- * @param nbasis [out] the number of orbitals
- * @param lowf [out] wavefunction coefficients
- * @param ekb [out] eigenvalues
- * @param occ [out] occupations
- * @param wk [out] weight of k points
- */
 template<typename T>
 void ModuleIO::read_abacus_lowf(const std::string& flowf, 
                                 int& ik,
@@ -131,20 +113,7 @@ std::vector<double>& occ, double& wk);
 template void ModuleIO::read_abacus_lowf(const std::string& flowf, int& ik, ModuleBase::Vector3<double>& kvec_c, 
 int& nbands, int& nbasis, std::vector<std::complex<float>>& lowf, std::vector<double>& ekb, 
 std::vector<double>& occ, double& wk);
-/**
- * @brief Read the wavefunction coefficients from the file (for real wavefunction coefficients)
- * 
- * @tparam T 
- * @param flowf [in] file name like "LOWF_K_*.txt", dumped from ABACUS INPUT out_wfc_lcao 1 
- * @param ik [out] the index of k points
- * @param kvec_c [out] the k vector in Cartesian coordinates
- * @param nbands [out] the number of bands
- * @param nbasis [out] the number of orbitals
- * @param lowf [out] wavefunction coefficients
- * @param ekb [out] eigenvalues
- * @param occ [out] occupations
- * @param wk [out] weight of k points
- */
+
 template<typename T>
 void ModuleIO::read_abacus_lowf(const std::string& flowf, 
                                 int& ik,
@@ -238,23 +207,6 @@ template void ModuleIO::read_abacus_lowf(const std::string& flowf, int& ik, Modu
 int& nbands, int& nbasis, std::vector<float>& lowf, std::vector<double>& ekb, std::vector<double>& occ, double& wk);
 
 #ifdef __MPI
-/**
- * @brief Restart the wavefunction coefficients from the file (MPI 2D-BCD version)
- * 
- * @tparam T: datatype of the wavefunction coefficients, can be double, float, std::complex<double> or std::complex<float>
- * @param out_dir [in] the directory where the wavefunction coefficients are stored
- * @param p2d [in] the 2D parallel distribution
- * @param nks [in] the number of k points
- * @param nbands [out] the number of bands
- * @param nbasis [out] the number of orbitals
- * @param lowf_loc [out] the local wavefunction coefficients, can be used to construct psi, see constructor No.8
- * @param ekb [out] the eigenvalues
- * @param occ [out] the occupations
- * @param kvec_c [out] the k vectors in Cartesian coordinates
- * @param wk [out] the weight of k points
- * 
- * @warning Cpxgemr2d not implemented yet
- */
 template<typename T>
 void ModuleIO::restart_from_file(const std::string& out_dir, // hard-code the file name to be LOWF_K_*.txt?
                                  const Parallel_2D& p2d,
@@ -315,20 +267,7 @@ std::vector<std::complex<double>>& lowf_loc, std::vector<double>& ekb, std::vect
 template void ModuleIO::restart_from_file(const std::string& out_dir, const Parallel_2D& p2d, const int& nks, int& nbands, int& nbasis,
 std::vector<std::complex<float>>& lowf_loc, std::vector<double>& ekb, std::vector<double>& occ, std::vector<ModuleBase::Vector3<double>>& kvec_c, std::vector<double>& wk);
 #endif
-/**
- * @brief Restart the wavefunction coefficients from the file (serial version)
- * 
- * @tparam T: datatype of the wavefunction coefficients, can be double, float, std::complex<double> or std::complex<float>
- * @param out_dir [in] the directory where the wavefunction coefficients are stored
- * @param nks [in] the number of k points
- * @param nbands [out] the number of bands
- * @param nbasis [out] the number of orbitals
- * @param lowf_loc [out] the local wavefunction coefficients, can be used to construct psi, see constructor No.8
- * @param ekb [out] the eigenvalues
- * @param occ [out] the occupations
- * @param kvec_c [out] the k vectors in Cartesian coordinates
- * @param wk [out] the weight of k points
- */
+
 template <typename T>
 void ModuleIO::restart_from_file(const std::string& out_dir, // hard-code the file name to be LOWF_K_*.txt?
                                  const int& nks,
--- source/module_io/read_wfc_lcao.h
+++ source/module_io/read_wfc_lcao.h
@@ -1,6 +1,6 @@
 /**
- * @file lowf_new.h
- * @author kirk0830 (you@domain.com)
+ * @file read_wfc_lcao.h
+ * @author kirk0830
  * @brief This file belongs to the project of removal of class Local_Orbital_Wfc (LOWF)
  * @version 0.1
  * @date 2024-06-04
@@ -28,11 +28,24 @@
  */
 namespace ModuleIO
 {
-// function 1: restart psi from the previous calculation (one single kpoint)
-// in: flowf, 
-// out: lowf(lowf_glb), ekb, occ, kvec_c, wk
-// only rank 0 will do this, but for larger wavefunction, it is needed to seperately
-// read the file
+// only when you know why you need the T, you can write function with template parameter, 
+// otherwise, you should overload the function for different types
+// For example in this case, ONLY lowf support to be std::complex<double> and std::complex<float>,
+// not ekb, occ, wk and kvec_c.
+/**
+ * @brief Read the wavefunction coefficients from the file (for complex wavefunction coefficients)
+ * 
+ * @tparam T 
+ * @param flowf [in] file name like "LOWF_K_*.txt", dumped from ABACUS INPUT out_wfc_lcao 1 
+ * @param ik [out] the index of k points
+ * @param kvec_c [out] the k vector in Cartesian coordinates
+ * @param nbands [out] the number of bands
+ * @param nbasis [out] the number of orbitals
+ * @param lowf [out] wavefunction coefficients
+ * @param ekb [out] eigenvalues
+ * @param occ [out] occupations
+ * @param wk [out] weight of k points
+ */
 template<typename T>
 void read_abacus_lowf(const std::string& flowf, 
                       int& ik,
@@ -43,6 +56,20 @@ void read_abacus_lowf(const std::string& flowf,
                       std::vector<double>& ekb, 
                       std::vector<double>& occ,
                       double& wk);
+/**
+ * @brief Read the wavefunction coefficients from the file (for real wavefunction coefficients)
+ * 
+ * @tparam T 
+ * @param flowf [in] file name like "LOWF_K_*.txt", dumped from ABACUS INPUT out_wfc_lcao 1 
+ * @param ik [out] the index of k points
+ * @param kvec_c [out] the k vector in Cartesian coordinates
+ * @param nbands [out] the number of bands
+ * @param nbasis [out] the number of orbitals
+ * @param lowf [out] wavefunction coefficients
+ * @param ekb [out] eigenvalues
+ * @param occ [out] occupations
+ * @param wk [out] weight of k points
+ */
 template<typename T>
 void read_abacus_lowf(const std::string& flowf, 
                       int& ik,
@@ -59,6 +86,23 @@ void read_abacus_lowf(const std::string& flowf,
 
 // only-MPI-visible function, because the use of comm_world
 #ifdef __MPI
+/**
+ * @brief Restart the wavefunction coefficients from the file (MPI 2D-BCD version)
+ * 
+ * @tparam T: datatype of the wavefunction coefficients, can be double, float, std::complex<double> or std::complex<float>
+ * @param out_dir [in] the directory where the wavefunction coefficients are stored
+ * @param p2d [in] the 2D parallel distribution
+ * @param nks [in] the number of k points
+ * @param nbands [out] the number of bands
+ * @param nbasis [out] the number of orbitals
+ * @param lowf_loc [out] the local wavefunction coefficients, can be used to construct psi, see constructor No.8
+ * @param ekb [out] the eigenvalues
+ * @param occ [out] the occupations
+ * @param kvec_c [out] the k vectors in Cartesian coordinates
+ * @param wk [out] the weight of k points
+ * 
+ * @warning Cpxgemr2d not implemented yet
+ */
 template <typename T>
 void restart_from_file(const std::string& out_dir, // hard-code the file name to be LOWF_K_*.txt?
                        const Parallel_2D& p2d,
@@ -72,6 +116,20 @@ void restart_from_file(const std::string& out_dir, // hard-code the file name to
                        std::vector<double>& wk);
 #endif
 // serial version, can always present
+/**
+ * @brief Restart the wavefunction coefficients from the file (serial version)
+ * 
+ * @tparam T: datatype of the wavefunction coefficients, can be double, float, std::complex<double> or std::complex<float>
+ * @param out_dir [in] the directory where the wavefunction coefficients are stored
+ * @param nks [in] the number of k points
+ * @param nbands [out] the number of bands
+ * @param nbasis [out] the number of orbitals
+ * @param lowf_loc [out] the local wavefunction coefficients, can be used to construct psi, see constructor No.8
+ * @param ekb [out] the eigenvalues
+ * @param occ [out] the occupations
+ * @param kvec_c [out] the k vectors in Cartesian coordinates
+ * @param wk [out] the weight of k points
+ */
 template <typename T>
 void restart_from_file(const std::string& out_dir, // hard-code the file name to be LOWF_K_*.txt?
                        const int& nks,
