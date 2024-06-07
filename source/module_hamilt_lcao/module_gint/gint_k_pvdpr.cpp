e4d57d433ca61a584f1768e40d6b9014b594e4d5
GHS <2733939252@qq.com>
2024-06-08 04:48:06 +0800

    first

--- source/module_hamilt_lcao/module_gint/gint_k_pvdpr.cpp
+++ source/module_hamilt_lcao/module_gint/gint_k_pvdpr.cpp
@@ -10,26 +10,32 @@
 #include "module_basis/module_ao/ORB_read.h"
 #include "module_hamilt_pw/hamilt_pwdft/global.h"
 
+#ifdef _OPENMP
+#include <omp.h>
+#endif
+
 void Gint_k::allocate_pvdpR(void)
 {
-    ModuleBase::TITLE("Gint_k","allocate_pvpR");
+    ModuleBase::TITLE("Gint_k", "allocate_pvpR");
 
     const int nspin = GlobalV::NSPIN;
-    assert(nspin>0);
+    assert(nspin > 0);
 
-    //xiaohui modify 2015-05-30
-    // the number of matrix element <phi_0 | V | dphi_R> is this->gridt->nnrg.
+    // xiaohui modify 2015-05-30
+    //  the number of matrix element <phi_0 | V | dphi_R> is this->gridt->nnrg.
     this->pvdpRx_reduced = new double*[nspin];
     this->pvdpRy_reduced = new double*[nspin];
     this->pvdpRz_reduced = new double*[nspin];
-    for(int is =0;is<nspin;is++)
+
+#pragma omp parallel for
+    for (int is = 0; is < nspin; is++)
     {
-        this->pvdpRx_reduced[is] = new double[this->gridt->nnrg];	
-        ModuleBase::GlobalFunc::ZEROS( pvdpRx_reduced[is], this->gridt->nnrg);
-        this->pvdpRy_reduced[is] = new double[this->gridt->nnrg];	
-        ModuleBase::GlobalFunc::ZEROS( pvdpRy_reduced[is], this->gridt->nnrg);
-        this->pvdpRz_reduced[is] = new double[this->gridt->nnrg];	
-        ModuleBase::GlobalFunc::ZEROS( pvdpRz_reduced[is], this->gridt->nnrg);
+        this->pvdpRx_reduced[is] = new double[this->gridt->nnrg];
+        ModuleBase::GlobalFunc::ZEROS(pvdpRx_reduced[is], this->gridt->nnrg);
+        this->pvdpRy_reduced[is] = new double[this->gridt->nnrg];
+        ModuleBase::GlobalFunc::ZEROS(pvdpRy_reduced[is], this->gridt->nnrg);
+        this->pvdpRz_reduced[is] = new double[this->gridt->nnrg];
+        ModuleBase::GlobalFunc::ZEROS(pvdpRz_reduced[is], this->gridt->nnrg);
     }
 
     ModuleBase::Memory::record("pvdpR_reduced", 3 * sizeof(double) * this->gridt->nnrg * nspin);
@@ -39,28 +45,32 @@ void Gint_k::allocate_pvdpR(void)
 
 void Gint_k::destroy_pvdpR(void)
 {
-    ModuleBase::TITLE("Gint_k","destroy_pvpR");
+    ModuleBase::TITLE("Gint_k", "destroy_pvpR");
 
     const int nspin = GlobalV::NSPIN;
-    assert(nspin>0);
-    
-	for(int is =0;is<nspin;is++) 
-	{
-		delete[] pvdpRx_reduced[is];
-	}
+    assert(nspin > 0);
+
+#pragma omp parallel for
+    for (int is = 0; is < nspin; is++)
+    {
+        delete[] pvdpRx_reduced[is];
+    }
     delete[] pvdpRx_reduced;
 
-	for(int is =0;is<nspin;is++) 
-	{
-		delete[] pvdpRy_reduced[is];
-	}
+#pragma omp parallel for
+    for (int is = 0; is < nspin; is++)
+    {
+        delete[] pvdpRy_reduced[is];
+    }
     delete[] pvdpRy_reduced;
 
-	for(int is =0;is<nspin;is++) 
-	{
-		delete[] pvdpRz_reduced[is];
-	}
+#pragma omp parallel for
+    for (int is = 0; is < nspin; is++)
+    {
+        delete[] pvdpRz_reduced[is];
+    }
     delete[] pvdpRz_reduced;
 
     return;
 }
+
