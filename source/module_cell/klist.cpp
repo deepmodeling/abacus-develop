2d814c1d36b6d972d4328f276d7c30deb50d4156
Haerxile <buhui@stu.pku.edu.cn>
2024-06-08 14:22:43 +0800

    find bug

--- source/module_cell/klist.cpp
+++ source/module_cell/klist.cpp
@@ -687,7 +687,7 @@ void K_Vectors::ibz_kpoint(const ModuleSymmetry::Symmetry& symm,
         auto matequal = [&symm](ModuleBase::Matrix3 a, ModuleBase::Matrix3 b) {
             return (symm.equal(a.e11, b.e11) && symm.equal(a.e12, b.e12) && symm.equal(a.e13, b.e13)
                     && symm.equal(a.e21, b.e21) && symm.equal(a.e22, b.e22) && symm.equal(a.e23, b.e23)
-                    && symm.equal(a.e31, b.e31) && symm.equal(a.e32, b.e32) && symm.equal(a.e33, b.e33));
+                    && symm.equal(a.e31, b.e31) && symm.equal(a.e23, b.e23) && symm.equal(a.e33, b.e33));
         };
         for (int i = 0; i < symm.nrotk; ++i)
         {
