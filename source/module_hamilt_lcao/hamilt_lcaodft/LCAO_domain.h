d47daaa97628991f964796085872684e57e29b1a
mohanchen <mohan.chen.chen.mohan@gmail.com>
2024-06-15 11:43:06 +0800

    add set_force and set_stress in LCAO_domain.h

--- source/module_hamilt_lcao/hamilt_lcaodft/LCAO_domain.h
+++ source/module_hamilt_lcao/hamilt_lcaodft/LCAO_domain.h
@@ -1,23 +1,65 @@
 #ifndef LCAO_DOMAIN_H
 #define LCAO_DOMAIN_H
 
-#include "module_cell/module_neighbor/sltk_atom_arrange.h"
-#include "module_cell/module_neighbor/sltk_grid_driver.h"
-#include "module_hamilt_pw/hamilt_pwdft/global.h"
-#include "module_hamilt_lcao/hamilt_lcaodft/hamilt_lcao.h"
-
+#include "module_hamilt_lcao/module_gint/grid_technique.h"
+#include "module_hamilt_lcao/module_gint/gint_gamma.h"
+#include "module_hamilt_lcao/module_gint/gint_k.h"
 
 namespace LCAO_domain
 {
 
-//! prepare grid integration 
-void grid_prepare(
-		const Grid_Technique& gt, 
-        Gint_Gamma &gint_gamma,
-        Gint_k &gint_k,
-		const ModulePW::PW_Basis& rhopw, 
-		const ModulePW::PW_Basis_Big& bigpw);
+    /**
+     * @brief prepare gird integration
+    */
+	void grid_prepare(
+			const Grid_Technique& gt, 
+			Gint_Gamma &gint_gamma,
+			Gint_k &gint_k,
+			const ModulePW::PW_Basis& rhopw, 
+			const ModulePW::PW_Basis_Big& bigpw);
 
+    /**
+     * @brief set the elements of force-related matrices in LCAO method 
+    */
+    void set_force (
+            const Parallel_Orbitals &pv,
+            const int& iw1_all,
+            const int& iw2_all,
+            const double& vx,
+            const double& vy,
+            const double& vz,
+            const char &dtype,
+            double* dsloc_x,
+            double* dsloc_y,
+            double* dsloc_z,
+            double* dhloc_fixed_x,
+            double* dhloc_fixed_y,
+            double* dhloc_fixed_z);
+
+    /**
+     * @brief set the elements of stress-related matrices in LCAO method 
+    */
+	void set_stress (
+			const Parallel_Orbitals &pv,
+			const int& iw1_all,
+			const int& iw2_all,
+			const double& vx,
+			const double& vy,
+			const double& vz,
+			const char &dtype,
+			const ModuleBase::Vector3<double> &dtau,
+			double* dsloc_11,
+			double* dsloc_12,
+			double* dsloc_13,
+			double* dsloc_22,
+			double* dsloc_23,
+			double* dsloc_33,
+			double* dhloc_fixed_11,
+			double* dhloc_fixed_12,
+			double* dhloc_fixed_13,
+			double* dhloc_fixed_22,
+			double* dhloc_fixed_23,
+			double* dhloc_fixed_33);
 
 }
 
--- source/module_hamilt_lcao/hamilt_lcaodft/LCAO_gen_fixedH.h
+++ source/module_hamilt_lcao/hamilt_lcaodft/LCAO_gen_fixedH.h
@@ -7,6 +7,7 @@
 #include "module_cell/module_neighbor/sltk_grid_driver.h"
 #include "module_hamilt_lcao/hamilt_lcaodft/LCAO_matrix.h"
 #include "module_basis/module_ao/ORB_gen_tables.h"
+#include "module_base/vector3.h"
 
 class LCAO_gen_fixedH
 {
@@ -17,12 +18,67 @@ class LCAO_gen_fixedH
     LCAO_gen_fixedH();
     ~LCAO_gen_fixedH();
 
-    //void calculate_NL_no(double* HlocR);
-    // void calculate_NL_no(std::complex<double>* HlocR);
-    //void calculate_T_no(double* HlocR);
-    // void calculate_T_no(std::complex<double>* HlocR);
-    //void calculate_S_no(double* SlocR);
-    // void calculate_S_no(std::complex<double>* SlocR);
+	void single_overlap(
+			const LCAO_Orbitals& orb,
+			const ORB_gen_tables& uot,
+			const Parallel_Orbitals& pv,
+			const UnitCell& ucell,
+			const int nspin,
+			const bool cal_stress,
+			const int iw1_all,
+            const int iw2_all,
+			const int m1, 
+			const int m2,
+			const char &dtype,
+			const int T1,
+			const int L1,
+			const int N1,
+			const int T2,
+			const int L2,
+			const int N2,
+			const ModuleBase::Vector3<double> &dtau,
+			const ModuleBase::Vector3<double> &tau1,
+			const ModuleBase::Vector3<double> &tau2,
+			const int npol,
+			const int jj,
+			const int jj0,
+			const int kk,
+			const int kk0,
+			int& nnr,  // output value
+			int& total_nnr, // output value
+			double *olm,    // output value
+			double *HSloc); // output value
+
+	void single_derivative(
+			const LCAO_Orbitals& orb,
+			const ORB_gen_tables& uot,
+			const Parallel_Orbitals& pv,
+			const UnitCell& ucell,
+			const int nspin,
+			const bool cal_stress,
+            const int iw1_all,
+            const int iw2_all,
+			const int m1, 
+			const int m2,
+			const char &dtype,
+			const int T1,
+			const int L1,
+			const int N1,
+			const int T2,
+			const int L2,
+			const int N2,
+			const ModuleBase::Vector3<double> &dtau,
+			const ModuleBase::Vector3<double> &tau1,
+			const ModuleBase::Vector3<double> &tau2,
+			const int npol,
+			const int jj,
+			const int jj0,
+			const int kk,
+			const int kk0,
+			int& nnr,  // output value
+			int& total_nnr, // output value
+			double *olm); // output value 
+
     void build_ST_new(const char& dtype,
                       const bool& cal_deri,
                       const UnitCell& ucell,
