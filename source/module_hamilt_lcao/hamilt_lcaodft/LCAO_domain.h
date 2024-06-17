45f89cbb40f839b84fddc6717bba8ab2e83ea635
weiqingzhou <weiqingzhou@whu.edu.cn>
2024-06-17 12:42:19 +0800

    delete lm in single_derivative in .h

--- source/module_hamilt_lcao/hamilt_lcaodft/LCAO_domain.h
+++ source/module_hamilt_lcao/hamilt_lcaodft/LCAO_domain.h
@@ -129,7 +129,6 @@ namespace LCAO_domain
      * @brief set each element of T matrices 
     */
 	void single_derivative(
-            LCAO_Matrix& lm,
             ForceStressArrays& fsr,
 			const LCAO_Orbitals& orb,
 			const ORB_gen_tables& uot,
