5cc4ed12a3f1a5c19f8d79698073c97e16c2e59b
kirk0830 <yike.huang@aliyun.com>
2024-06-21 17:47:21 +0800

    change limit from 50 to 500

--- source/module_basis/module_ao/ORB_atomic.cpp
+++ source/module_basis/module_ao/ORB_atomic.cpp
@@ -45,14 +45,9 @@ void Numerical_Orbital::set_orbital_info
 	}
 
 	// we need this to generate numerical_orbital_lm.
-	if (total_nchi_in < 0) 
+	if (total_nchi_in < 0 || total_nchi_in > 500) 
 	{
-		ModuleBase::WARNING_QUIT("Numerical_Orbital::init", "total_nchi < 0");
-	}
-	else if (total_nchi_in > 50)
-	{
-		// this is a warning, not a quit.
-		ModuleBase::WARNING("Numerical_Orbital::init", "total_nchi > 50");
+		ModuleBase::WARNING_QUIT("Numerical_Orbital::init", "total_nchi < 0 or > 500");
 	}
 	else 
 	{
