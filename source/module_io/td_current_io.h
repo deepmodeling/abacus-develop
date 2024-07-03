bca759b7890b670ec1d9274736b5092da35ac68b
HTZhao <104255052+ESROAMER@users.noreply.github.com>
2024-07-03 23:40:45 +0800

    Update td_current_io.h

--- source/module_io/td_current_io.h
+++ source/module_io/td_current_io.h
@@ -4,7 +4,6 @@
 #include "module_basis/module_nao/two_center_bundle.h"
 #include "module_elecstate/elecstate_lcao.h"
 #include "module_elecstate/module_dm/density_matrix.h"
-#include "module_hamilt_lcao/hamilt_lcaodft/force_stress_arrays.h"
 #include "module_psi/psi.h"
 
 namespace ModuleIO
