bc573897bff07258254ffe5467cbb988c5bc7d77
haozhihan <haozhi.han@stu.pku.edu.cn>
2024-07-07 16:40:27 +0800

    remove chinese

--- source/module_hsolver/diag_const_nums.h
+++ source/module_hsolver/diag_const_nums.h
@@ -11,32 +11,3 @@ struct const_nums
 };
 
 #endif
-
-
-// #ifndef DIAG_CONST_NUMS_H
-// #define DIAG_CONST_NUMS_H
-
-// #include <complex>
-
-// template <typename T>
-// struct const_nums
-// {
-//     T zero;
-//     T one;
-//     T neg_one;
-//     const_nums();
-// };
-
-// // 特化模板以支持 double 类型
-// template <>
-// const_nums<double>::const_nums() : zero(0.0), one(1.0), neg_one(-1.0) {}
-
-// // 特化模板以支持 std::complex<double> 类型
-// template <>
-// const_nums<std::complex<double>>::const_nums() : zero(std::complex<double>(0.0, 0.0)), one(std::complex<double>(1.0, 0.0)), neg_one(std::complex<double>(-1.0, 0.0)) {}
-
-// // 特化模板以支持 std::complex<float> 类型
-// template <>
-// const_nums<std::complex<float>>::const_nums() : zero(std::complex<float>(0.0, 0.0)), one(std::complex<float>(1.0, 0.0)), neg_one(std::complex<float>(-1.0, 0.0)) {}
-
-// #endif // DIAG_CONST_NUMS_H
