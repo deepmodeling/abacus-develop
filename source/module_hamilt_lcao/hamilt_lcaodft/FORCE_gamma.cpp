f7dbc921c6050c275bdd262d676e266ad42d710e
dyzheng <zhengdy@bjaisi.com>
2024-08-14 18:36:22 +0800

    Refactor&Fix: soc force&stress for LCAO

--- source/module_hamilt_lcao/hamilt_lcaodft/FORCE_gamma.cpp
+++ source/module_hamilt_lcao/hamilt_lcaodft/FORCE_gamma.cpp
@@ -106,15 +106,6 @@ void Force_LCAO<double>::allocate(const Parallel_Orbitals& pv,
                               &GlobalC::GridD,
                               nullptr);
 
-    LCAO_domain::build_Nonlocal_mu_new(pv,
-                                       fsr,
-                                       nullptr,
-                                       cal_deri,
-                                       GlobalC::ucell,
-                                       orb,
-                                       *(two_center_bundle.overlap_orb_beta),
-                                       &GlobalC::GridD);
-
     // calculate asynchronous S matrix to output for Hefei-NAMD
     if (PARAM.inp.cal_syns)
     {
@@ -228,7 +219,7 @@ void Force_LCAO<double>::ftable(const bool isforce,
     //tvnl_dphi
     PulayForceStress::cal_pulay_fs(ftvnl_dphi, stvnl_dphi, *dm, ucell, pv, dHx, dHxy, isforce, isstress);
 
-    this->cal_fvnl_dbeta(dm,
+    /*this->cal_fvnl_dbeta(dm,
                          pv,
                          ucell,
                          orb,
@@ -237,7 +228,7 @@ void Force_LCAO<double>::ftable(const bool isforce,
                          isforce,
                          isstress,
                          fvnl_dbeta,
-                         svnl_dbeta);
+                         svnl_dbeta);*/
 
     // vl_dphi
     PulayForceStress::cal_pulay_fs(fvl_dphi, svl_dphi, *dm, ucell, pelec->pot, gint, isforce, isstress, false/*reset dm to gint*/);
--- source/module_hamilt_lcao/hamilt_lcaodft/FORCE_k.cpp
+++ source/module_hamilt_lcao/hamilt_lcaodft/FORCE_k.cpp
@@ -131,16 +131,6 @@ void Force_LCAO<std::complex<double>>::allocate(const Parallel_Orbitals& pv,
                               &GlobalC::GridD,
                               nullptr); // delete lm.Hloc_fixedR
 
-    // calculate dVnl=<phi|dVnl|dphi> in LCAO
-    LCAO_domain::build_Nonlocal_mu_new(pv,
-                                       fsr,
-                                       nullptr,
-                                       cal_deri,
-                                       GlobalC::ucell,
-                                       orb,
-                                       *(two_center_bundle.overlap_orb_beta),
-                                       &GlobalC::GridD);
-
     // calculate asynchronous S matrix to output for Hefei-NAMD
     if (PARAM.inp.cal_syns)
     {
@@ -329,7 +319,7 @@ void Force_LCAO<std::complex<double>>::ftable(const bool isforce,
     // vl_dphi
     PulayForceStress::cal_pulay_fs(fvl_dphi, svl_dphi, *dm, ucell, pelec->pot, gint, isforce, isstress, false/*reset dm to gint*/);
 
-    this->cal_fvnl_dbeta(dm,
+    /*this->cal_fvnl_dbeta(dm,
                          pv,
                          ucell,
                          orb,
@@ -338,7 +328,7 @@ void Force_LCAO<std::complex<double>>::ftable(const bool isforce,
                          isforce,
                          isstress,
                          fvnl_dbeta,
-                         svnl_dbeta);
+                         svnl_dbeta);*/
 
 #ifdef __DEEPKS
     if (PARAM.inp.deepks_scf)
