57f576e4be7f0b9e0f781c9f7ddd05c345022808
Haozhi Han <haozhi.han@stu.pku.edu.cn>
2024-08-23 12:50:24 +0800

    remove this->phsol in SpinConstrain

--- source/module_hamilt_lcao/module_deltaspin/test/spin_constrain_test.cpp
+++ source/module_hamilt_lcao/module_deltaspin/test/spin_constrain_test.cpp
@@ -363,7 +363,7 @@ TYPED_TEST(SpinConstrainTest, SetSolverParameters)
 {
     K_Vectors kv;
     this->sc.set_nspin(4);
-    this->sc.set_solver_parameters(kv, nullptr, nullptr, nullptr, nullptr, "genelpa");
+    this->sc.set_solver_parameters(kv, nullptr, nullptr, nullptr, "genelpa");
     EXPECT_EQ(this->sc.get_nspin(), 4);
     EXPECT_EQ(this->sc.phsol, nullptr);
     EXPECT_EQ(this->sc.p_hamilt, nullptr);
