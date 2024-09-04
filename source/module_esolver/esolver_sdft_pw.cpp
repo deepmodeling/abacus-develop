9125478324359070e93b092e04cdbc95ca1e4549
A-006 <3158793232@qq.com>
2024-09-04 15:34:21 +0800

    change parameter in esolver cpp files

--- source/module_esolver/esolver_sdft_pw.cpp
+++ source/module_esolver/esolver_sdft_pw.cpp
@@ -302,7 +302,7 @@ void ESolver_SDFT_PW::others(const int istep)
 {
     ModuleBase::TITLE("ESolver_SDFT_PW", "others");
 
-    if (GlobalV::CALCULATION == "nscf")
+    if (PARAM.inp.calculation == "nscf")
     {
         this->nscf();
     }
