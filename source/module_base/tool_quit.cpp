08f762fb20810ef7b3e6929fc26fb78295a1cd4e
A-006 <3158793232@qq.com>
2024-09-05 15:31:26 +0800

    change parameter colour in cpp files

--- source/module_base/tool_quit.cpp
+++ source/module_base/tool_quit.cpp
@@ -1,5 +1,6 @@
 #include "tool_quit.h"
 
+#include "module_parameter/parameter.h"
 #ifdef __MPI
 #include "mpi.h"
 #endif
@@ -88,7 +89,7 @@ void WARNING_QUIT(const std::string &file,const std::string &description,int ret
 	// 35: zi
 	// 36: qing
 	// 37: white
-	if(GlobalV::COLOUR)
+	if(PARAM.inp.colour)
 	{
 		//printf( "\e[32m%s\e[0m\n", " -------------- SOMETHING TO WARN YOU ! ------------");
 		//printf( " \e[32m%s\e[0m\n", description.c_str());
