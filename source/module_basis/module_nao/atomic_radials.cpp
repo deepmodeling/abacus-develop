b40d2fe769fd48d53d610fb7bd33ba7180c1f87c
kirk0830 <yike.huang@aliyun.com>
2024-09-23 11:54:15 +0800

    recover the original read_abacus_orb because pyabacus error

--- source/module_basis/module_nao/atomic_radials.cpp
+++ source/module_basis/module_nao/atomic_radials.cpp
@@ -118,33 +118,198 @@ void AtomicRadials::read_abacus_orb(std::ifstream& ifs, std::ofstream* ptr_log, 
     double dr = 0; // grid spacing
     std::string tmp;
 
-    int nr;
-    std::vector<int> nzeta;
-    std::vector<std::vector<double>> radials;
-
-    ModuleIO::read_abacus_orb(ifs, symbol_, orb_ecut_, nr, dr, nzeta, radials, rank);
-
-    lmax_ = nzeta.size() - 1;
-    nzeta_ = new int[lmax_ + 1];
-    std::copy(nzeta.begin(), nzeta.end(), nzeta_);
-
-    nchi_ = std::accumulate(nzeta.begin(), nzeta.end(), 0);
-    nzeta_max_ = *std::max_element(nzeta.begin(), nzeta.end());
-
-    indexing();
-    
-    std::vector<double> rgrid(nr);
-    std::iota(rgrid.begin(), rgrid.end(), 0);
-    std::for_each(rgrid.begin(), rgrid.end(), [dr](double& r) { r *= dr; });
-    chi_ = new NumericalRadial[nchi_];
-    int ichi = 0;
-    for (int l = 0; l <= lmax_; ++l)
+    if (rank == 0)
     {
-        for (int izeta = 0; izeta < nzeta[l]; ++izeta)
+        /*
+         * read the header & grid information, including
+         *
+         * 1. element symbol --> symbol_
+         * 2. energy cutoff --> orb_ecut_
+         * 3. maximum angular momentum --> lmax_
+         * 4. number of radial functions for each angular momentum --> nzeta_
+         * 5. number of grid points --> ngrid
+         * 6. grid spacing --> dr
+         *                                                                              */
+        while (ifs >> tmp)
         {
-            chi_[index(l, izeta)].build(l, true, nr, rgrid.data(), radials[ichi].data(), 0, izeta, symbol_, itype_, false);
-            chi_[index(l, izeta)].normalize();
-            ++ichi;
+            if (tmp == "Element")
+            {
+                ifs >> symbol_;
+            }
+            else if (tmp == "Cutoff(Ry)")
+            {
+                ifs >> orb_ecut_;
+            }
+            else if (tmp == "Lmax")
+            {
+                ifs >> lmax_;
+#ifdef __DEBUG
+                assert(lmax_ >= 0);
+#endif
+                nzeta_ = new int[lmax_ + 1];
+                for (int l = 0; l <= lmax_; ++l)
+                {
+                    ifs >> tmp >> tmp >> tmp >> nzeta_[l]; // skip "Number" "of" "Xorbital-->"
+                }
+            }
+            else if (tmp == "Mesh")
+            {
+                ifs >> ngrid;
+                continue;
+            }
+            else if (tmp == "dr")
+            {
+                ifs >> dr;
+                break;
+            }
         }
+
+        /*
+         * calculate:
+         *
+         * 1. the total number of radial functions --> nchi_
+         * 2. maximum number of radial functions for each angular momentum --> nzeta_max_
+         * 3. a map from (l, izeta) to 1-d array index in chi_
+         *                                                                              */
+        nchi_ = 0;
+        for (int l = 0; l <= lmax_; ++l)
+        {
+            nchi_ += nzeta_[l];
+        }
+        nzeta_max_ = *std::max_element(nzeta_, nzeta_ + lmax_ + 1);
+        indexing(); // build index_map_
     }
+
+#ifdef __MPI
+    Parallel_Common::bcast_string(symbol_);
+    Parallel_Common::bcast_double(orb_ecut_);
+    Parallel_Common::bcast_int(lmax_);
+
+    Parallel_Common::bcast_int(nchi_);
+    Parallel_Common::bcast_int(nzeta_max_);
+
+    Parallel_Common::bcast_int(ngrid);
+    Parallel_Common::bcast_double(dr);
+#endif
+
+    if (rank != 0)
+    {
+        nzeta_ = new int[lmax_ + 1];
+        index_map_ = new int[(lmax_ + 1) * nzeta_max_];
+    }
+
+#ifdef __MPI
+    Parallel_Common::bcast_int(nzeta_, lmax_ + 1);
+    Parallel_Common::bcast_int(index_map_, (lmax_ + 1) * nzeta_max_);
+#endif
+
+    double* rvalue = new double[ngrid];
+    double* rgrid = new double[ngrid];
+    for (int ir = 0; ir != ngrid; ++ir)
+    {
+        rgrid[ir] = ir * dr;
+    }
+    chi_ = new NumericalRadial[nchi_];
+
+    // record whether an orbital has been read or not
+    bool* is_read = new bool[nchi_];
+    for (int i = 0; i != nchi_; ++i)
+    {
+        is_read[i] = false;
+    }
+
+    int l = 0;
+    int izeta = 0;
+    for (int i = 0; i != nchi_; ++i)
+    {
+        if (rank == 0)
+        {
+            /*
+             * read the orbital information, including
+             *
+             * 1. angular momentum
+             * 2. zeta number
+             * 3. values on the grid
+             *                                                                              */
+            // ifs >> tmp >> tmp >> tmp; // skip "Type" "L" "N"
+            ifs >> tmp >> tmp >> tmp;
+#ifdef __DEBUG
+            assert(tmp == "N");
+#endif
+
+            ifs >> tmp >> l >> izeta;
+#ifdef __DEBUG
+            assert(l >= 0 && l <= lmax_);
+            assert(izeta >= 0 && izeta < nzeta_[l]);
+#endif
+
+            for (int ir = 0; ir != ngrid; ++ir)
+            {
+                ifs >> rvalue[ir];
+            }
+        }
+
+#ifdef __MPI
+        Parallel_Common::bcast_int(l);
+        Parallel_Common::bcast_int(izeta);
+        Parallel_Common::bcast_double(rvalue, ngrid);
+#endif
+#ifdef __DEBUG
+        assert(index(l, izeta) >= 0 && index(l, izeta) < nchi_);
+        assert(!is_read[index(l, izeta)]);
+#endif
+        is_read[index(l, izeta)] = true;
+
+        // skip the initialization of sbt_ in this stage
+        chi_[index(l, izeta)].build(l, true, ngrid, rgrid, rvalue, 0, izeta, symbol_, itype_, false);
+        chi_[index(l, izeta)].normalize();
+    }
+
+    delete[] is_read;
+    delete[] rvalue;
+    delete[] rgrid;
 }
+
+// void AtomicRadials::read_abacus_orb(std::ifstream& ifs, std::ofstream* ptr_log, const int rank)
+// {
+//     /*
+//      * Read the orbital file.
+//      *
+//      * For orbital file format, see
+//      * (new) abacus-develop/tools/SIAB/PyTorchGradient/source/IO/print_orbital.py
+//      * (old) abacus-develop/tools/SIAB/SimulatedAnnealing/source/src_spillage/Plot_Psi.cpp
+//      *                                                                                  */
+//     int ngrid = 0; // number of grid points
+//     double dr = 0; // grid spacing
+//     std::string tmp;
+
+//     int nr;
+//     std::vector<int> nzeta;
+//     std::vector<std::vector<double>> radials;
+
+//     ModuleIO::read_abacus_orb(ifs, symbol_, orb_ecut_, nr, dr, nzeta, radials, rank);
+
+//     lmax_ = nzeta.size() - 1;
+//     nzeta_ = new int[lmax_ + 1];
+//     std::copy(nzeta.begin(), nzeta.end(), nzeta_);
+
+//     nchi_ = std::accumulate(nzeta.begin(), nzeta.end(), 0);
+//     nzeta_max_ = *std::max_element(nzeta.begin(), nzeta.end());
+
+//     indexing();
+    
+//     std::vector<double> rgrid(nr);
+//     std::iota(rgrid.begin(), rgrid.end(), 0);
+//     std::for_each(rgrid.begin(), rgrid.end(), [dr](double& r) { r *= dr; });
+//     chi_ = new NumericalRadial[nchi_];
+//     int ichi = 0;
+//     for (int l = 0; l <= lmax_; ++l)
+//     {
+//         for (int izeta = 0; izeta < nzeta[l]; ++izeta)
+//         {
+//             chi_[index(l, izeta)].build(l, true, nr, rgrid.data(), radials[ichi].data(), 0, izeta, symbol_, itype_, false);
+//             chi_[index(l, izeta)].normalize();
+//             ++ichi;
+//         }
+//     }
+// }
