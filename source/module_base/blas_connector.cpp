f8937b2271aa89eade70f194120e5d1193017d94
Critsium-xy <tsfxwbbzxy@163.com>
2024-10-10 21:26:10 +0800

    Remove inline keyword

--- source/module_base/blas_connector.cpp
+++ source/module_base/blas_connector.cpp
@@ -1,24 +1,20 @@
-#include "module_base/blas_connector.h"
+#include "blas_connector.h"
 
-inline
 void BlasConnector::axpy( const int n, const float alpha, const float *X, const int incX, float *Y, const int incY)
 {
 	saxpy_(&n, &alpha, X, &incX, Y, &incY);
 }
 
-inline
 void BlasConnector::axpy( const int n, const double alpha, const double *X, const int incX, double *Y, const int incY)
 {
 	daxpy_(&n, &alpha, X, &incX, Y, &incY);
 }
 
-inline
 void BlasConnector::axpy( const int n, const std::complex<float> alpha, const std::complex<float> *X, const int incX, std::complex<float> *Y, const int incY)
 {
 	caxpy_(&n, &alpha, X, &incX, Y, &incY);
 }
 
-inline
 void BlasConnector::axpy( const int n, const std::complex<double> alpha, const std::complex<double> *X, const int incX, std::complex<double> *Y, const int incY)
 {
 	zaxpy_(&n, &alpha, X, &incX, Y, &incY);
@@ -26,25 +22,21 @@ void BlasConnector::axpy( const int n, const std::complex<double> alpha, const s
 
 
 // x=a*x
-inline
 void BlasConnector::scal( const int n,  const float alpha, float *X, const int incX)
 {
 	sscal_(&n, &alpha, X, &incX);
 }
 
-inline
 void BlasConnector::scal( const int n, const double alpha, double *X, const int incX)
 {
 	dscal_(&n, &alpha, X, &incX);
 }
-	
-inline
+
 void BlasConnector::scal( const int n, const std::complex<float> alpha, std::complex<float> *X, const int incX)
 {
 	cscal_(&n, &alpha, X, &incX);
 }
 
-inline
 void BlasConnector::scal( const int n, const std::complex<double> alpha, std::complex<double> *X, const int incX)
 {
 	zscal_(&n, &alpha, X, &incX);
@@ -52,19 +44,17 @@ void BlasConnector::scal( const int n, const std::complex<double> alpha, std::co
 
 
 // d=x*y
-inline
 float BlasConnector::dot( const int n, const float *X, const int incX, const float *Y, const int incY)
 {
 	return sdot_(&n, X, &incX, Y, &incY);
 }
-inline
+
 double BlasConnector::dot( const int n, const double *X, const int incX, const double *Y, const int incY)
 {
 	return ddot_(&n, X, &incX, Y, &incY);
 }
 
 // C = a * A.? * B.? + b * C
-inline
 void BlasConnector::gemm(const char transa, const char transb, const int m, const int n, const int k,
 	const float alpha, const float *a, const int lda, const float *b, const int ldb,
 	const float beta, float *c, const int ldc)
@@ -74,7 +64,6 @@ void BlasConnector::gemm(const char transa, const char transb, const int m, cons
 		&beta, c, &ldc);
 }
 
-inline
 void BlasConnector::gemm(const char transa, const char transb, const int m, const int n, const int k,
 	const double alpha, const double *a, const int lda, const double *b, const int ldb,
 	const double beta, double *c, const int ldc)
@@ -84,7 +73,6 @@ void BlasConnector::gemm(const char transa, const char transb, const int m, cons
 		&beta, c, &ldc);
 }
 
-inline
 void BlasConnector::gemm(const char transa, const char transb, const int m, const int n, const int k,
     const std::complex<float> alpha, const std::complex<float> *a, const int lda, const std::complex<float> *b, const int ldb,
     const std::complex<float> beta, std::complex<float> *c, const int ldc)
@@ -94,7 +82,6 @@ void BlasConnector::gemm(const char transa, const char transb, const int m, cons
             &beta, c, &ldc);
 }
 
-inline
 void BlasConnector::gemm(const char transa, const char transb, const int m, const int n, const int k,
 	const std::complex<double> alpha, const std::complex<double> *a, const int lda, const std::complex<double> *b, const int ldb,
 	const std::complex<double> beta, std::complex<double> *c, const int ldc)
@@ -104,7 +91,6 @@ void BlasConnector::gemm(const char transa, const char transb, const int m, cons
 		&beta, c, &ldc);
 }
 
-inline
 void BlasConnector::gemv(const char trans, const int m, const int n,
     const double alpha, const double* A, const int lda, const double* X, const int incx,
     const double beta, double* Y, const int incy)
@@ -112,7 +98,6 @@ void BlasConnector::gemv(const char trans, const int m, const int n,
     dgemv_(&trans, &m, &n, &alpha, A, &lda, X, &incx, &beta, Y, &incy);
 }
 
-inline
 void BlasConnector::gemv(const char trans, const int m, const int n,
     const std::complex<float> alpha, const std::complex<float> *A, const int lda, const std::complex<float> *X, const int incx,
     const std::complex<float> beta, std::complex<float> *Y, const int incy)
@@ -120,7 +105,6 @@ void BlasConnector::gemv(const char trans, const int m, const int n,
     cgemv_(&trans, &m, &n, &alpha, A, &lda, X, &incx, &beta, Y, &incy);
 }
 
-inline
 void BlasConnector::gemv(const char trans, const int m, const int n,
     const std::complex<double> alpha, const std::complex<double> *A, const int lda, const std::complex<double> *X, const int incx,
     const std::complex<double> beta, std::complex<double> *Y, const int incy)
@@ -130,32 +114,29 @@ void BlasConnector::gemv(const char trans, const int m, const int n,
 
 
 // out = ||x||_2
-inline
 float BlasConnector::nrm2( const int n, const float *X, const int incX )
 {
 	return snrm2_( &n, X, &incX );
 }
 
-inline
+
 double BlasConnector::nrm2( const int n, const double *X, const int incX )
 {
 	return dnrm2_( &n, X, &incX );
 }
 
-inline
+
 double BlasConnector::nrm2( const int n, const std::complex<double> *X, const int incX )
 {
 	return dznrm2_( &n, X, &incX );
 }
 
 // copies a into b
-inline
 void BlasConnector::copy(const long n, const double *a, const int incx, double *b, const int incy)
 {
 	dcopy_(&n, a, &incx, b, &incy);
 }
 
-inline
 void BlasConnector::copy(const long n, const std::complex<double> *a, const int incx, std::complex<double> *b, const int incy)
 {
 	zcopy_(&n, a, &incx, b, &incy);
--- source/module_base/blas_connector.h
+++ source/module_base/blas_connector.h
@@ -118,76 +118,76 @@ public:
 
 	// Peize Lin add 2016-08-04
 	// y=a*x+y
-	static inline
+	static
 	void axpy( const int n, const float alpha, const float *X, const int incX, float *Y, const int incY);
 
-	static inline
+	static
 	void axpy( const int n, const double alpha, const double *X, const int incX, double *Y, const int incY);
 
-	static inline
+	static
 	void axpy( const int n, const std::complex<float> alpha, const std::complex<float> *X, const int incX, std::complex<float> *Y, const int incY);
 
-	static inline
+	static
 	void axpy( const int n, const std::complex<double> alpha, const std::complex<double> *X, const int incX, std::complex<double> *Y, const int incY);
 
 
 	// Peize Lin add 2016-08-04
 	// x=a*x
-	static inline
+	static
 	void scal( const int n,  const float alpha, float *X, const int incX);
 
-	static inline
+	static
 	void scal( const int n, const double alpha, double *X, const int incX);
 
-	static inline
+	static
 	void scal( const int n, const std::complex<float> alpha, std::complex<float> *X, const int incX);
 
-	static inline
+	static
 	void scal( const int n, const std::complex<double> alpha, std::complex<double> *X, const int incX);
 
 
 	// Peize Lin add 2017-10-27
 	// d=x*y
-	static inline
+	static
 	float dot( const int n, const float *X, const int incX, const float *Y, const int incY);
 
-	static inline
+	static
 	double dot( const int n, const double *X, const int incX, const double *Y, const int incY);
 
 
 	// Peize Lin add 2017-10-27, fix bug trans 2019-01-17
 	// C = a * A.? * B.? + b * C
-	static inline
+	static
 	void gemm(const char transa, const char transb, const int m, const int n, const int k,
 		const float alpha, const float *a, const int lda, const float *b, const int ldb,
 		const float beta, float *c, const int ldc);
 
-	static inline
+	static
 	void gemm(const char transa, const char transb, const int m, const int n, const int k,
 		const double alpha, const double *a, const int lda, const double *b, const int ldb,
 		const double beta, double *c, const int ldc);
 
-    static inline
+    static
     void gemm(const char transa, const char transb, const int m, const int n, const int k,
               const std::complex<float> alpha, const std::complex<float> *a, const int lda, const std::complex<float> *b, const int ldb,
               const std::complex<float> beta, std::complex<float> *c, const int ldc);
 
-	static inline
+	static
 	void gemm(const char transa, const char transb, const int m, const int n, const int k,
 		const std::complex<double> alpha, const std::complex<double> *a, const int lda, const std::complex<double> *b, const int ldb,
 		const std::complex<double> beta, std::complex<double> *c, const int ldc);
 
-    static inline
+    static
     void gemv(const char trans, const int m, const int n,
         const double alpha, const double* A, const int lda, const double* X, const int incx,
         const double beta, double* Y, const int incy);
 
-    static inline
+    static
     void gemv(const char trans, const int m, const int n,
           const std::complex<float> alpha, const std::complex<float> *A, const int lda, const std::complex<float> *X, const int incx,
           const std::complex<float> beta, std::complex<float> *Y, const int incy);
 
-    static inline
+    static
     void gemv(const char trans, const int m, const int n,
               const std::complex<double> alpha, const std::complex<double> *A, const int lda, const std::complex<double> *X, const int incx,
               const std::complex<double> beta, std::complex<double> *Y, const int incy);
@@ -195,21 +195,21 @@ public:
 
 	// Peize Lin add 2018-06-12
 	// out = ||x||_2
-	static inline
+	static
 	float nrm2( const int n, const float *X, const int incX );
 
-	static inline
+	static
 	double nrm2( const int n, const double *X, const int incX );
 
-	static inline
+	static
 	double nrm2( const int n, const std::complex<double> *X, const int incX );
 
 
 	// copies a into b
-	static inline
+	static
 	void copy(const long n, const double *a, const int incx, double *b, const int incy);
 
-	static inline
+	static
 	void copy(const long n, const std::complex<double> *a, const int incx, std::complex<double> *b, const int incy);
 };
 
