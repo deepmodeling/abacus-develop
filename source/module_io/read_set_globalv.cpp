f97d89195efb301e6fd1b09fa04396590022dfc8
YuLiu98 <liuyu@stu.pku.edu.cn>
2024-10-11 17:17:13 +0800

    Fix: read STRU in md restart

--- source/module_io/read_set_globalv.cpp
+++ source/module_io/read_set_globalv.cpp
@@ -23,14 +23,6 @@ void ReadInput::set_globalv(Parameter& para)
         para.sys.global_matrix_dir = to_dir(para.sys.global_matrix_dir);
 
         /// get the global readin directory
-        // if (para.inp.read_file_dir == "auto")
-        // {
-        //     para.sys.global_readin_dir = para.globalv.global_out_dir;
-        // }
-        // else
-        // {
-        //     para.sys.global_readin_dir = para.inp.read_file_dir + '/';
-        // }
         para.sys.global_readin_dir = para.inp.read_file_dir + '/';
         para.sys.global_readin_dir = to_dir(para.sys.global_readin_dir);
 
@@ -38,16 +30,15 @@ void ReadInput::set_globalv(Parameter& para)
         if (para.inp.calculation == "md" && para.mdp.md_restart)
         {
             int istep = current_md_step(para.sys.global_readin_dir);
-            para.sys.global_in_stru = para.sys.global_stru_dir + "STRU_MD_" + std::to_string(istep);
 
-            // if (para.inp.read_file_dir == "auto")
-            // {
-            //     para.sys.global_in_stru = para.sys.global_stru_dir + "STRU_MD_" + std::to_string(istep);
-            // }
-            // else
-            // {
-            //     para.sys.global_in_stru = para.inp.read_file_dir + "STRU_MD_" + std::to_string(istep);
-            // }
+            if (para.inp.read_file_dir == to_dir("OUT." + para.input.suffix))
+            {
+                para.sys.global_in_stru = para.sys.global_stru_dir + "STRU_MD_" + std::to_string(istep);
+            }
+            else
+            {
+                para.sys.global_in_stru = para.inp.read_file_dir + "STRU_MD_" + std::to_string(istep);
+            }
         }
         else
         {
