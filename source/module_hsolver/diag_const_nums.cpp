c6203f6854e36a32123e52acc2ab8bd16fbab74e
Critsium-xy <tsfxwbbzxy@163.com>
2024-10-17 20:09:24 +0800

    Remove useless code

--- source/module_hsolver/diag_const_nums.cpp
+++ source/module_hsolver/diag_const_nums.cpp
@@ -13,15 +13,12 @@ const_nums<double>::const_nums()
 {
     base_device::memory::resize_memory_op<double, base_device::DEVICE_CPU>()(
                         this->cpu_ctx, this->zero, 1);
-    //this->zero = (double*)malloc(sizeof(double));
     this->zero[0] = 0.0;
     base_device::memory::resize_memory_op<double, base_device::DEVICE_CPU>()(
                         this->cpu_ctx, this->one, 1);
-    //this->one = (double*)malloc(sizeof(double));
     this->one[0] = 1.0;
     base_device::memory::resize_memory_op<double, base_device::DEVICE_CPU>()(
                         this->cpu_ctx, this->neg_one, 1);
-    //this->neg_one = (double*)malloc(sizeof(double));
     this->neg_one[0] = -1.0;
 }
 
@@ -31,15 +28,12 @@ const_nums<float>::const_nums()
 {
     base_device::memory::resize_memory_op<float, base_device::DEVICE_CPU>()(
                         this->cpu_ctx, this->zero, 1);
-    //this->zero = (float*)malloc(sizeof(float));
     this->zero[0] = 0.0;
     base_device::memory::resize_memory_op<float, base_device::DEVICE_CPU>()(
                         this->cpu_ctx, this->one, 1);
-    //this->one = (float*)malloc(sizeof(float));
     this->one[0] = 1.0;
     base_device::memory::resize_memory_op<float, base_device::DEVICE_CPU>()(
                         this->cpu_ctx, this->neg_one, 1);
-    //this->neg_one = (float*)malloc(sizeof(float));
     this->neg_one[0] = -1.0;
 }
 
@@ -49,15 +43,12 @@ const_nums<std::complex<double>>::const_nums()
 {
     base_device::memory::resize_memory_op<std::complex<double>, base_device::DEVICE_CPU>()(
                         this->cpu_ctx, this->zero, 1);
-    //this->zero = (std::complex<double>*)malloc(sizeof(std::complex<double>));
     this->zero[0] = std::complex<double>(0.0, 0.0);
     base_device::memory::resize_memory_op<std::complex<double>, base_device::DEVICE_CPU>()(
                         this->cpu_ctx, this->one, 1);
-    //this->one = (std::complex<double>*)malloc(sizeof(std::complex<double>));
     this->one[0] = std::complex<double>(1.0, 0.0);
     base_device::memory::resize_memory_op<std::complex<double>, base_device::DEVICE_CPU>()(
                         this->cpu_ctx, this->neg_one, 1);
-    //this->neg_one = (std::complex<double>*)malloc(sizeof(std::complex<double>));
     this->neg_one[0] = std::complex<double>(-1.0, 0.0);
 }
 
@@ -67,14 +58,11 @@ const_nums<std::complex<float>>::const_nums()
 {
     base_device::memory::resize_memory_op<std::complex<float>, base_device::DEVICE_CPU>()(
                         this->cpu_ctx, this->zero, 1);
-    //this->zero = (std::complex<float>*)malloc(sizeof(std::complex<float>));
     this->zero[0] = std::complex<float>(0.0, 0.0);
     base_device::memory::resize_memory_op<std::complex<float>, base_device::DEVICE_CPU>()(
                         this->cpu_ctx, this->one, 1);
-    //this->one = (std::complex<float>*)malloc(sizeof(std::complex<float>));
     this->one[0] = std::complex<float>(1.0, 0.0);
     base_device::memory::resize_memory_op<std::complex<float>, base_device::DEVICE_CPU>()(
                         this->cpu_ctx, this->neg_one, 1);
-    //this->neg_one = (std::complex<float>*)malloc(sizeof(std::complex<float>));
     this->neg_one[0] = std::complex<float>(-1.0, 0.0);
 }
