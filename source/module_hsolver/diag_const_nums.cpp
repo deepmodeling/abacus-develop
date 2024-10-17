f6c2263d15cf0db6c6939c935d70b166e3ea2e88
Critsium-xy <tsfxwbbzxy@163.com>
2024-10-17 19:27:24 +0800

    Modify pointer usage

--- source/module_hsolver/diag_const_nums.cpp
+++ source/module_hsolver/diag_const_nums.cpp
@@ -13,13 +13,13 @@ const_nums<double>::const_nums()
 {
     base_device::memory::resize_memory_op<double, base_device::DEVICE_CPU>()(
                         this->cpu_ctx, this->zero, 1);
-    *this->zero = 0.0;
+    this->zero[0] = 0.0;
     base_device::memory::resize_memory_op<double, base_device::DEVICE_CPU>()(
                         this->cpu_ctx, this->one, 1);
-    *this->one = 1.0;
+    this->one[0] = 1.0;
     base_device::memory::resize_memory_op<double, base_device::DEVICE_CPU>()(
                         this->cpu_ctx, this->neg_one, 1);
-    *this->neg_one = -1.0;
+    this->neg_one[0] = -1.0;
 }
 
 // Specialize templates to support double types
@@ -28,13 +28,13 @@ const_nums<float>::const_nums()
 {
     base_device::memory::resize_memory_op<float, base_device::DEVICE_CPU>()(
                         this->cpu_ctx, this->zero, 1);
-    *this->zero = 0.0;
+    this->zero[0] = 0.0;
     base_device::memory::resize_memory_op<float, base_device::DEVICE_CPU>()(
                         this->cpu_ctx, this->one, 1);
-    *this->one = 1.0;
+    this->one[0] = 1.0;
     base_device::memory::resize_memory_op<float, base_device::DEVICE_CPU>()(
                         this->cpu_ctx, this->neg_one, 1);
-    *this->neg_one = -1.0;
+    this->neg_one[0] = -1.0;
 }
 
 // Specialized templates to support std:: complex<double>types
@@ -43,13 +43,13 @@ const_nums<std::complex<double>>::const_nums()
 {
     base_device::memory::resize_memory_op<std::complex<double>, base_device::DEVICE_CPU>()(
                         this->cpu_ctx, this->zero, 1);
-    *this->zero = std::complex<double>(0.0, 0.0);
+    this->zero[0] = std::complex<double>(0.0, 0.0);
     base_device::memory::resize_memory_op<std::complex<double>, base_device::DEVICE_CPU>()(
                         this->cpu_ctx, this->one, 1);
-    *this->one = std::complex<double>(1.0, 0.0);
+    this->one[0] = std::complex<double>(1.0, 0.0);
     base_device::memory::resize_memory_op<std::complex<double>, base_device::DEVICE_CPU>()(
                         this->cpu_ctx, this->neg_one, 1);
-    *this->neg_one = std::complex<double>(-1.0, 0.0);
+    this->neg_one[0] = std::complex<double>(-1.0, 0.0);
 }
 
 // Specialized templates to support std:: complex<float>types
@@ -58,11 +58,11 @@ const_nums<std::complex<float>>::const_nums()
 {
     base_device::memory::resize_memory_op<std::complex<float>, base_device::DEVICE_CPU>()(
                         this->cpu_ctx, this->zero, 1);
-    *this->zero = std::complex<float>(0.0, 0.0);
+    this->zero[0] = std::complex<float>(0.0, 0.0);
     base_device::memory::resize_memory_op<std::complex<float>, base_device::DEVICE_CPU>()(
                         this->cpu_ctx, this->one, 1);
-    *this->one = std::complex<float>(1.0, 0.0);
+    this->one[0] = std::complex<float>(1.0, 0.0);
     base_device::memory::resize_memory_op<std::complex<float>, base_device::DEVICE_CPU>()(
                         this->cpu_ctx, this->neg_one, 1);
-    *this->neg_one = std::complex<float>(-1.0, 0.0);
+    this->neg_one[0] = std::complex<float>(-1.0, 0.0);
 }
