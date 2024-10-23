8b2f66a1eba498ceb4578163344a21e57347c11c
chenxub <chenhsu.bai@gmail.com>
2024-10-23 19:19:31 +0800

    Refactor hsolver module and remove unused code

--- python/pyabacus/src/py_abacus.cpp
+++ python/pyabacus/src/py_abacus.cpp
@@ -12,5 +12,5 @@ PYBIND11_MODULE(_core, m)
     m.doc() = "Python extension for ABACUS built with pybind11 and scikit-build.";
     bind_base_math(m);
     bind_m_nao(m);
-    bind_hsolver(m);
+    // bind_hsolver(m);
 }
