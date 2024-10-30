f188af485a565b51e85e458b0472eef007f3e5a0
Critsium-xy <tsfxwbbzxy@163.com>
2024-10-30 10:02:17 +0800

    Change memory_op construction

--- source/module_base/module_device/memory_op.cpp
+++ source/module_base/module_device/memory_op.cpp
@@ -22,17 +22,9 @@ struct resize_memory_op<FPTYPE, base_device::DEVICE_CPU>
     {
         if (arr != nullptr)
         {
-#ifdef __DSP
-            free_ht(arr);
-#else
             free(arr);
-#endif
         }
-#ifdef __DSP
-        arr = (FPTYPE*)malloc_ht(sizeof(FPTYPE) * size, GlobalV::MY_RANK);
-#else
         arr = (FPTYPE*)malloc(sizeof(FPTYPE) * size);
-#endif
         std::string record_string;
         if (record_in != nullptr)
         {
@@ -104,11 +96,7 @@ struct delete_memory_op<FPTYPE, base_device::DEVICE_CPU>
 {
     void operator()(const base_device::DEVICE_CPU* dev, FPTYPE* arr)
     {
-#ifdef __DSP
-        free_ht(arr);
-#else
         free(arr);
-#endif
     }
 };
 
@@ -356,6 +344,244 @@ template struct delete_memory_op<float, base_device::DEVICE_GPU>;
 template struct delete_memory_op<double, base_device::DEVICE_GPU>;
 template struct delete_memory_op<std::complex<float>, base_device::DEVICE_GPU>;
 template struct delete_memory_op<std::complex<double>, base_device::DEVICE_GPU>;
+#endif
+
+#ifdef __DSP
+
+template <typename FPTYPE>
+struct resize_memory_op<FPTYPE, base_device::DEVICE_DSP>
+{
+    void operator()(const base_device::DEVICE_DSP* dev, FPTYPE*& arr, const size_t size, const char* record_in)
+    {
+        if (arr != nullptr)
+        {
+            free_ht(arr);
+        }
+        arr = (FPTYPE*)malloc_ht(sizeof(FPTYPE) * size);
+        std::string record_string;
+        if (record_in != nullptr)
+        {
+            record_string = record_in;
+        }
+        else
+        {
+            record_string = "no_record";
+        }
+
+        if (record_string != "no_record")
+        {
+            ModuleBase::Memory::record(record_string, sizeof(FPTYPE) * size);
+        }
+    }
+};
+
+template <typename FPTYPE>
+struct synchronize_memory_op<FPTYPE, base_device::DEVICE_DSP, base_device::DEVICE_CPU>
+{
+    void operator()(const base_device::DEVICE_DSP* dev_out,
+                    const base_device::DEVICE_CPU* dev_in,
+                    FPTYPE* arr_out,
+                    const FPTYPE* arr_in,
+                    const size_t size)
+    {
+        ModuleBase::OMP_PARALLEL([&](int num_thread, int thread_id) {
+            int beg = 0, len = 0;
+            ModuleBase::BLOCK_TASK_DIST_1D(num_thread, thread_id, size, (size_t)4096 / sizeof(FPTYPE), beg, len);
+            memcpy(arr_out + beg, arr_in + beg, sizeof(FPTYPE) * len);
+        });
+    }
+};
+
+template <typename FPTYPE>
+struct synchronize_memory_op<FPTYPE, base_device::DEVICE_CPU, base_device::DEVICE_DSP>
+{
+    void operator()(const base_device::DEVICE_CPU* dev_out,
+                    const base_device::DEVICE_DSP* dev_in,
+                    FPTYPE* arr_out,
+                    const FPTYPE* arr_in,
+                    const size_t size)
+    {
+        ModuleBase::OMP_PARALLEL([&](int num_thread, int thread_id) {
+            int beg = 0, len = 0;
+            ModuleBase::BLOCK_TASK_DIST_1D(num_thread, thread_id, size, (size_t)4096 / sizeof(FPTYPE), beg, len);
+            memcpy(arr_out + beg, arr_in + beg, sizeof(FPTYPE) * len);
+        });
+    }
+};
+
+template <typename FPTYPE>
+struct synchronize_memory_op<FPTYPE, base_device::DEVICE_DSP, base_device::DEVICE_DSP>
+{
+    void operator()(const base_device::DEVICE_DSP* dev_out,
+                    const base_device::DEVICE_DSP* dev_in,
+                    FPTYPE* arr_out,
+                    const FPTYPE* arr_in,
+                    const size_t size)
+    {
+        ModuleBase::OMP_PARALLEL([&](int num_thread, int thread_id) {
+            int beg = 0, len = 0;
+            ModuleBase::BLOCK_TASK_DIST_1D(num_thread, thread_id, size, (size_t)4096 / sizeof(FPTYPE), beg, len);
+            memcpy(arr_out + beg, arr_in + beg, sizeof(FPTYPE) * len);
+        });
+    }
+};
+
+
+template <typename FPTYPE_out, typename FPTYPE_in>
+struct cast_memory_op<FPTYPE_out, FPTYPE_in, base_device::DEVICE_CPU, base_device::DEVICE_DSP>
+{
+    void operator()(const base_device::DEVICE_CPU* dev_out,
+                    const base_device::DEVICE_DSP* dev_in,
+                    FPTYPE_out* arr_out,
+                    const FPTYPE_in* arr_in,
+                    const size_t size)
+    {
+#ifdef _OPENMP
+#pragma omp parallel for schedule(static, 4096 / sizeof(FPTYPE_out))
+#endif
+        for (int ii = 0; ii < size; ii++)
+        {
+            arr_out[ii] = static_cast<FPTYPE_out>(arr_in[ii]);
+        }
+    }
+};
+
+template <typename FPTYPE_out, typename FPTYPE_in>
+struct cast_memory_op<FPTYPE_out, FPTYPE_in, base_device::DEVICE_DSP, base_device::DEVICE_DSP>
+{
+    void operator()(const base_device::DEVICE_DSP* dev_out,
+                    const base_device::DEVICE_DSP* dev_in,
+                    FPTYPE_out* arr_out,
+                    const FPTYPE_in* arr_in,
+                    const size_t size)
+    {
+#ifdef _OPENMP
+#pragma omp parallel for schedule(static, 4096 / sizeof(FPTYPE_out))
+#endif
+        for (int ii = 0; ii < size; ii++)
+        {
+            arr_out[ii] = static_cast<FPTYPE_out>(arr_in[ii]);
+        }
+    }
+};
+
+template <typename FPTYPE_out, typename FPTYPE_in>
+struct cast_memory_op<FPTYPE_out, FPTYPE_in, base_device::DEVICE_DSP, base_device::DEVICE_CPU>
+{
+    void operator()(const base_device::DEVICE_DSP* dev_out,
+                    const base_device::DEVICE_CPU* dev_in,
+                    FPTYPE_out* arr_out,
+                    const FPTYPE_in* arr_in,
+                    const size_t size)
+    {
+#ifdef _OPENMP
+#pragma omp parallel for schedule(static, 4096 / sizeof(FPTYPE_out))
+#endif
+        for (int ii = 0; ii < size; ii++)
+        {
+            arr_out[ii] = static_cast<FPTYPE_out>(arr_in[ii]);
+        }
+    }
+};
+
+template <typename FPTYPE>
+struct delete_memory_op<FPTYPE, base_device::DEVICE_DSP>
+{
+    void operator()(const base_device::DEVICE_DSP* dev, FPTYPE* arr)
+    {
+        free_ht(arr);
+    }
+};
+
+
+template struct resize_memory_op<int, base_device::DEVICE_DSP>;
+template struct resize_memory_op<float, base_device::DEVICE_DSP>;
+template struct resize_memory_op<double, base_device::DEVICE_DSP>;
+template struct resize_memory_op<std::complex<float>, base_device::DEVICE_DSP>;
+template struct resize_memory_op<std::complex<double>, base_device::DEVICE_DSP>;
+
+template struct synchronize_memory_op<int, base_device::DEVICE_CPU, base_device::DEVICE_DSP>;
+template struct synchronize_memory_op<int, base_device::DEVICE_DSP, base_device::DEVICE_CPU>;
+template struct synchronize_memory_op<int, base_device::DEVICE_DSP, base_device::DEVICE_DSP>;
+template struct synchronize_memory_op<float, base_device::DEVICE_CPU, base_device::DEVICE_DSP>;
+template struct synchronize_memory_op<float, base_device::DEVICE_DSP, base_device::DEVICE_CPU>;
+template struct synchronize_memory_op<float, base_device::DEVICE_DSP, base_device::DEVICE_DSP>;
+template struct synchronize_memory_op<double, base_device::DEVICE_CPU, base_device::DEVICE_DSP>;
+template struct synchronize_memory_op<double, base_device::DEVICE_DSP, base_device::DEVICE_CPU>;
+template struct synchronize_memory_op<double, base_device::DEVICE_DSP, base_device::DEVICE_DSP>;
+template struct synchronize_memory_op<std::complex<float>, base_device::DEVICE_CPU, base_device::DEVICE_DSP>;
+template struct synchronize_memory_op<std::complex<float>, base_device::DEVICE_DSP, base_device::DEVICE_CPU>;
+template struct synchronize_memory_op<std::complex<float>, base_device::DEVICE_DSP, base_device::DEVICE_DSP>;
+template struct synchronize_memory_op<std::complex<double>, base_device::DEVICE_CPU, base_device::DEVICE_DSP>;
+template struct synchronize_memory_op<std::complex<double>, base_device::DEVICE_DSP, base_device::DEVICE_CPU>;
+template struct synchronize_memory_op<std::complex<double>, base_device::DEVICE_DSP, base_device::DEVICE_DSP>;
+
+template struct cast_memory_op<float, float, base_device::DEVICE_DSP, base_device::DEVICE_DSP>;
+template struct cast_memory_op<double, double, base_device::DEVICE_DSP, base_device::DEVICE_DSP>;
+template struct cast_memory_op<float, double, base_device::DEVICE_DSP, base_device::DEVICE_DSP>;
+template struct cast_memory_op<double, float, base_device::DEVICE_DSP, base_device::DEVICE_DSP>;
+template struct cast_memory_op<std::complex<float>,
+                               std::complex<float>,
+                               base_device::DEVICE_DSP,
+                               base_device::DEVICE_DSP>;
+template struct cast_memory_op<std::complex<double>,
+                               std::complex<double>,
+                               base_device::DEVICE_DSP,
+                               base_device::DEVICE_DSP>;
+template struct cast_memory_op<std::complex<float>,
+                               std::complex<double>,
+                               base_device::DEVICE_DSP,
+                               base_device::DEVICE_DSP>;
+template struct cast_memory_op<std::complex<double>,
+                               std::complex<float>,
+                               base_device::DEVICE_DSP,
+                               base_device::DEVICE_DSP>;
+template struct cast_memory_op<float, float, base_device::DEVICE_DSP, base_device::DEVICE_CPU>;
+template struct cast_memory_op<double, double, base_device::DEVICE_DSP, base_device::DEVICE_CPU>;
+template struct cast_memory_op<float, double, base_device::DEVICE_DSP, base_device::DEVICE_CPU>;
+template struct cast_memory_op<double, float, base_device::DEVICE_DSP, base_device::DEVICE_CPU>;
+template struct cast_memory_op<std::complex<float>,
+                               std::complex<float>,
+                               base_device::DEVICE_DSP,
+                               base_device::DEVICE_CPU>;
+template struct cast_memory_op<std::complex<double>,
+                               std::complex<double>,
+                               base_device::DEVICE_DSP,
+                               base_device::DEVICE_CPU>;
+template struct cast_memory_op<std::complex<float>,
+                               std::complex<double>,
+                               base_device::DEVICE_DSP,
+                               base_device::DEVICE_CPU>;
+template struct cast_memory_op<std::complex<double>,
+                               std::complex<float>,
+                               base_device::DEVICE_DSP,
+                               base_device::DEVICE_CPU>;
+template struct cast_memory_op<float, float, base_device::DEVICE_CPU, base_device::DEVICE_DSP>;
+template struct cast_memory_op<double, double, base_device::DEVICE_CPU, base_device::DEVICE_DSP>;
+template struct cast_memory_op<float, double, base_device::DEVICE_CPU, base_device::DEVICE_DSP>;
+template struct cast_memory_op<double, float, base_device::DEVICE_CPU, base_device::DEVICE_DSP>;
+template struct cast_memory_op<std::complex<float>,
+                               std::complex<float>,
+                               base_device::DEVICE_CPU,
+                               base_device::DEVICE_DSP>;
+template struct cast_memory_op<std::complex<double>,
+                               std::complex<double>,
+                               base_device::DEVICE_CPU,
+                               base_device::DEVICE_DSP>;
+template struct cast_memory_op<std::complex<float>,
+                               std::complex<double>,
+                               base_device::DEVICE_CPU,
+                               base_device::DEVICE_DSP>;
+template struct cast_memory_op<std::complex<double>,
+                               std::complex<float>,
+                               base_device::DEVICE_CPU,
+                               base_device::DEVICE_DSP>;
+
+template struct delete_memory_op<int, base_device::DEVICE_DSP>;
+template struct delete_memory_op<float, base_device::DEVICE_DSP>;
+template struct delete_memory_op<double, base_device::DEVICE_DSP>;
+template struct delete_memory_op<std::complex<float>, base_device::DEVICE_DSP>;
+template struct delete_memory_op<std::complex<double>, base_device::DEVICE_DSP>;
 #endif
 
 } // namespace memory
