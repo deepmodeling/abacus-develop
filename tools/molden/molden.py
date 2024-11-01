6a283773c2ef49a8fb48e68889ba80589f639b7e
kirk0830 <yike.huang@aliyun.com>
2024-11-01 14:16:56 +0800

    minor fix

--- tools/molden/molden.py
+++ tools/molden/molden.py
@@ -905,7 +905,8 @@ def moldengen(folder: str, ndigits=3, ngto=7, rel_r=2, fmolden="ABACUS.molden"):
     import numpy as np
 
     files = os.listdir(folder)
-    assert ("STRU" in files) and ("INPUT" in files) and ("KPT" in files)
+    assert ("STRU" in files) and ("INPUT" in files) and ("KPT" in files),\
+        "STRU, INPUT, KPT files are required in the folder."
     cwd = os.path.abspath(os.getcwd())
     os.chdir(folder)
     ####################
