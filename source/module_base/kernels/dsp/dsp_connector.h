657661488064d30ef9d6da8eafd07d22b3ff7f94
Critsium-xy <tsfxwbbzxy@163.com>
2024-11-04 11:56:11 +0800

    Fix bug 2

--- source/module_base/kernels/dsp/dsp_connector.h
+++ source/module_base/kernels/dsp/dsp_connector.h
@@ -68,13 +68,12 @@ void cgemm_mth_(const char *transa, const char *transb,
 
 // The next is dsp utils. It may be moved to other files if this file get too huge
 
-base_device::DEVICE_CPU* cpu_ctx = {};
 base_device::AbacusDevice_t device = {};
 
 template <typename T>
-void dsp_dav_subspace_reduce(T* hcc, T* scc, int nbase_x, int notconv, MPI_Comm diag_comm){
+void dsp_dav_subspace_reduce(T* hcc, T* scc, nbase, int nbase_x, int notconv, MPI_Comm diag_comm){
 
-	using syncmem_complex_op = base_device::memory::synchronize_memory_op<T, Device, Device>;
+	using syncmem_complex_op = base_device::memory::synchronize_memory_op<T, DEVICE_CPU, DEVICE_CPU>;
 
 	auto* swap = new T[notconv * nbase_x];
     auto* target = new T[notconv * nbase_x];
