ba5b170b0066075c42c217d61d1d60ee3b904abd
maki49 <1579492865@qq.com>
2024-11-06 11:24:42 +0800

    rename 2order_nested_ptr

--- source/module_lr/utils/lr_util.h
+++ source/module_lr/utils/lr_util.h
@@ -51,10 +51,10 @@ namespace LR_Util
     //====== newers and deleters========
     /// @brief  delete 2d pointer  
     template <typename T>
-    void delete_pointer_dim2(T** p2, size_t size);
+    void _deallocate_2order_nested_ptr(T** p2, size_t size);
     /// @brief  new 2d pointer  
     template <typename T>
-    void new_pointer_dim2(T**& p2, size_t size1, size_t size2);
+    void _allocate_2order_nested_ptr(T**& p2, size_t size1, size_t size2);
 
     template<typename T> ct::Tensor newTensor(const ct::TensorShape& shape)
     {
