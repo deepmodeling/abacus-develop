55082f555a626005064f8eb9efea6d965bc386eb
YuLiu98 <liuyu@stu.pku.edu.cn>
2024-11-14 15:53:43 +0800

    Refactor: move cal_edm_tddft to module_dm

---
+++ source/module_elecstate/module_dm/cal_edm_tddft.h
@@ -0,0 +1,16 @@
+#ifndef CAL_DM_PSI_H
+#define CAL_DM_PSI_H
+
+#include "module_basis/module_ao/parallel_orbitals.h"
+#include "module_cell/klist.h"
+#include "module_elecstate/elecstate_lcao.h"
+#include "module_hamilt_general/hamilt.h"
+
+namespace elecstate
+{
+void cal_edm_tddft(Parallel_Orbitals& pv,
+                   elecstate::ElecState* pelec,
+                   K_Vectors& kv,
+                   hamilt::Hamilt<std::complex<double>>* p_hamilt);
+} // namespace elecstate
+#endif
