71451f379df3fa5abdabca9a9d12599f75c781ba
YuLiu98 <liuyu@stu.pku.edu.cn>
2024-11-18 17:18:18 +0800

    add head files

--- source/module_esolver/esolver_gets.cpp
+++ source/module_esolver/esolver_gets.cpp
@@ -2,8 +2,12 @@
 
 #include "module_base/timer.h"
 #include "module_cell/module_neighbor/sltk_atom_arrange.h"
+#include "module_elecstate/elecstate_lcao.h"
+#include "module_hamilt_lcao/hamilt_lcaodft/LCAO_domain.h"
 #include "module_hamilt_lcao/hamilt_lcaodft/hamilt_lcao.h"
 #include "module_hamilt_lcao/hamilt_lcaodft/operator_lcao/operator_lcao.h"
+#include "module_io/print_info.h"
+#include "module_io/write_HS_R.h"
 
 namespace ModuleESolver
 {
