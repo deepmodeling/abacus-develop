01e962569ddeaef949f30e6640344cd598307052
YuLiu98 <liuyu@stu.pku.edu.cn>
2024-11-21 20:17:26 +0800

    update dpks_cal_e_delta_band

--- source/module_esolver/esolver_ks_lcao.cpp
+++ source/module_esolver/esolver_ks_lcao.cpp
@@ -879,14 +879,7 @@ void ESolver_KS_LCAO<TK, TR>::iter_finish(const int istep, int& iter)
         const std::vector<std::vector<TK>>& dm
             = dynamic_cast<const elecstate::ElecStateLCAO<TK>*>(this->pelec)->get_DM()->get_DMK_vector();
 
-        if (PARAM.globalv.gamma_only_local)
-        {
-            GlobalC::ld.cal_e_delta_band(dm);
-        }
-        else
-        {
-            GlobalC::ld.cal_e_delta_band_k(dm, this->kv.get_nks());
-        }
+        GlobalC::ld.dpks_cal_e_delta_band(dm, this->kv.get_nks());
     }
 #endif
 
