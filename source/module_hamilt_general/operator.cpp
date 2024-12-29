6ff1b3a0cff19b3146d81123f6f45909c3b49a15
haozhihan <haozhi.han@stu.pku.edu.cn>
2024-12-29 05:45:03 +0000

    check correct

--- source/module_hamilt_general/operator.cpp
+++ source/module_hamilt_general/operator.cpp
@@ -89,8 +89,8 @@ typename Operator<T, Device>::hpsi_info Operator<T, Device>::hPsi(hpsi_info& inp
                     psi_input->npol,
                     tmpsi_in,
                     this->hpsi->get_pointer(),
-                    psi_input->get_ngk(op->ik),
-                    // psi_input->get_current_nbas(),
+                    // psi_input->get_ngk(op->ik),
+                    psi_input->get_current_nbas(),
                     is_first_node);
             break;
         }
