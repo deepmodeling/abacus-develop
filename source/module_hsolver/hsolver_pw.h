fe8e0c08ab7c230ca4b61a696744b76a1cda4e03
Jie Li <li_j20@fudan.edu.cn>
2025-02-08 18:53:24 +0800

    add k continuity in hsolver

--- source/module_hsolver/hsolver_pw.h
+++ source/module_hsolver/hsolver_pw.h
@@ -5,6 +5,7 @@
 #include "module_hamilt_general/hamilt.h"
 #include "module_base/macros.h"
 #include "module_basis/module_pw/pw_basis_k.h"
+#include <unordered_map>
 
 namespace hsolver
 {
@@ -98,6 +99,15 @@ class HSolverPW
 
     void paw_func_after_kloop(psi::Psi<T, Device>& psi, elecstate::ElecState* pes,const double tpiba,const int nat);
 #endif
+
+    // K-point continuity related members
+    bool use_k_continuity = false;
+    std::vector<int> k_order;
+    std::unordered_map<int, int> k_parent;
+    std::vector<ModuleBase::Vector3<double>> kvecs_c;
+    
+    void build_k_neighbors();
+    void propagate_psi(psi::Psi<T>& psi, const int from_ik, const int to_ik);
 };
 
 } // namespace hsolver
