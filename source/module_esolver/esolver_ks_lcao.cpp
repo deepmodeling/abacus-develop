7d718e668f15f6745bb8078c35932e08e7918282
ErjieWu <wuerjie529@gmail.com>
2025-03-04 11:19:33 +0800

    update esolver_ks_lcao.cpp.

--- source/module_esolver/esolver_ks_lcao.cpp
+++ source/module_esolver/esolver_ks_lcao.cpp
@@ -224,6 +224,7 @@ void ESolver_KS_LCAO<TK, TR>::before_all_runners(UnitCell& ucell, const Input_pa
         DeePKS_domain::read_pdm((PARAM.inp.init_chg == "file"),
                                 PARAM.inp.deepks_equiv,
                                 ld.init_pdm,
+                                ucell.nat,
                                 orb_.Alpha[0].getTotal_nchi() * ucell.nat,
                                 ld.lmaxd,
                                 ld.inl_l,
