8a1f4ac8fa90fc688b9c91be4351f70990d2fe1f
JamesMisaka <ff6757442@gmail.com>
2025-03-08 12:15:41 +0800

    update elpa

--- toolchain/scripts/stage3/install_elpa.sh
+++ toolchain/scripts/stage3/install_elpa.sh
@@ -3,14 +3,16 @@
 # TODO: Review and if possible fix shellcheck errors.
 # shellcheck disable=all
 
-# Last Update in 2024-0811
+# Last Update in 2025-0308
 
 [ "${BASH_SOURCE[0]}" ] && SCRIPT_NAME="${BASH_SOURCE[0]}" || SCRIPT_NAME=$0
 SCRIPT_DIR="$(cd "$(dirname "$SCRIPT_NAME")/.." && pwd -P)"
 
 # From https://elpa.mpcdf.mpg.de/software/tarball-archive/ELPA_TARBALL_ARCHIVE.html
-elpa_ver="2024.05.001"
-elpa_sha256="9caf41a3e600e2f6f4ce1931bd54185179dade9c171556d0c9b41bbc6940f2f6"
+# elpa_ver="2024.05.001"
+# elpa_sha256="9caf41a3e600e2f6f4ce1931bd54185179dade9c171556d0c9b41bbc6940f2f6"
+elpa_ver="2025.01.001"
+elpa_sha256="3ef0c6aed9a3e05db6efafe6e14d66eb88b2a1354d61e765b7cde0d3d5f3951e"
 
 
 source "${SCRIPT_DIR}"/common_vars.sh
