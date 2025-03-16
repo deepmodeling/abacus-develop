b3defd5b1b1db8485396bc4b5ea265a71a7166ec
tang070205 <tang070205@proton.me>
2025-03-16 15:16:22 +0800

    Add a specified Fortran mpi compiler for elpa to use

--- toolchain/scripts/stage1/install_openmpi.sh
+++ toolchain/scripts/stage1/install_openmpi.sh
@@ -80,6 +80,7 @@ case "${with_openmpi}" in
         --prefix=${pkg_install_dir} \
         --libdir="${pkg_install_dir}/lib" \
         --with-libevent=internal \
+        --enable-mpi-fortran FC=gfortran \
         ${EXTRA_CONFIGURE_FLAGS} \
         > configure.log 2>&1 || tail -n ${LOG_LINES} configure.log
       make -j $(get_nprocs) > make.log 2>&1 || tail -n ${LOG_LINES} make.log
