eb0a3020ee84a6527b9c3ce4a80ec18914109d3c
mohanchen <mohan.chen.chen.mohan@gmail.com>
2025-03-27 09:40:51 +0800

    beging modifying the autotests

--- tests/integrate/tools/run_check.sh
+++ tests/integrate/tools/run_check.sh
@@ -21,34 +21,43 @@ check_out(){
 	done
 }
 
-test -e ../general_info||echo "plese write the file general_info"
-test -e ../general_info||exit 0
+test -e ../general_info|| echo "plese prepare the general_info file."
+
+test -e ../general_info|| exit 0
+
 exec_path=`grep EXEC ../general_info|awk '{printf $2}'`
+
 test -e $exec_path || echo "Error! ABACUS path was wrong!!"
+
 test -e $exec_path || exit 0
+
 CA=`grep CHECKACCURACY ../general_info | awk '{printf $2}'`
+
 NP=`grep NUMBEROFPROCESS ../general_info | awk '{printf $2}'`
 
-	path_here=`pwd`
-	echo "Test in $path_here"
+path_here=`pwd`
+echo "Test in $path_here"
 
-	echo "Begin test with $NP cores"
-	#parallel test
-	mpirun -np $NP $exec_path > log.txt
-	test -d OUT.autotest || echo "Some errors happened in ABACUS!"
-	test -d OUT.autotest || exit 0
+echo "Begin testing the example with $NP cores"
 
-	#if any input parameters for this script, just generate reference file.
-	if test -z $1 
-	then
-		../tools/catch_properties.sh result.out
-		check_out result.out
-	elif [ $1 == "debug" ] 
-	then
-		./catch_properties.sh result.out
-		check_out result.out
-	else
-		../tools/catch_properties.sh result.ref
-		rm -r OUT.autotest
-		rm log.txt
-	fi
+#parallel test
+mpirun -np $NP $exec_path > log.txt
+
+test -d OUT.autotest || echo "Some errors happened in ABACUS!"
+
+test -d OUT.autotest || exit 0
+
+#if any input parameters for this script, just generate reference file.
+if test -z $1 
+then
+../tools/catch_properties.sh result.out
+check_out result.out
+elif [ $1 == "debug" ] 
+then
+./catch_properties.sh result.out
+check_out result.out
+else
+../tools/catch_properties.sh result.ref
+rm -r OUT.autotest
+rm log.txt
+fi
