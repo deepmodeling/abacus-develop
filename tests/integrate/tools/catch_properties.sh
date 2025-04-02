6cf861571d976b753323d2cf6cc0fa1ebe3fbabf
Liangxuan <1351620715@qq.com>
2025-04-02 21:45:36 +0800

    add integrate test for deepks_out_labels = 2

--- tests/integrate/tools/catch_properties.sh
+++ tests/integrate/tools/catch_properties.sh
@@ -586,11 +586,37 @@ if ! test -z "$deepks_out_labels" && [ $deepks_out_labels == 1 ]; then
 		echo "deepks_s_label $deepks_s_label" >>$1
 	fi
 fi
+if ! test -z "$deepks_out_labels" && [ $deepks_out_labels == 2 ]; then
+	deepks_atom=`python3 get_sum_numpy.py OUT.autotest/deepks_atom.npy `
+	echo "deepks_atom $deepks_atom" >> $1
+	deepks_box=`python3 get_sum_numpy.py OUT.autotest/deepks_box.npy `
+	echo "deepks_box $deepks_box" >> $1
+	deepks_energy=`python3 get_sum_numpy.py OUT.autotest/deepks_energy.npy `
+	echo "deepks_energy $deepks_energy" >> $1
+	if ! test -z "$has_force" && [ $has_force == 1 ]; then
+	    deepks_force=`python3 get_sum_numpy.py OUT.autotest/deepks_force.npy `
+		echo "deepks_force $deepks_force" >> $1
+	fi
+	if ! test -z "$has_stress" && [ $has_stress == 1 ]; then
+	    deepks_stress=`python3 get_sum_numpy.py OUT.autotest/deepks_stress.npy `
+		echo "deepks_stress $deepks_stress" >> $1
+	fi
+	if ! test -z "$deepks_bandgap" && [ $deepks_bandgap == 1 ]; then
+	    deepks_orbital=`python3 get_sum_numpy.py OUT.autotest/deepks_orbital.npy `
+		echo "deepks_orbital $deepks_orbital" >> $1
+	fi
+	if ! test -z "$deepks_v_delta" && [[ $deepks_v_delta -gt 0 ]]; then
+	    deepks_hamiltonian=`python3 get_sum_numpy.py OUT.autotest/deepks_hamiltonian.npy `
+		echo "deepks_hamiltonian $deepks_hamiltonian" >> $1
+		deepks_overlap=`python3 get_sum_numpy.py OUT.autotest/deepks_overlap.npy `
+		echo "deepks_overlap $deepks_overlap" >> $1
+	fi
+fi
 
 #--------------------------------------------
 # band gap information
 #--------------------------------------------
-if ! test -z "$deepks_bandgap" && [ $deepks_bandgap == 1 ]; then
+if ! test -z "$deepks_bandgap" && [ $deepks_bandgap == 1 ] && [ $deepks_out_labels == 1 ]; then
 	odelta=`python3 get_odelta.py`
 	echo "odelta $odelta" >>$1
 	oprec=`python3 get_oprec.py`
@@ -601,7 +627,7 @@ fi
 #--------------------------------------------
 # check vdelta in deepks
 #--------------------------------------------
-if ! test -z "$deepks_v_delta" && [ $deepks_v_delta == 1 ]; then
+if ! test -z "$deepks_v_delta" && [ $deepks_v_delta == 1 ] && [ $deepks_out_labels == 1 ]; then
 	totalh=`python3 get_sum_numpy.py OUT.autotest/deepks_htot.npy `
 	echo "totalh $totalh" >>$1
 	totalvdelta=`python3 get_v_delta.py`
@@ -613,7 +639,7 @@ fi
 #--------------------------------------------
 # check vdelta in deepks
 #--------------------------------------------
-if ! test -z "$deepks_v_delta" && [ $deepks_v_delta == 2 ]; then
+if ! test -z "$deepks_v_delta" && [ $deepks_v_delta == 2 ] && [ $deepks_out_labels == 1 ]; then
 	totalh=`python3 get_sum_numpy.py OUT.autotest/deepks_htot.npy `
 	echo "totalh $totalh" >>$1
 	totalvdelta=`python3 get_v_delta.py`
