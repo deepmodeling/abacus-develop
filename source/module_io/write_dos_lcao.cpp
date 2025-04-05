e45ab26d98a4b1bc5d9d7bed4487f0114e56de3b
mohanchen <mohan.chen.chen.mohan@gmail.com>
2025-04-05 16:39:14 +0800

    change DOS name

--- source/module_io/write_dos_lcao.cpp
+++ source/module_io/write_dos_lcao.cpp
@@ -1,29 +1,20 @@
 #include "write_dos_lcao.h"
+#include "cal_dos.h"
 
 #include "module_parameter/parameter.h"
-#ifdef __MPI
-#include <mpi.h>
-#endif
-#include "cal_dos.h"
 #include "module_base/global_function.h"
 #include "module_base/global_variable.h"
 #include "module_hamilt_pw/hamilt_pwdft/global.h"
 #include "write_orb_info.h"
 
-#include <sys/time.h>
-#include <vector>
-
-#ifdef __LCAO
-#include "module_cell/module_neighbor/sltk_atom_arrange.h" //qifeng-2019-01-21
+#include "module_cell/module_neighbor/sltk_atom_arrange.h"
 #include "module_cell/module_neighbor/sltk_grid_driver.h"
 #include "module_hamilt_lcao/hamilt_lcaodft/hamilt_lcao.h"
-#endif
 
-#include "module_base/blas_connector.h"
-#include "module_base/complexmatrix.h"
-#include "module_base/matrix.h"
 #include "module_base/parallel_reduce.h"
+#include "module_base/blas_connector.h"
 #include "module_base/scalapack_connector.h"
+
 
 // for gamma only
 template <>
@@ -36,7 +27,10 @@ void ModuleIO::write_dos_lcao(const UnitCell& ucell,
                               const double& dos_scale,
                               const double& bcoeff,
                               const K_Vectors& kv,
-                              hamilt::Hamilt<double>* p_ham)
+                              const int nbands,
+                              const elecstate::efermi &energy_fermi,
+                              hamilt::Hamilt<double>* p_ham,
+                              std::ofstream &ofs_running)
 {
     ModuleBase::TITLE("ModuleIO", "write_dos_lcao");
     
@@ -45,7 +39,7 @@ void ModuleIO::write_dos_lcao(const UnitCell& ucell,
     double emax = 0.0;
     double emin = 0.0;
 
-	prepare_dos(GlobalV::ofs_running,
+	prepare_dos(ofs_running,
 			energy_fermi,
 			ekb,
 			kv.get_nks(),
@@ -94,7 +88,7 @@ void ModuleIO::write_dos_lcao(const UnitCell& ucell,
 
         psi->fix_k(is);
         const double* ppsi = psi->get_pointer();
-        for (int i = 0; i < PARAM.inp.nbands; ++i)
+        for (int i = 0; i < nbands; ++i)
         {
             ModuleBase::GlobalFunc::ZEROS(waveg, PARAM.globalv.nlocal);
 
@@ -305,12 +299,12 @@ void ModuleIO::write_dos_lcao(const UnitCell& ucell,
 				kv.get_nkstot(),
 				kv.wk,
 				kv.isk,
-				PARAM.inp.nbands,
+				nbands,
 				ekb,
 				wg);
 	}
 
-    GlobalV::ofs_running << " DOS CALCULATIONS ENDS." << std::endl;
+    ofs_running << " DOS CALCULATIONS ENDS." << std::endl;
 
     return;
 }
@@ -326,7 +320,10 @@ void ModuleIO::write_dos_lcao(const UnitCell& ucell,
                               const double& dos_scale,
                               const double& bcoeff,
                               const K_Vectors& kv,
-                              hamilt::Hamilt<std::complex<double>>* p_ham)
+                              const int nbands,
+                              const elecstate::efermi &energy_fermi,
+                              hamilt::Hamilt<std::complex<double>>* p_ham,
+                              std::ofstream &ofs_running)
 {
     ModuleBase::TITLE("ModuleIO", "write_dos_lcao");
 
@@ -424,7 +421,7 @@ void ModuleIO::write_dos_lcao(const UnitCell& ucell,
                         p_dwfc[index] = conj(psi->get_pointer()[index]);
                     }
 
-                    for (int i = 0; i < PARAM.inp.nbands; ++i)
+                    for (int i = 0; i < nbands; ++i)
                     {
 
                         ModuleBase::GlobalFunc::ZEROS(waveg, PARAM.globalv.nlocal);
@@ -623,7 +620,7 @@ void ModuleIO::write_dos_lcao(const UnitCell& ucell,
         std::stringstream ss;
         ss << PARAM.globalv.global_out_dir << "DOS" << is + 1;
         std::stringstream ss1;
-        ss1 << PARAM.globalv.global_out_dir << "DOS" << is + 1 << "_smearing.dat";
+        ss1 << PARAM.globalv.global_out_dir << "DOS" << is + 1 << "_smear.dat";
 
 		ModuleIO::cal_dos(is,
 				ss.str(),
@@ -636,12 +633,12 @@ void ModuleIO::write_dos_lcao(const UnitCell& ucell,
 				kv.get_nkstot(),
 				kv.wk,
 				kv.isk,
-				PARAM.inp.nbands,
+				nbands,
 				ekb,
 				wg);
 	}
 
-    GlobalV::ofs_running << " DOS CALCULATIONS ENDS." << std::endl; 
+    ofs_running << " DOS CALCULATIONS ENDS." << std::endl; 
 
     return;
 }
