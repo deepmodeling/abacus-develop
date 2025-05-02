483ef26c3437b10ef83e643472122b849530a0a6
JamesMisaka <ff6757442@163.com>
2025-05-02 10:19:04 +0800

    update rapidjson cmake finding

--- toolchain/scripts/stage4/install_rapidjson.sh
+++ toolchain/scripts/stage4/install_rapidjson.sh
@@ -56,7 +56,12 @@ case "$with_rapidjson" in
         mkdir -p "${pkg_install_dir}"
         cp -r $dirname/* "${pkg_install_dir}/"
         # for CMake to find rapidjson
-        cp ${pkg_install_dir}/RapidJSONConfig.cmake.in ${pkg_install_dir}/RapidJSONConfig.cmake
+        #cp ${pkg_install_dir}/RapidJSONConfig.cmake.in ${pkg_install_dir}/RapidJSONConfig.cmake
+        cat << EOF > "${pkg_install_dir}/RapidJSONConfig.cmake"
+get_filename_component(RAPIDJSON_CMAKE_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
+set(RAPIDJSON_INCLUDE_DIRS "@INCLUDE_INSTALL_DIR@")
+message(STATUS "RapidJSON found. Headers: ${RAPIDJSON_INCLUDE_DIRS}")
+EOF
         write_checksums "${install_lock_file}" "${SCRIPT_DIR}/stage4/$(basename ${SCRIPT_NAME})"
     fi
     fi
