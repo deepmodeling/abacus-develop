8b8c17a4ac0d04245863a0752a484214b5eddc1a
haozhihan <haozhi.han@outlook.com>
2025-05-19 08:25:40 +0000

    update

--- source/module_esolver/esolver_ks_pw.cpp
+++ source/module_esolver/esolver_ks_pw.cpp
@@ -529,7 +529,7 @@ void ESolver_KS_PW<T, Device>::hamilt2rho_single(UnitCell& ucell, const int iste
                                                      PARAM.inp.calculation,
                                                      PARAM.inp.basis_type,
                                                      PARAM.inp.ks_solver,
-                                                     PARAM.inp.use_paw,
+                                                     false,
                                                      PARAM.globalv.use_uspp,
                                                      PARAM.inp.nspin,
                                                      hsolver::DiagoIterAssist<T, Device>::SCF_ITER,
