aa40c9b0bc9cf360669d113a9f3729186e1b688d
jiyuang <jiyuyang@mail.ustc.edu.cn>
2025-05-19 15:08:51 +0800

    fix test

--- source/module_io/test_serial/read_input_item_test.cpp
+++ source/module_io/test_serial/read_input_item_test.cpp
@@ -1360,7 +1360,7 @@ TEST_F(InputTest, Item_test2)
         param.input.exx_hybrid_beta = "default";
         param.input.dft_functional = "hse";
         it->second.reset_value(it->second, param);
-        EXPECT_EQ(param.input.exx_hybrid_beta, "0.25");
+        EXPECT_EQ(param.input.exx_hybrid_beta, "0");
 
         param.input.exx_hybrid_beta= "default";
         param.input.dft_functional = "none";
@@ -1378,7 +1378,7 @@ TEST_F(InputTest, Item_test2)
         param.input.exx_hse_omega = "default";
         param.input.dft_functional = "lc_pbe";
         it->second.reset_value(it->second, param);
-        EXPECT_EQ(param.input.exx_hse_omega, "0.11");
+        EXPECT_EQ(param.input.exx_hse_omega, "0.33");
 
         param.input.exx_hse_omega = "default";
         param.input.dft_functional = "lc_wpbe";
