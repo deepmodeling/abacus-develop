d381255034a8d388f47bdc02ea848f01660593c9
jiyuang <jiyuyang@mail.ustc.edu.cn>
2025-05-19 11:49:38 +0800

    delete GlobalC

--- source/module_ri/ewald_Vq.h
+++ source/module_ri/ewald_Vq.h
@@ -214,6 +214,6 @@ class Ewald_Vq
     inline double cal_V_Rcut(const int it0, const int it1);
     inline double get_Rcut_max(const int it0, const int it1);
 };
-#include "ewald_vq.hpp"
+#include "ewald_Vq.hpp"
 
 #endif
