8cbf3a897e82733e21217341daed29d77cd9e111
mohanchen <mohan.chen.chen.mohan@gmail.com>
2025-06-04 11:49:03 +0800

    add ctrl_output_lcao.h and ctrl_output_lcao.cpp files

---
+++ source/module_io/ctrl_output_lcao.h
@@ -0,0 +1,11 @@
+#ifndef CTRL_OUTPUT_LCAO_H 
+#define CTRL_OUTPUT_LCAO_H 
+
+#include <complex>
+
+namespace ModuleIO
+{
+	template <typename TK, typename TR>
+		void ctrl_output_lcao(); 
+}
+#endif
