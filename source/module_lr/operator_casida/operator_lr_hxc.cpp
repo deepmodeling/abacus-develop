7a19cef3a4a36ab48c685fe09a1c8b2e3490f8b3
Critsium-xy <tsfxwbbzxy@163.com>
2025-06-26 23:19:08 +0800

    Reupload of previous PR

--- source/module_lr/operator_casida/operator_lr_hxc.cpp
+++ source/module_lr/operator_casida/operator_lr_hxc.cpp
@@ -6,8 +6,8 @@
 #include "module_lr/utils/lr_util.h"
 #include "module_lr/utils/lr_util_hcontainer.h"
 #include "module_lr/utils/lr_util_print.h"
-// #include "module_hamilt_lcao/hamilt_lcaodft/DM_gamma_2d_to_grid.h"
-#include "module_hamilt_lcao/module_hcontainer/hcontainer_funcs.h"
+// #include "source_lcao/hamilt_lcaodft/DM_gamma_2d_to_grid.h"
+#include "source_lcao/module_hcontainer/hcontainer_funcs.h"
 #include "module_lr/ao_to_mo_transformer/ao_to_mo.h"
 #include "source_pw/hamilt_pwdft/global.h"
 
