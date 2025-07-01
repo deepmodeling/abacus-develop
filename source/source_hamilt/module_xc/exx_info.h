0ad01df91ded9f9f6d0db775bf21b2ae70dc5910
Yuyang ji <1041176461@qq.com>
2025-07-01 14:52:15 +0800

    fix compile

--- source/source_hamilt/module_xc/exx_info.h
+++ source/source_hamilt/module_xc/exx_info.h
@@ -51,10 +51,9 @@ struct Exx_Info
 
     struct Exx_Info_RI
     {
-        const std::map<Conv_Coulomb_Pot_K::Coulomb_Type, std::vector<std::map<std::string,std::string>>> &coulomb_param;
-                std::unordered_map<Conv_Coulomb_Pot_K::Coulomb_Method, 
+        std::map<Conv_Coulomb_Pot_K::Coulomb_Method, 
             std::pair<bool, 
-                std::unordered_map<Conv_Coulomb_Pot_K::Coulomb_Type, 
+                std::_map<Conv_Coulomb_Pot_K::Coulomb_Type, 
                     std::vector<std::map<std::string,std::string>>>>> coulomb_settings;
 
         bool real_number = false;
