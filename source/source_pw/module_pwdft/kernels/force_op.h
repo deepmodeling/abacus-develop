82f193b7ee4091e7ca5605c173b9c2c6035168f1
dzzz2001 <dzcpku2001@163.com>
2025-08-02 22:51:11 +0800

    fix bug

--- source/source_pw/module_pwdft/kernels/force_op.h
+++ source/source_pw/module_pwdft/kernels/force_op.h
@@ -155,21 +155,6 @@ struct cal_force_loc_op{
         const int nat,
         const int npw,
         const FPTYPE tpiba_omega,
-        const int* ig2igg,
-        const FPTYPE* gcar,
-        const FPTYPE* tau,
-        const std::complex<FPTYPE>* aux,
-        const FPTYPE* vloc,
-        const int vloc_nr,
-        FPTYPE* forcelc);
-};
-
-template <typename FPTYPE, typename Device>
-struct cal_force_loc_op{
-    void operator()(
-        const int nat,
-        const int npw,
-        const FPTYPE tpiba_omega,
         const int* iat2it,
         const int* ig2igg,
         const FPTYPE* gcar,
