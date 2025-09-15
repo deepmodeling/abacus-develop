267470616592db3ad67fe5e5798606f2c1d2db32
Critsium-xy <tsfxwbbzxy@163.com>
2025-09-15 16:41:02 +0800

    Rename namespace

--- source/source_basis/module_pw/pw_basis.h
+++ source/source_basis/module_pw/pw_basis.h
@@ -245,7 +245,7 @@ public:
     int nmaxgr = 0;  // Gamma_only: max between npw and (nrxx+1)/2, others: max between npw and nrxx
                      // Thus std::complex<double>[nmaxgr] is able to contain either reciprocal or real data
     // FFT ft;
-    FFT_Bundle fft_bundle;
+    ModuleBase::FFT_Bundle fft_bundle;
     //The position of pointer in and out can be equal(in-place transform) or different(out-of-place transform).
     
     template <typename FPTYPE>
