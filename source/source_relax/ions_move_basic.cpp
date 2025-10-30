91be215bdb6c07b2734475d74d9decbc291e8190
Fei Yang <2501213217@stu.pku.edu.cn>
2025-10-30 20:35:32 +0800

    remove vectorvector in bfgs and add parameter cp2k

--- source/source_relax/ions_move_basic.cpp
+++ source/source_relax/ions_move_basic.cpp
@@ -24,7 +24,9 @@ double Ions_Move_Basic::best_xxx = 1.0;
 
 int Ions_Move_Basic::out_stru = 0;
 std::vector<std::string> Ions_Move_Basic::relax_method = {"bfgs","2"};
-std::string Ions_Move_Basic::cp2k = "none";
+namespace Ions_Move_Basic {
+    std::string cp2k = "none";
+}
 
 void Ions_Move_Basic::setup_gradient(const UnitCell &ucell, const ModuleBase::matrix &force, double *pos, double *grad)
 {
