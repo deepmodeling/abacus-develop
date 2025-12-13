62efb32ca73d8b56c1c6508e77fff0d91ac99595
Levi Zhou <31941107+ZhouXY-PKU@users.noreply.github.com>
2025-12-13 10:21:25 +0800

    Update global.h

--- source/source_pw/module_pwdft/global.h
+++ source/source_pw/module_pwdft/global.h
@@ -42,48 +42,6 @@ static const char* _cublasGetErrorString(cublasStatus_t error)
     return "<unknown>";
 }
 
-static const char* _cufftGetErrorString(cufftResult_t error)
-{
-    switch (error)
-    {
-    case CUFFT_SUCCESS:
-        return "CUFFT_SUCCESS";
-    case CUFFT_INVALID_PLAN:
-        return "CUFFT_INVALID_PLAN";
-    case CUFFT_ALLOC_FAILED:
-        return "CUFFT_ALLOC_FAILED";
-    case CUFFT_INVALID_TYPE:
-        return "CUFFT_INVALID_TYPE";
-    case CUFFT_INVALID_VALUE:
-        return "CUFFT_INVALID_VALUE";
-    case CUFFT_INTERNAL_ERROR:
-        return "CUFFT_INTERNAL_ERROR";
-    case CUFFT_EXEC_FAILED:
-        return "CUFFT_EXEC_FAILED";
-    case CUFFT_SETUP_FAILED:
-        return "CUFFT_SETUP_FAILED";
-    case CUFFT_INVALID_SIZE:
-        return "CUFFT_INVALID_SIZE";
-    case CUFFT_UNALIGNED_DATA:
-        return "CUFFT_UNALIGNED_DATA";
-    case CUFFT_INCOMPLETE_PARAMETER_LIST:
-        return "CUFFT_INCOMPLETE_PARAMETER_LIST";
-    case CUFFT_INVALID_DEVICE:
-        return "CUFFT_INVALID_DEVICE";
-    case CUFFT_PARSE_ERROR:
-        return "CUFFT_PARSE_ERROR";
-    case CUFFT_NO_WORKSPACE:
-        return "CUFFT_NO_WORKSPACE";
-    case CUFFT_NOT_IMPLEMENTED:
-        return "CUFFT_NOT_IMPLEMENTED";
-    case CUFFT_LICENSE_ERROR:
-        return "CUFFT_LICENSE_ERROR";
-    case CUFFT_NOT_SUPPORTED:
-        return "CUFFT_NOT_SUPPORTED";
-    }
-    return "<unknown>";
-}
-
 #define CHECK_CUDA(func)                                                                                               \
     {                                                                                                                  \
         cudaError_t status = (func);                                                                                   \
@@ -120,7 +78,7 @@ static const char* _cufftGetErrorString(cufftResult_t error)
         if (status != CUFFT_SUCCESS)                                                                                   \
         {                                                                                                              \
             printf("In File %s : CUFFT API failed at line %d with error: %s (%d)\n", __FILE__, __LINE__,               \
-                   _cufftGetErrorString(status), status);                                                              \
+                   ModuleBase::cuda_compat::cufftGetErrorStringCompat(status), status);                                                              \
         }                                                                                                              \
     }
 #endif // __CUDA
