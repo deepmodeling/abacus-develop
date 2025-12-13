4058573d5fc29aeec2d56dbfc2f8b3a49cb78d19
Chen Nuo <49788094+Cstandardlib@users.noreply.github.com>
2025-12-13 19:37:53 +0800

    Put InitPools in ParaGlobalDeathTest, switch off CaptureStdout

--- source/source_base/test_parallel/parallel_global_test.cpp
+++ source/source_base/test_parallel/parallel_global_test.cpp
@@ -80,25 +80,6 @@ class ParaGlobal : public ::testing::Test
     }
 };
 
-TEST_F(ParaGlobal, InitPools)
-{
-    nproc = 12;
-    mpi.kpar = 3;
-    mpi.nstogroup = 3;
-    my_rank = 5;
-    testing::internal::CaptureStdout();
-    EXPECT_EXIT(Parallel_Global::init_pools(nproc,
-                                my_rank,
-                                mpi.nstogroup,
-                                mpi.kpar,
-                                mpi.nproc_in_stogroup,
-                                mpi.rank_in_stogroup,
-                                mpi.MY_BNDGROUP,
-                                mpi.nproc_in_pool,
-                                mpi.rank_in_pool,
-                                mpi.my_pool), ::testing::ExitedWithCode(1), "Error:");
-}
-
 TEST_F(ParaGlobal, SplitGrid)
 {
     // NPROC is set to 4 in parallel_global_test.sh
@@ -233,6 +214,23 @@ class ParaGlobalDeathTest : public ::testing::Test
     }
 };
 
+TEST_F(ParaGlobalDeathTest, InitPools)
+{
+    nproc = 12;
+    mpi.kpar = 3;
+    mpi.nstogroup = 3;
+    my_rank = 5;
+    EXPECT_EXIT(Parallel_Global::init_pools(nproc,
+                                my_rank,
+                                mpi.nstogroup,
+                                mpi.kpar,
+                                mpi.nproc_in_stogroup,
+                                mpi.rank_in_stogroup,
+                                mpi.MY_BNDGROUP,
+                                mpi.nproc_in_pool,
+                                mpi.rank_in_pool,
+                                mpi.my_pool), ::testing::ExitedWithCode(1), "Error:");
+}
 
 TEST_F(ParaGlobalDeathTest, DivideMPIPoolsNgEqZero)
 {
