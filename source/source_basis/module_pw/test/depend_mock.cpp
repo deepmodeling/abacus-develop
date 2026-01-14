3606a66cf6ac3c701517497d8181dce9ca6c8c3f
ubuntu <3158793232@qq.com>
2026-01-14 21:57:59 +0800

    Fix: implement MPI reductions in depend_mock for parallel tests

--- source/source_basis/module_pw/test/depend_mock.cpp
+++ source/source_basis/module_pw/test/depend_mock.cpp
@@ -12,67 +12,64 @@ namespace GlobalV
 MPI_Comm POOL_WORLD;
 namespace Parallel_Reduce
 {
-    template<typename T> void reduce_all(T& object) { return; };
-    template<typename T> void reduce_all(T* object, const int n) { return; };
-    template<typename T> void reduce_pool(T& object) { return; };
-    template<typename T> void reduce_pool(T* object, const int n) { return; };
+    template<typename T> void reduce_all(T& object);
+    template<typename T> void reduce_all(T* object, const int n);
+    template<typename T> void reduce_pool(T& object);
+    template<typename T> void reduce_pool(T* object, const int n);
 
     template<>
-    void reduce_all<int>(int& object) { return; };
+    void reduce_all<int>(int& object) { MPI_Allreduce(MPI_IN_PLACE, &object, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD); }
     template<>
-    void reduce_all<long long>(long long& object) { return; };
+    void reduce_all<long long>(long long& object) { MPI_Allreduce(MPI_IN_PLACE, &object, 1, MPI_LONG_LONG, MPI_SUM, MPI_COMM_WORLD); }
     template<>
-    void reduce_all<double>(double& object) { return; };
+    void reduce_all<double>(double& object) { MPI_Allreduce(MPI_IN_PLACE, &object, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD); }
     template<>
-    void reduce_all<float>(float& object) { return; };
+    void reduce_all<float>(float& object) { MPI_Allreduce(MPI_IN_PLACE, &object, 1, MPI_FLOAT, MPI_SUM, MPI_COMM_WORLD); }
     template<>
-    void reduce_all<std::complex<double>>(std::complex<double>& object) { return; };
+    void reduce_all<std::complex<double>>(std::complex<double>& object) { MPI_Allreduce(MPI_IN_PLACE, &object, 1, MPI_DOUBLE_COMPLEX, MPI_SUM, MPI_COMM_WORLD); }
     template<>
-    void reduce_all<std::complex<float>>(std::complex<float>& object) { return; };
+    void reduce_all<std::complex<float>>(std::complex<float>& object) { MPI_Allreduce(MPI_IN_PLACE, &object, 1, MPI_C_FLOAT_COMPLEX, MPI_SUM, MPI_COMM_WORLD); }
 
     template<>
-    void reduce_all<int>(int* object, const int n) { return; };
+    void reduce_all<int>(int* object, const int n) { MPI_Allreduce(MPI_IN_PLACE, object, n, MPI_INT, MPI_SUM, MPI_COMM_WORLD); }
     template<>
-    void reduce_all<long long>(long long* object, const int n) { return; };
+    void reduce_all<long long>(long long* object, const int n) { MPI_Allreduce(MPI_IN_PLACE, object, n, MPI_LONG_LONG, MPI_SUM, MPI_COMM_WORLD); }
     template<>
-    void reduce_all<double>(double* object, const int n) { return; };
+    void reduce_all<double>(double* object, const int n) { MPI_Allreduce(MPI_IN_PLACE, object, n, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD); }
     template<>
-    void reduce_all<std::complex<double>>(std::complex<double>* object, const int n) { return; };
+    void reduce_all<std::complex<double>>(std::complex<double>* object, const int n) { MPI_Allreduce(MPI_IN_PLACE, object, n, MPI_DOUBLE_COMPLEX, MPI_SUM, MPI_COMM_WORLD); }
     template<>
-    void reduce_all<std::complex<float>>(std::complex<float>* object, const int n) { return; };
+    void reduce_all<std::complex<float>>(std::complex<float>* object, const int n) { MPI_Allreduce(MPI_IN_PLACE, object, n, MPI_C_FLOAT_COMPLEX, MPI_SUM, MPI_COMM_WORLD); }
 
     template<>
-    void reduce_pool<float>(float& object) { return; };
+    void reduce_pool<float>(float& object) { MPI_Allreduce(MPI_IN_PLACE, &object, 1, MPI_FLOAT, MPI_SUM, POOL_WORLD); }
     template<>
-    void reduce_pool<double>(double& object) { return; };
+    void reduce_pool<double>(double& object) { MPI_Allreduce(MPI_IN_PLACE, &object, 1, MPI_DOUBLE, MPI_SUM, POOL_WORLD); }
     template<>
-    void reduce_pool<std::complex<double>>(std::complex<double>& object) { return; };
+    void reduce_pool<std::complex<double>>(std::complex<double>& object) { MPI_Allreduce(MPI_IN_PLACE, &object, 1, MPI_DOUBLE_COMPLEX, MPI_SUM, POOL_WORLD); }
 
     template<>
-    void reduce_pool<int>(int* object, const int n) { return; };
+    void reduce_pool<int>(int* object, const int n) { MPI_Allreduce(MPI_IN_PLACE, object, n, MPI_INT, MPI_SUM, POOL_WORLD); }
     template<>
-    void reduce_pool<double>(double* object, const int n) { return; };
-    template<>
-    void reduce_pool<std::complex<float>>(std::complex<float>* object, const int n) { return; };
-    template<>
-    void reduce_pool<std::complex<double>>(std::complex<double>* object, const int n) { return; };
+    void reduce_pool<double>(double* object, const int n) { MPI_Allreduce(MPI_IN_PLACE, object, n, MPI_DOUBLE, MPI_SUM, POOL_WORLD); }
 
-    void reduce_or_all(bool& object) { return; };
+    void reduce_max_pool(int* object, const int n) { MPI_Allreduce(MPI_IN_PLACE, object, n, MPI_INT, MPI_MAX, POOL_WORLD); }
+    void reduce_min_pool(double& object) { MPI_Allreduce(MPI_IN_PLACE, &object, 1, MPI_DOUBLE, MPI_MIN, POOL_WORLD); }
+
+    // Other stubs can remain as is if not used or if they don't break logic
+    void reduce_or_all(bool& object) { MPI_Allreduce(MPI_IN_PLACE, &object, 1, MPI_C_BOOL, MPI_LOR, MPI_COMM_WORLD); }
     
     template <typename T>
-    void reduce_max_all(T& object) { return; };
-    template<> void reduce_max_all<double>(double& object) { return; };
-    template<> void reduce_max_all<float>(float& object) { return; };
-    template<> void reduce_max_all<int>(int& object) { return; };
+    void reduce_max_all(T& object);
+    template<> void reduce_max_all<double>(double& object) { MPI_Allreduce(MPI_IN_PLACE, &object, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD); }
+    template<> void reduce_max_all<float>(float& object) { MPI_Allreduce(MPI_IN_PLACE, &object, 1, MPI_FLOAT, MPI_MAX, MPI_COMM_WORLD); }
+    template<> void reduce_max_all<int>(int& object) { MPI_Allreduce(MPI_IN_PLACE, &object, 1, MPI_INT, MPI_MAX, MPI_COMM_WORLD); }
 
     template <typename T>
-    void reduce_min_all(T& object) { return; };
-    template<> void reduce_min_all<double>(double& object) { return; };
-    template<> void reduce_min_all<float>(float& object) { return; };
-    template<> void reduce_min_all<int>(int& object) { return; };
-
-    void reduce_max_pool(int* object, const int n) { return; };
-    void reduce_min_pool(double& object) { return; };
+    void reduce_min_all(T& object);
+    template<> void reduce_min_all<double>(double& object) { MPI_Allreduce(MPI_IN_PLACE, &object, 1, MPI_DOUBLE, MPI_MIN, MPI_COMM_WORLD); }
+    template<> void reduce_min_all<float>(float& object) { MPI_Allreduce(MPI_IN_PLACE, &object, 1, MPI_FLOAT, MPI_MIN, MPI_COMM_WORLD); }
+    template<> void reduce_min_all<int>(int& object) { MPI_Allreduce(MPI_IN_PLACE, &object, 1, MPI_INT, MPI_MIN, MPI_COMM_WORLD); }
 
     void reduce_or_bp(bool& object) { return; };
 
