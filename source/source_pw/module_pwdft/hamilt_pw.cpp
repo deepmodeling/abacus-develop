0d7bc86cce1cf976937fce7c554391a8bc653078
sunliang98 <1700011430@pku.edu.cn>
2026-01-22 16:00:18 +0800

    Fix: Update hamilt_pw.cpp

--- source/source_pw/module_pwdft/hamilt_pw.cpp
+++ source/source_pw/module_pwdft/hamilt_pw.cpp
@@ -77,6 +77,7 @@ HamiltPW<T, Device>::HamiltPW(elecstate::Potential* pot_in,
         if (PARAM.inp.ml_exx) // sunliang
         {
             pot_register_in.push_back("ml_exx");
+        }
         // DFT-1/2
         if (PARAM.inp.dfthalf_type == 1) {
             pot_register_in.push_back("dfthalf");
