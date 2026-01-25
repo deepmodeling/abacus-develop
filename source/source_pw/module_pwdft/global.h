7ba39717004d185a0a2893eade9502f95cb8a8de
abacus_fixer <mohanchen@pku.eud.cn>
2026-01-25 09:14:59 +0800

    update global.h

--- source/source_pw/module_pwdft/global.h
+++ source/source_pw/module_pwdft/global.h
@@ -3,17 +3,14 @@
 
 #include "source_base/global_function.h"
 #include "source_base/global_variable.h"
-#include "source_estate/module_charge/charge_mixing.h"
 #include "source_io/restart.h"
 #include "source_relax/relax_driver.h"
 #ifdef __EXX
 #include "source_hamilt/module_xc/exx_info.h"
 #include "source_lcao/module_ri/exx_lip.h"
 #endif
-#include "source_estate/magnetism.h"
 #include "source_hamilt/module_xc/xc_functional.h"
 #include "source_base/module_device/device_check.h"
-#include "source_cell/parallel_kpoints.h"
 #include "source_cell/unitcell.h"
 
 #endif
