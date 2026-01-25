3a3898a7790760e4888daa16191b7600e7fec710
abacus_fixer <mohanchen@pku.eud.cn>
2026-01-25 09:33:17 +0800

    update global.h

--- source/source_pw/module_pwdft/global.h
+++ source/source_pw/module_pwdft/global.h
@@ -5,21 +5,17 @@
 #include "source_base/global_variable.h"
 #include "source_io/restart.h"
 
-
+#include "source_relax/ions_move_methods.h"
+#include "source_relax/lattice_change_methods.h"
 #include "source_cell/unitcell.h"
-#include "source_esolver/esolver.h"
-#include "source_esolver/esolver_ks.h"
-#include "source_relax/relax_sync.h"
-#include "source_relax/relax_nsync.h"
 #include "source_relax/bfgs.h"
 #include "source_io/module_parameter/input_parameter.h"
 
-
-#include "source_io/module_parameter/parameter.h"
 #ifdef __EXX
 #include "source_hamilt/module_xc/exx_info.h"
 #include "source_lcao/module_ri/exx_lip.h"
 #endif
+
 #include "source_hamilt/module_xc/xc_functional.h"
 #include "source_base/module_device/device_check.h"
 
