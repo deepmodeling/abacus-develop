5552c1a192505db1427c26fe06150761d9aac9da
abacus_fixer <mohanchen@pku.eud.cn>
2026-01-25 09:12:59 +0800

    update global.h

--- source/source_pw/module_pwdft/global.h
+++ source/source_pw/module_pwdft/global.h
@@ -4,7 +4,6 @@
 #include "source_base/global_function.h"
 #include "source_base/global_variable.h"
 #include "source_estate/module_charge/charge_mixing.h"
-#include "source_pw/module_pwdft/VNL_in_pw.h"
 #include "source_io/restart.h"
 #include "source_relax/relax_driver.h"
 #ifdef __EXX
@@ -14,13 +13,7 @@
 #include "source_estate/magnetism.h"
 #include "source_hamilt/module_xc/xc_functional.h"
 #include "source_base/module_device/device_check.h"
-
-//==========================================================
-// EXPLAIN : define "GLOBAL CLASS"
-//==========================================================
-
 #include "source_cell/parallel_kpoints.h"
 #include "source_cell/unitcell.h"
-// extern Magnetism mag;
 
 #endif
