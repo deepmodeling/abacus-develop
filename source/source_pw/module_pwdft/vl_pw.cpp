d6aaca7f7b624f7d92abb48152010b7bcf4a69f0
ubuntu <3158793232@qq.com>
2026-01-30 13:29:08 +0800

    clean format

--- source/source_pw/module_pwdft/vl_pw.cpp
+++ source/source_pw/module_pwdft/vl_pw.cpp
@@ -3,7 +3,7 @@
 #include "source_base/libm/libm.h"
 #include "source_base/math_integral.h"
 #include "source_base/timer.h"
-#include "source_base/tool_quit.h"
+
 pseudopot_cell_vl::pseudopot_cell_vl()
 {
 	numeric = nullptr;
