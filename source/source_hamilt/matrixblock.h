ed1fe3135b692d5b479eb26c920a04174cbfe923
linpz <linpz@mail.ustc.edu.cn>
2026-03-14 05:02:38 +0800

    remove nullptr in MatrixBlock

--- source/source_hamilt/matrixblock.h
+++ source/source_hamilt/matrixblock.h
@@ -9,10 +9,10 @@ template <typename T> struct MatrixBlock
 {
     /* this is a simple template block of a matrix
        would change to Eigen in the future */
-    T* p = nullptr;
+    T* p;
     size_t row;
     size_t col;
-    const int* desc = nullptr;
+    const int* desc;
 };
 
 } // namespace hamilt
