cf1350fe553a9a79bcfea44ef03453d81742a7b0
line-ar-line <2300011032@stu.pku.edu.cn>
2026-03-21 11:28:36 +0800

    2026PKUCourseHW5:Solve Magic Number

--- source/source_pw/module_stodft/sto_wf.cpp
+++ source/source_pw/module_stodft/sto_wf.cpp
@@ -64,13 +64,14 @@ void Stochastic_WF<T, Device>::clean_chiallorder()
 template <typename T, typename Device>
 void Stochastic_WF<T, Device>::init_sto_orbitals(const int seed_in)
 {
+	const unsigned int RANK_SEED_OFFSET = 10000;
     if (seed_in == 0 || seed_in == -1)
     {
-        srand((unsigned)time(nullptr) + GlobalV::MY_RANK * 10000); // GlobalV global variables are reserved
+        srand(static_cast<unsigned int>(time(nullptr)) + GlobalV::MY_RANK * RANK_SEED_OFFSET); // GlobalV global variables are reserved
     }
     else
     {
-        srand((unsigned)std::abs(seed_in) + (GlobalV::MY_BNDGROUP * GlobalV::NPROC_IN_BNDGROUP + GlobalV::RANK_IN_BPGROUP) * 10000);
+        srand(static_cast<unsigned int>(std::abs(seed_in)) + (GlobalV::MY_BNDGROUP * GlobalV::NPROC_IN_BNDGROUP + GlobalV::RANK_IN_BPGROUP) * RANK_SEED_OFFSET);
     }
 
     this->allocate_chi0();
