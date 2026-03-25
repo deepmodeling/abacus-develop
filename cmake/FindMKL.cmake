632ab89e5a875da8fd3edee0c20e2023dee456c9
Growl <uwsy1059@qq.com>
2026-03-26 02:17:13 +0800

    Enable compatibility of GNU compilers + MKL

--- cmake/FindMKL.cmake
+++ cmake/FindMKL.cmake
@@ -9,13 +9,19 @@ if(NOT TARGET MKL::MKL)
 
 find_path(MKL_INCLUDE mkl_service.h HINTS ${MKLROOT}/include)
 
-find_library(MKL_INTEL NAMES mkl_intel_lp64 HINTS ${MKLROOT}/lib ${MKLROOT}/lib/intel64)
-find_library(MKL_INTEL_THREAD NAMES mkl_intel_thread HINTS ${MKLROOT}/lib ${MKLROOT}/lib/intel64)
 find_library(MKL_CORE NAMES mkl_core HINTS ${MKLROOT}/lib ${MKLROOT}/lib/intel64)
-find_library(MKL_IOMP5 NAMES iomp5
-  HINTS ENV CMPLR_ROOT
-  PATH_SUFFIXES lib lib/intel64 linux/compiler/lib/intel64_lin
-)
+if(CMAKE_CXX_COMPILER_ID MATCHES "Intel")
+  find_library(MKL_INTERFACE_LIB NAMES mkl_intel_lp64 HINTS ${MKLROOT}/lib ${MKLROOT}/lib/intel64)
+  find_library(MKL_THREAD NAMES mkl_intel_thread HINTS ${MKLROOT}/lib ${MKLROOT}/lib/intel64)
+  find_library(MKL_IOMP5 NAMES iomp5
+    HINTS ENV CMPLR_ROOT
+    PATH_SUFFIXES lib lib/intel64 linux/compiler/lib/intel64_lin
+  )
+else()
+  find_library(MKL_INTERFACE_LIB NAMES mkl_gf_lp64 HINTS ${MKLROOT}/lib ${MKLROOT}/lib/intel64)
+  find_library(MKL_THREAD NAMES mkl_gnu_thread HINTS ${MKLROOT}/lib ${MKLROOT}/lib/intel64)
+  # With GCC we use system-installed GNU OpenMP
+endif()
 if(ENABLE_MPI)
   find_library(MKL_SCALAPACK NAMES mkl_scalapack_lp64 HINTS ${MKLROOT}/lib ${MKLROOT}/lib/intel64)
   find_library(MKL_BLACS_INTELMPI NAMES mkl_blacs_intelmpi_lp64 HINTS ${MKLROOT}/lib ${MKLROOT}/lib/intel64)
@@ -26,22 +32,22 @@ include(FindPackageHandleStandardArgs)
 # if all listed variables are TRUE
 
 if(ENABLE_MPI)
-  find_package_handle_standard_args(MKL DEFAULT_MSG MKL_INTEL MKL_INTEL_THREAD MKL_CORE MKL_SCALAPACK MKL_BLACS_INTELMPI MKL_INCLUDE)
+  find_package_handle_standard_args(MKL DEFAULT_MSG MKL_INTERFACE_LIB MKL_THREAD MKL_CORE MKL_SCALAPACK MKL_BLACS_INTELMPI MKL_INCLUDE)
 else()
-  find_package_handle_standard_args(MKL MKL_INTEL MKL_INTEL_THREAD MKL_CORE MKL_INCLUDE)
+  find_package_handle_standard_args(MKL MKL_INTERFACE_LIB MKL_THREAD MKL_CORE MKL_INCLUDE)
 endif()
 
 if(MKL_FOUND)
-  if(NOT TARGET MKL::INTEL)
-    add_library(MKL::INTEL UNKNOWN IMPORTED)
-    set_target_properties(MKL::INTEL PROPERTIES
-      IMPORTED_LOCATION "${MKL_INTEL}"
+  if(NOT TARGET MKL::INTERFACE_LIB)
+    add_library(MKL::INTERFACE_LIB UNKNOWN IMPORTED)
+    set_target_properties(MKL::INTERFACE_LIB PROPERTIES
+      IMPORTED_LOCATION "${MKL_INTERFACE_LIB}"
       INTERFACE_INCLUDE_DIRECTORIES "${MKL_INCLUDE}")
   endif()
-  if(NOT TARGET MKL::INTEL_THREAD)
-    add_library(MKL::INTEL_THREAD UNKNOWN IMPORTED)
-    set_target_properties(MKL::INTEL_THREAD PROPERTIES
-      IMPORTED_LOCATION "${MKL_INTEL_THREAD}"
+  if(NOT TARGET MKL::THREAD)
+    add_library(MKL::THREAD UNKNOWN IMPORTED)
+    set_target_properties(MKL::THREAD PROPERTIES
+      IMPORTED_LOCATION "${MKL_THREAD}"
       INTERFACE_INCLUDE_DIRECTORIES "${MKL_INCLUDE}")
   endif()
   if(NOT TARGET MKL::CORE)
@@ -72,14 +78,14 @@ if(MKL_FOUND)
     set_property(TARGET MKL::MKL PROPERTY
     INTERFACE_LINK_LIBRARIES
     "-Wl,--start-group"
-    MKL::INTEL MKL::INTEL_THREAD MKL::CORE MKL::MKL_SCALAPACK MKL::BLACS_INTELMPI
+    MKL::INTERFACE_LIB MKL::THREAD MKL::CORE MKL::MKL_SCALAPACK MKL::BLACS_INTELMPI
     "-Wl,--end-group"
     )
   else()
     set_property(TARGET MKL::MKL PROPERTY
     INTERFACE_LINK_LIBRARIES
     "-Wl,--start-group"
-    MKL::INTEL MKL::INTEL_THREAD MKL::CORE
+    MKL::INTERFACE_LIB MKL::THREAD MKL::CORE
     "-Wl,--end-group"
     )
   endif()
@@ -90,9 +96,9 @@ if(MKL_FOUND)
 endif()
 
 if(ENABLE_MPI)
-  mark_as_advanced(MKL_INCLUDE MKL_INTEL MKL_INTEL_THREAD MKL_CORE MKL_SCALAPACK MKL_BLACS_INTELMPI)
+  mark_as_advanced(MKL_INCLUDE MKL_INTERFACE_LIB MKL_THREAD MKL_CORE MKL_SCALAPACK MKL_BLACS_INTELMPI)
 else()
-  mark_as_advanced(MKL_INCLUDE MKL_INTEL MKL_INTEL_THREAD MKL_CORE)
+  mark_as_advanced(MKL_INCLUDE MKL_INTERFACE_LIB MKL_THREAD MKL_CORE)
 endif()
 
 endif() # MKL::MKL
