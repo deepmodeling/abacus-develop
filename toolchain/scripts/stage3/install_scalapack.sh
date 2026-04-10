bf35a25ece5427d087b8593e199844b6a513678f
Growl <uwsy1059@qq.com>
2026-04-10 23:00:40 +0800

    Make remove_path more robust

--- toolchain/scripts/stage3/install_scalapack.sh
+++ toolchain/scripts/stage3/install_scalapack.sh
@@ -126,7 +126,6 @@ EOF
 export SCALAPACK_ROOT="${pkg_install_dir}"
 export SCALAPACK_LDFLAGS="${SCALAPACK_LDFLAGS}"
 export SCALAPACK_LIBS="${SCALAPACK_LIBS}"
-export SCALAPACK_ROOT="${pkg_install_dir}"
 export CP_DFLAGS="\${CP_DFLAGS} IF_MPI(-D__SCALAPACK|)"
 export CP_LDFLAGS="\${CP_LDFLAGS} IF_MPI(${SCALAPACK_LDFLAGS}|)"
 export CP_LIBS="IF_MPI(-lscalapack|) \${CP_LIBS}"
--- toolchain/scripts/stage4/install_cereal.sh
+++ toolchain/scripts/stage4/install_cereal.sh
@@ -115,10 +115,6 @@ if [ "$with_cereal" != "__DONTUSE__" ]; then
 prepend_path CPATH "${pkg_install_dir}/include"
 prepend_path CMAKE_PREFIX_PATH "${pkg_install_dir}"
 EOF
-    else
-        cat << EOF > "${BUILDDIR}/setup_cereal"
-export CEREAL_ROOT="${pkg_install_dir}"
-EOF
     fi
     cat "${BUILDDIR}/setup_cereal" >> $SETUPFILE
     cat << EOF >> "${BUILDDIR}/setup_cereal"
