94ea4ab3e2a7a919a6dd35615e6b18aeec3ab048
abacus_fixer <mohanchen@pku.eud.cn>
2026-05-24 18:38:10 +0800

    update elf

--- source/source_io/module_elf/write_elf.cpp
+++ source/source_io/module_elf/write_elf.cpp
@@ -18,61 +18,78 @@ void write_elf(
     const int& precision,
     const std::string& geom_block)
 {
-    // For nspin = 4, we only calculate the total ELF using the rho_total and tau_total,
-    // containing in the first channel of rho and tau.
-    // What's more, we have not introduced the U(1) and SU(2) gauge invariance corrections
-    // proposed by Desmarais J K, Vignale G, Bencheikh K, et al. Physical Review Letters, 2024, 133(13): 136401,
-    // where the current density is also included in the ELF calculation.
+    // For nspin = 4, we only calculate the total ELF using the
+    // rho_total and tau_total, containing in the first channel of
+    // rho and tau.
+    // What's more, we have not introduced the U(1) and SU(2) gauge
+    // invariance corrections proposed by Desmarais J K, Vignale G,
+    // Bencheikh K, et al. Physical Review Letters, 2024,
+    // 133(13): 136401, where the current density is also included
+    // in the ELF calculation.
 
     const int nspin_eff = (nspin == 4) ? 1 : nspin;
 
-    std::vector<std::vector<double>> elf(nspin_eff, std::vector<double>(rho_basis->nrxx, 0.));
+    const int nrxx = rho_basis->nrxx;
+    const int npw = rho_basis->npw;
+
+    std::vector<std::vector<double>> elf(nspin_eff, std::vector<double>(nrxx, 0.));
     // 1) calculate the kinetic energy density of vW KEDF
-    std::vector<std::vector<double>> tau_vw(nspin_eff, std::vector<double>(rho_basis->nrxx, 0.));
-    std::vector<double> phi(rho_basis->nrxx, 0.); // phi = sqrt(rho)
+    std::vector<std::vector<double>> tau_vw(nspin_eff, std::vector<double>(nrxx, 0.));
+    std::vector<double> phi(nrxx, 0.);
 
     for (int is = 0; is < nspin_eff; ++is)
     {
-#pragma omp parallel for schedule(static) default(none) shared(phi, rho, rho_basis, is)
-        for (int ir = 0; ir < rho_basis->nrxx; ++ir)
+#pragma omp parallel for schedule(static) \
+    default(none) firstprivate(nrxx) shared(phi, rho, rho_basis, is)
+        for (int ir = 0; ir < nrxx; ++ir)
         {
             phi[ir] = std::sqrt(std::abs(rho[is][ir]));
         }
-        
-        std::vector<std::vector<double>> gradient_phi(3, std::vector<double>(rho_basis->nrxx, 0.));
-        std::vector<std::complex<double>> recip_phi(rho_basis->npw, 0.0);
-        std::vector<std::complex<double>> recip_gradient_phi(rho_basis->npw, 0.0);
-        
+
+        std::vector<std::vector<double>> gradient_phi(3, std::vector<double>(nrxx, 0.));
+        std::vector<std::complex<double>> recip_phi(npw, 0.0);
+        std::vector<std::complex<double>> recip_gradient_phi(npw, 0.0);
+
         rho_basis->real2recip(phi.data(), recip_phi.data());
-        
+
         std::complex<double> img(0.0, 1.0);
         for (int j = 0; j < 3; ++j)
         {
-#pragma omp parallel for schedule(static) default(none) shared(recip_gradient_phi, img, rho_basis, recip_phi, j)
-            for (int ip = 0; ip < rho_basis->npw; ++ip)
+#pragma omp parallel for schedule(static) \
+    default(none) firstprivate(img, npw) \
+    shared(recip_gradient_phi, rho_basis, recip_phi, j)
+            for (int ip = 0; ip < npw; ++ip)
             {
-                recip_gradient_phi[ip] = img * rho_basis->gcar[ip][j] * recip_phi[ip] * rho_basis->tpiba;
+                recip_gradient_phi[ip]
+                    = img * rho_basis->gcar[ip][j]
+                    * recip_phi[ip] * rho_basis->tpiba;
             }
 
             rho_basis->recip2real(recip_gradient_phi.data(), gradient_phi[j].data());
 
-#pragma omp parallel for schedule(static) default(none) shared(tau_vw, gradient_phi, is, j, rho_basis)
-            for (int ir = 0; ir < rho_basis->nrxx; ++ir)
+#pragma omp parallel for schedule(static) \
+    default(none) firstprivate(nrxx) \
+    shared(tau_vw, gradient_phi, is, j, rho_basis)
+            for (int ir = 0; ir < nrxx; ++ir)
             {
-                tau_vw[is][ir] += gradient_phi[j][ir] * gradient_phi[j][ir] / 2. * 2.; // convert Ha to Ry.
+                tau_vw[is][ir] += gradient_phi[j][ir]
+                    * gradient_phi[j][ir] / 2. * 2.;
             }
         }
     }
 
     // 2) calculate the kinetic energy density of TF KEDF
-    std::vector<std::vector<double>> tau_TF(nspin_eff, std::vector<double>(rho_basis->nrxx, 0.));
+    std::vector<std::vector<double>> tau_TF(nspin_eff, std::vector<double>(nrxx, 0.));
+
     const double c_tf
         = 3.0 / 10.0 * std::pow(3 * std::pow(M_PI, 2.0), 2.0 / 3.0)
-          * 2.0; // 10/3*(3*pi^2)^{2/3}, multiply by 2 to convert unit from Hartree to Ry, finally in Ry*Bohr^(-2)
+          * 2.0; // convert unit from Hartree to Ry
     if (nspin == 1 || nspin == 4)
     {
-#pragma omp parallel for schedule(static) default(none) shared(rho, tau_TF, c_tf, rho_basis)
-        for (int ir = 0; ir < rho_basis->nrxx; ++ir)
+#pragma omp parallel for schedule(static) \
+    default(none) firstprivate(c_tf, nrxx) \
+    shared(rho, tau_TF, rho_basis)
+        for (int ir = 0; ir < nrxx; ++ir)
         {
             if (rho[0][ir] > 0.0)
             {
@@ -86,15 +103,19 @@ void write_elf(
     }
     else if (nspin == 2)
     {
-        // the spin-scaling law: tau_TF[rho_up, rho_dn] = 1/2 * (tau_TF[2*rho_up] + tau_TF[2*rho_dn])
+        // spin-scaling: tau_TF[rho_up,rho_dn]
+        //     = 1/2 * (tau_TF[2*rho_up] + tau_TF[2*rho_dn])
         for (int is = 0; is < nspin; ++is)
         {
-#pragma omp parallel for schedule(static) default(none) shared(rho, tau_TF, c_tf, is, rho_basis)
-            for (int ir = 0; ir < rho_basis->nrxx; ++ir)
+#pragma omp parallel for schedule(static) \
+    default(none) firstprivate(c_tf, nrxx) \
+    shared(rho, tau_TF, is, rho_basis)
+            for (int ir = 0; ir < nrxx; ++ir)
             {
                 if (rho[is][ir] > 0.0)
                 {
-                    tau_TF[is][ir] = 0.5 * c_tf * std::pow(2.0 * rho[is][ir], 5.0 / 3.0);
+                    tau_TF[is][ir] = 0.5 * c_tf
+                        * std::pow(2.0 * rho[is][ir], 5.0 / 3.0);
                 }
                 else
                 {
@@ -104,16 +125,20 @@ void write_elf(
         }
     }
 
-    // 3) calculate the enhancement factor F = (tau_KS - tau_vw) / tau_TF, and then ELF = 1 / (1 + F^2)
-    const double eps = 1.0e-5; // suppress the numerical instability in LCAO (Ref: Acta Phys. -Chim. Sin. 2011, 27(12), 2786-2792. doi: 10.3866/PKU.WHXB20112786)
+    // 3) calculate the enhancement factor F = (tau_KS - tau_vw) /
+    //    tau_TF, and then ELF = 1 / (1 + F^2)
+    const double eps = 1.0e-5;
     for (int is = 0; is < nspin_eff; ++is)
     {
-#pragma omp parallel for schedule(static) default(none) shared(elf, tau, tau_vw, tau_TF, eps, is, rho_basis)
-        for (int ir = 0; ir < rho_basis->nrxx; ++ir)
+#pragma omp parallel for schedule(static) \
+    default(none) firstprivate(eps, nrxx) \
+    shared(elf, tau, tau_vw, tau_TF, is, rho_basis)
+        for (int ir = 0; ir < nrxx; ++ir)
         {
             if (tau_TF[is][ir] > 1.0e-12)
             {
-                elf[is][ir] = (tau[is][ir] - tau_vw[is][ir] + eps) / tau_TF[is][ir];
+                elf[is][ir] = (tau[is][ir] - tau_vw[is][ir] + eps)
+                    / tau_TF[is][ir];
                 elf[is][ir] = 1. / (1. + elf[is][ir] * elf[is][ir]);
             }
             else
@@ -141,13 +166,14 @@ void write_elf(
             ef_tmp,
             ucell_,
             precision,
-            out_fermi);   
+            out_fermi);
     }
     else if (nspin == 2)
     {
         for (int is = 0; is < nspin; ++is)
         {
-            std::string fn_temp = out_dir + "/elf" + std::to_string(is + 1) + geom_block + ".cube";
+            std::string fn_temp = out_dir + "/elf"
+                + std::to_string(is + 1) + geom_block + ".cube";
 
             const int ispin = is + 1;
 
@@ -160,15 +186,27 @@ void write_elf(
                 ef_tmp,
                 ucell_,
                 precision,
-                out_fermi);   
+                out_fermi);
         }
 
-        std::vector<double> elf_tot(rho_basis->nrxx, 0.0);
-#pragma omp parallel for schedule(static) default(none) shared(elf_tot, tau, tau_vw, tau_TF, rho_basis)
-        for (int ir = 0; ir < rho_basis->nrxx; ++ir)
+        std::vector<double> elf_tot(nrxx, 0.0);
+#pragma omp parallel for schedule(static) \
+    default(none) firstprivate(eps, nrxx) \
+    shared(elf_tot, tau, tau_vw, tau_TF, rho_basis)
+        for (int ir = 0; ir < nrxx; ++ir)
         {
-            elf_tot[ir] = (tau[0][ir] + tau[1][ir] - tau_vw[0][ir] - tau_vw[1][ir]) / (tau_TF[0][ir] + tau_TF[1][ir]);
-            elf_tot[ir] = 1. / (1. + elf_tot[ir] * elf_tot[ir]);
+            if (tau_TF[0][ir] + tau_TF[1][ir] > 1.0e-12)
+            {
+                elf_tot[ir] = (tau[0][ir] + tau[1][ir]
+                    - tau_vw[0][ir] - tau_vw[1][ir] + eps)
+                    / (tau_TF[0][ir] + tau_TF[1][ir]);
+                elf_tot[ir] = 1.
+                    / (1. + elf_tot[ir] * elf_tot[ir]);
+            }
+            else
+            {
+                elf_tot[ir] = 0.0;
+            }
         }
         std::string fn = out_dir + "/elf" + geom_block + ".cube";
 
