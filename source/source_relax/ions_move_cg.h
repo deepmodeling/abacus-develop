499adf24bddc321c0be9f8df133ed75b10234bb9
abacus_fixer <mohanchen@pku.eud.cn>
2026-06-09 14:34:12 +0800

    update

--- source/source_relax/ions_move_cg.h
+++ source/source_relax/ions_move_cg.h
@@ -3,42 +3,28 @@
 
 #include "source_base/matrix.h"
 #include "source_cell/unitcell.h"
-class Ions_Move_CG
+#include "cg_base.h"
+#include <vector>
+
+class Ions_Move_CG : public CG_Base
 {
   public:
     Ions_Move_CG();
-    ~Ions_Move_CG();
+    ~Ions_Move_CG() = default;
 
     void allocate(void);
     void start(UnitCell &ucell, const ModuleBase::matrix &force, const double &etot);
 
     static double RELAX_CG_THR;
-    int sd_step=0;
-    int cg_step=0;
+    int sd_step = 0;
+    int cg_step = 0;
 
   private:
-    double * pos0 = nullptr;
-    double * grad0 = nullptr;
-    double * cg_grad0 = nullptr;
-    double * move0 = nullptr;
-    double e0=0.0;
-    // setup gradients.
-    void setup_cg_grad(double *grad,
-                       const double *grad0,
-                       double *cg_grad,
-                       const double *cg_grad0,
-                       const int &ncggrad,
-                       int &flag); // LiuXh fix bug of lpf, 20180515
-    void setup_move(double *move, double *cg_gradn, const double &trust_radius);
-    void Brent(double &fa, double &fb, double &fc, double &xa, double &xb, double &xc, double &best_x, double &xpt);
-    void f_cal(const double *g0, const double *g1, const int &dim, double &f_value);
-    void third_order(const double &e0,
-                     const double &e1,
-                     const double &fa,
-                     const double &fb,
-                     const double x,
-                     double &best_x);
-    void normalize(double *cg_gradn, const double *cg_grad, int dim);
+    std::vector<double> pos0;
+    std::vector<double> grad0;
+    std::vector<double> cg_grad0;
+    std::vector<double> move0;
+    double e0 = 0.0;
 };
 
 #endif
