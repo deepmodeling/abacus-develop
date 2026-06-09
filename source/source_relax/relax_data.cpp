bdb057a07e7ac938eb2233300733057e1bfa88a1
abacus_fixer <mohanchen@pku.eud.cn>
2026-06-09 14:00:42 +0800

    add relax_data

---
+++ source/source_relax/relax_data.cpp
@@ -0,0 +1,18 @@
+#include "relax_data.h"
+
+int Relax_Data::dim = 0;
+bool Relax_Data::converged = false;
+double Relax_Data::largest_grad = 0.0;
+int Relax_Data::istep = 0;
+double Relax_Data::ediff = 0.0;
+double Relax_Data::etot = 0.0;
+double Relax_Data::etot_p = 0.0;
+
+void Relax_Data::allocate(const int dim_in) {
+    pos.resize(dim_in, 0.0);
+    grad.resize(dim_in, 0.0);
+    move.resize(dim_in, 0.0);
+    pos_p.resize(dim_in, 0.0);
+    grad_p.resize(dim_in, 0.0);
+    move_p.resize(dim_in, 0.0);
+}
