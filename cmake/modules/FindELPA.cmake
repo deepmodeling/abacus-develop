9d340190bd92794e15eeec23c63dc28350494d91
Growl <uwsy1059@qq.com>
2026-07-14 12:12:07 +0800

    Keep ELPA_DIR but mark as deprecated

--- cmake/modules/FindELPA.cmake
+++ cmake/modules/FindELPA.cmake
@@ -3,17 +3,64 @@
 # Find the native ELPA headers and libraries through pkg-config.
 #
 
-find_package(PkgConfig)
+# ========================================================================
+# Deprecated (TODO: Remove this part)
+# ========================================================================
 
-if(PKG_CONFIG_FOUND)
+# Compatible layer towards old manual routines
+if(DEFINED ELPA_DIR)
+  message(WARNING "ELPA_DIR is deprecated and will be removed in the future release.")
+endif()
+if(DEFINED ELPA_INCLUDE_DIR)
+  set(ELPA_INCLUDE_DIRS ${ELPA_INCLUDE_DIR})
+endif()
+if(DEFINED ELPA_LIBRARIES)
+  set(ELPA_LINK_LIBRARIES ${ELPA_LIBRARIES})
+endif()
+
+find_path(ELPA_INCLUDE_DIRS
+    elpa/elpa.h
+    HINTS ${ELPA_DIR}
+    PATH_SUFFIXES "include" "include/elpa"
+    )
+# Fix #3589
+# First if judges if ELPA dir specified
+if(ELPA_INCLUDE_DIRS MATCHES "^/usr/include/elpa/.*")
+  # Second if judges if global visible ELPA header found
+  if(DEFINED ELPA_DIR OR CMAKE_PREFIX_PATH MATCHES ".*elpa.*")
+    unset(ELPA_INCLUDE_DIRS)
+  endif()
+endif()
+if(ENABLE_OPENMP)
+  find_library(ELPA_LINK_LIBRARIES
+    NAMES elpa_openmp elpa
+    HINTS ${ELPA_DIR}
+    PATH_SUFFIXES "lib"
+  )
+else()
+  find_library(ELPA_LINK_LIBRARIES
+    NAMES elpa
+    HINTS ${ELPA_DIR}
+    PATH_SUFFIXES "lib"
+  )
+endif()
+
+# ========================================================================
+
+if(NOT ELPA_INCLUDE_DIRS)
+  find_package(PkgConfig)
+  if(NOT PKG_CONFIG_FOUND)
+    message(FATAL_ERROR "Pkg-config is needed to get all information about the ELPA library")
+  endif()
   # Find preferred library corresponding with ABACUS configuration first
   if(ENABLE_OPENMP)
     pkg_search_module(ELPA REQUIRED IMPORTED_TARGET GLOBAL elpa_openmp elpa)
   else()
-    pkg_search_module(ELPA REQUIRED IMPORTED_TARGET GLOBAL elpa elpa_openmp)
+    pkg_search_module(ELPA REQUIRED IMPORTED_TARGET GLOBAL elpa)
   endif()
-else()
-  message(FATAL_ERROR "Pkg-config is needed to get all information about the ELPA library")
+  if(${ELPA_VERSION} VERSION_LESS "2021.05.001")
+    message(FATAL_ERROR "ELPA version >= 2021.05.001 is required.")
+  endif()
 endif()
 
 # Handle the QUIET and REQUIRED arguments and
@@ -36,8 +83,20 @@ endif()
 
 set(CMAKE_REQUIRED_INCLUDES ${CMAKE_REQUIRED_INCLUDES} ${ELPA_INCLUDE_DIR})
 
-if(${ELPA_VERSION} VERSION_LESS "2021.05.001")
-  message(FATAL_ERROR "ELPA version >= 2021.05.001 is required.")
+# Compability workaround for ELPA_DIR
+# TODO: Remove this check
+include(CheckCXXSourceCompiles)
+check_cxx_source_compiles("
+#include <elpa/elpa_version.h>
+#if ELPA_API_VERSION < 20210430
+#error ELPA version is too old.
+#endif
+int main(){}
+"
+ELPA_VERSION_SATISFIES
+)
+if(NOT ELPA_VERSION_SATISFIES)
+  message(FATAL_ERROR "ELPA version is too old. We support version 2021 or higher.")
 endif()
 
 mark_as_advanced(ELPA_INCLUDE_DIR ELPA_LIBRARY)
