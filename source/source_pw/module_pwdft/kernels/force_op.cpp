e7ba46c1f73dadd92e3f86f591b3e06251703b01
dyzheng <zhengdy@bjaisi.com>
2026-07-24 19:53:47 +0800

    Revert: erroneous PW sigma_y sign fix was wrong
    PW kernel uses different 2x2 matrix layout from LCAO:
      kernel: ps(up)=coeff[0]*becp(up)+coeff[2]*becp(dn)
              ps(dn)=coeff[1]*becp(up)+coeff[3]*becp(dn)
      So: coeff[2]=H_DS[up,dn], coeff[1]=H_DS[dn,up]
    
      LCAO:  coeff[1]=H_DS[up,dn], coeff[2]=H_DS[dn,up]
    
      The indices are SWAPPED. Old PW code was already correct.
      Reverting to original convention:
      coeff[1]=(+lambda_y), coeff[2]=(-lambda_y)

--- source/source_pw/module_pwdft/kernels/force_op.cpp
+++ source/source_pw/module_pwdft/kernels/force_op.cpp
@@ -406,8 +406,8 @@ struct cal_force_nl_op<FPTYPE, base_device::DEVICE_CPU>
                 int iat = iat0 + ia;
                 int sum = sum0 + ia * nproj;
                 const std::complex<FPTYPE> coefficients0(lambda[iat*3+2], 0.0);
-                const std::complex<FPTYPE> coefficients1(lambda[iat*3] , -lambda[iat*3+1]);
-                const std::complex<FPTYPE> coefficients2(lambda[iat*3] , lambda[iat*3+1]);
+                const std::complex<FPTYPE> coefficients1(lambda[iat*3] , lambda[iat*3+1]);
+                const std::complex<FPTYPE> coefficients2(lambda[iat*3] , -lambda[iat*3+1]);
                 const std::complex<FPTYPE> coefficients3(-lambda[iat*3+2], 0.0);
                 for (int ib = 0; ib < nbands_occ; ib++)
                 {
--- source/source_pw/module_pwdft/op_pw_proj.cpp
+++ source/source_pw/module_pwdft/op_pw_proj.cpp
@@ -163,8 +163,8 @@ void OnsiteProj<OperatorPW<T, Device>>::cal_ps_delta_spin(const int npol, const 
         for(int iat=0;iat<this->ucell->nat;iat++)
         {
             tmp_lambda_coeff[iat * 4] = std::complex<double>(lambda[iat][2], 0.0);
-            tmp_lambda_coeff[iat * 4 + 1] = std::complex<double>(lambda[iat][0], -lambda[iat][1]);
-            tmp_lambda_coeff[iat * 4 + 2] = std::complex<double>(lambda[iat][0], lambda[iat][1]);
+            tmp_lambda_coeff[iat * 4 + 1] = std::complex<double>(lambda[iat][0], lambda[iat][1]);
+            tmp_lambda_coeff[iat * 4 + 2] = std::complex<double>(lambda[iat][0], -lambda[iat][1]);
             tmp_lambda_coeff[iat * 4 + 3] = std::complex<double>(-lambda[iat][2], 0.0);
         }
     }
